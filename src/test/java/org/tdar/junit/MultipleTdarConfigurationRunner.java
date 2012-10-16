b04afee1ff4b7d1a80a9794228d50acc7599386d
abrin <abrin@localhost>
2012-10-16 07:32:07 -0700

    adjusting testrunner

--- src/test/java/org/tdar/junit/MultipleTdarConfigurationRunner.java
+++ src/test/java/org/tdar/junit/MultipleTdarConfigurationRunner.java
@@ -3,29 +3,34 @@ package org.tdar.junit;
 import org.junit.Ignore;
 import org.junit.runner.Description;
 import org.junit.runner.notification.RunNotifier;
-import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.tdar.core.configuration.TdarConfiguration;
-
+import org.tdar.junit.RunWithTdarConfiguration;
 public class MultipleTdarConfigurationRunner extends SpringJUnit4ClassRunner {
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     public MultipleTdarConfigurationRunner(Class<?> klass) throws InitializationError {
         super(klass);
     }
 
-    @Override
-    protected Description describeChild(FrameworkMethod method) {
-        // if (method.getAnnotation(RunWithTdarConfiguration.class) != null &&
-        // method.getAnnotation(Ignore.class) == null) {
-        // return describeTest(method);
-        // }
-        return super.describeChild(method);
-    }
+    
+//    
+//    @Override
+//    protected Description describeChild(FrameworkMethod method) {
+//        // if (method.getAnnotation(RunWithTdarConfiguration.class) != null &&
+//        // method.getAnnotation(Ignore.class) == null) {
+//        // return describeTest(method);
+//        // }
+//        return super.describeChild(method);
+//    }
 
     private Description describeTest(FrameworkMethod method) {
         String[] configs = method.getAnnotation(RunWithTdarConfiguration.class).runWith();
+        logger.info(testName(method));
         Description description = Description.createSuiteDescription(testName(method), method.getAnnotations());
 
         for (int i = 0; i < configs.length; i++) {
@@ -38,7 +43,7 @@ public class MultipleTdarConfigurationRunner extends SpringJUnit4ClassRunner {
     @Override
     protected void runChild(final FrameworkMethod method, RunNotifier notifier) {
         Description description = describeTest(method);
-
+        String testName = testName(method);
         final String currentConfig = TdarConfiguration.getInstance().getConfigurationFile();
         if (method.getAnnotation(RunWithTdarConfiguration.class) != null &&
                 method.getAnnotation(Ignore.class) == null) {
@@ -46,7 +51,7 @@ public class MultipleTdarConfigurationRunner extends SpringJUnit4ClassRunner {
 
             if (configs.length > 0) {
                 for (int i = 0; i < configs.length; i++) {
-                    System.out.println(configs[i]);
+                    logger.info(String.format("#############     Running %s with config [%s]      #############", testName, configs[i]));
                     TdarConfiguration.getInstance().setConfigurationFile(configs[i]);
                     runLeaf(methodBlock(method), description.getChildren().get(i), notifier);
                 }
