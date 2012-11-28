e1d318116e51daa86b91ea264ce52594677465ba
jdevos <jdevos@localhost>
2012-11-28 03:50:36 -0700

    quoting queries against @AutocompleteAnalyzer fields, adding a couple fixme's
    - I'm pretty sure that we'll always want to quote our searches against  autocomplete fields.  consider that when we index the name_auto field of "brown university" all of the ngrams will start with "o".  So a lucene query for  "name_auto: (brown university)"  will match name_auto values that start with "brown"   (good)  and name_auto values that start with "university" (bad).
    
    - along that line,AutocompleteTitleQueryPart  also includes a querypart that searches the title_sort field.  But since sort fields aren't analyzed I don't think searching against them will yield anything helpful.  But I'm fuzzy on how lucene works at index time so i left it as a fixme lest I break something.
    
    -theres a broken selector in tdar-base.css that I think is meant for the "searchbox", but it looks weird when I try to fix it.  punting w/ another fixme.

--- src/main/java/org/tdar/search/query/part/AutocompleteTitleQueryPart.java
+++ src/main/java/org/tdar/search/query/part/AutocompleteTitleQueryPart.java
@@ -29,8 +29,9 @@ public class AutocompleteTitleQueryPart implements QueryPart<String> {
             return null;
         QueryPartGroup titleGroup = new QueryPartGroup(Operator.OR);
         FieldQueryPart<String> autoPart = new FieldQueryPart<String>(QueryFieldNames.TITLE_AUTO, title).setBoost(TITLE_BOOST).setPhraseFormatters(
-                PhraseFormatter.ESCAPED);
+                PhraseFormatter.ESCAPE_QUOTED);
         autoPart.setOperator(Operator.AND);
+        //FIXME: while allowed, I'm not sure it's helpful to include non-analyzed fields in a search, especially considering the fact that it will use a default analyzer at search-time. arguments otherwise?
         FieldQueryPart<String> part = new FieldQueryPart<String>(QueryFieldNames.TITLE_SORT, value).setPhraseFormatters(PhraseFormatter.ESCAPED,
                 PhraseFormatter.WILDCARD);
         FieldQueryPart<String> part2 = new FieldQueryPart<String>(QueryFieldNames.TITLE, value);
