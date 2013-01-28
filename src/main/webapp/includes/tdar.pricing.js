452df88a30114e995310e6b77f340eb346f60442
Jim deVos <james.t.devos@asu.edu>
2013-01-28 15:31:17 -0700

    fix for js error on pricing init

--- src/main/webapp/includes/tdar.pricing.js
+++ src/main/webapp/includes/tdar.pricing.js
@@ -54,12 +54,14 @@ var _initPricing = function(form, ajaxUrl) {
           //(i +1), data[i].model, data[i].subtotal );
           var total_files =0;
           var total_mb = 0;
-          for (var j=0; j < item.parts.length; j++) {
-              var part = item.parts[j];
-              var line = sprintf("<tr><td>{0}</td><td>{1}</td><td>{2} MB</td><td>${3}</td></tr>", part.name, part.numFiles * part.quantity, part.numMb  * part.quantity, part.subtotal);
-              total_files += part.numFiles * part.quantity;
-              total_mb += part.numMb * part.quantity;
-              $est.append(line);
+          if(item.parts && item.parts.length) {
+              for (var j=0; j < item.parts.length; j++) {
+                  var part = item.parts[j];
+                  var line = sprintf("<tr><td>{0}</td><td>{1}</td><td>{2} MB</td><td>${3}</td></tr>", part.name, part.numFiles * part.quantity, part.numMb  * part.quantity, part.subtotal);
+                  total_files += part.numFiles * part.quantity;
+                  total_mb += part.numMb * part.quantity;
+                  $est.append(line);
+              }
           }
           var line = sprintf("<tr class='table-row-separator-above'><td></td><td class='subtotal'>{0}</td><td class='subtotal'>{1} MB</td><td class='red'>${2}</td></tr>", total_files, total_mb, subtotal);
           $est.append(line);
