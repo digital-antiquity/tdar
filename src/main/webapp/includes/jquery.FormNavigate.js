bc566034e756e10587317b903e211846653651f2
Jim deVos <james.t.devos@asu.edu>
2013-02-01 16:44:58 -0700

    make form dirty when user deletes a repeat-row.

--- src/main/webapp/includes/jquery.FormNavigate.js
+++ src/main/webapp/includes/jquery.FormNavigate.js
@@ -15,9 +15,14 @@
   * ==================
   *
   * How to Use:
-  * $("YourForm").FormNavigate("YourMessage");
+  * $("YourForm").FormNavigate(options);
   *  -- "YourForm" as Your Form ID $("#form") or any method to grab your form
-  *  -- "YourMessage" as Your onBeforeUnload Prompt Message Here
+  *  -- options: can be either a string (representing the confirmation message the plugin will display), or an object with the following fields
+  *         message:    confirmation messaged displayed to the user
+  *         customEvents:     A space-separated list of additional events that, when triggered within the form, will cause the plugin to consider the form "dirty".
+  *                     The plugin will always monitor keyup and change events.  
+  *     
+  *
   *
   * This plugin handles onchange of input type of text, textarea, password, radio, checkbox, select and file to toggle on 
 
@@ -25,18 +30,31 @@ and off of window.onbeforeunload event.
   * Users are able to configure the custom onBeforeUnload message.
   */
 (function($){
-    $.fn.FormNavigate = function(message) {
+    $.fn.FormNavigate = function(options) {
+        "use strict";
+        var _options = {customEvents:null, message:"Your changes have not been saved."};
+        if(typeof options === "string") {
+            _options.message = options;
+        }else if(typeof options === "object") {
+            $.extend(_options, options);
+        }
+        
         this.each(function() {
             var $this = $(this);
             $this.data("formNavigate", true);
         	$(window).bind("beforeunload", function (event) {
-                    if ($this.data("formNavigate")) {  event.cancelBubble = true;  }  else  { return message;}
+                    if ($this.data("formNavigate")) {  event.cancelBubble = true;  }  else  { return _options.message;}
         	});
         	
-        	$this.one("keyup change", function(evt) {
-        	    console.log("Form #%s has become dirty. event:%s\t target:%s\t ", $this.attr("id"), evt.type, evt.target);
-        	    $this.data("formNavigate", false);
-        	});
+        	var _eventHandler = function(evt) {
+                console.log("Form #%s has become dirty. event:%s\t target:%s\t ", $this.attr("id"), evt.type, evt.target);
+                $this.data("formNavigate", false);
+        	};
+        	
+        	$this.one("keyup change", _eventHandler);
+        	if(_options.customEvents) {
+        	    $this.one(_options.customEvents, _eventHandler);
+        	}
         	
         	$this.find("input:submit, .submitButton").click(function(){
         	    $this.data("formNavigate", true);
--- src/main/webapp/includes/tdar.common.js
+++ src/main/webapp/includes/tdar.common.js
@@ -1077,7 +1077,9 @@ TDAR.common = function() {
 
         // I must be "last"
         $("#possibleJsError").val("SAVE");
-        $(form).not('.disableFormNavigate').FormNavigate("Leaving the page will cause any unsaved data to be lost!");
+        $(form).not('.disableFormNavigate').FormNavigate({
+            message:"Leaving the page will cause any unsaved data to be lost!",
+            customEvents: "repeatrowdeleted"});
 
     };
     
