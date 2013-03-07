f36bea907a43aa2eb7a94c50085cf084353c30aa
abrin <abrin@localhost>
2013-03-07 09:14:19 -0700

    work on shapefiles and freemarker

---
+++ src/main/java/org/tdar/filestore/tasks/ShapefileReaderTask.java
@@ -0,0 +1,58 @@
+package org.tdar.filestore.tasks;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.FeatureSource;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
+import org.opengis.feature.Feature;
+import org.opengis.feature.GeometryAttribute;
+import org.opengis.geometry.Geometry;
+import org.tdar.filestore.tasks.Task.AbstractTask;
+
+public class ShapefileReaderTask extends AbstractTask {
+
+    @Override
+    public void run() throws Exception {
+
+        
+        File file = new File("mayshapefile.shp");
+//        http://stackoverflow.com/questions/2044876/does-anyone-know-of-a-library-in-java-that-can-parse-esri-shapefiles
+        try {
+          Map connect = new HashMap();
+          connect.put("url", file.toURL());
+
+          DataStore dataStore = DataStoreFinder.getDataStore(connect);
+          String[] typeNames = dataStore.getTypeNames();
+          String typeName = typeNames[0];
+
+          System.out.println("Reading content " + typeName);
+
+          FeatureSource featureSource = dataStore.getFeatureSource(typeName);
+          FeatureCollection collection = featureSource.getFeatures();
+          FeatureIterator iterator = collection.features();
+
+
+          try {
+            while (iterator.hasNext()) {
+              Feature feature = iterator.next();
+              GeometryAttribute sourceGeometry = feature.getDefaultGeometryProperty();
+            }
+          } finally {
+            iterator.close();
+          }
+
+        } catch (Throwable e) {}        
+    }
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
