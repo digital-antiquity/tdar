65d3c67540fc2228b4e05ee62553d149ba8e1994
Jim deVos <james.t.devos@asu.edu>
2013-04-03 15:45:56 -0700

    modifying ResourceAnnotationTest to align w/ current implementation of equals & hashcode.

--- src/test/java/org/tdar/core/bean/entity/ResourceAnnotationTest.java
+++ src/test/java/org/tdar/core/bean/entity/ResourceAnnotationTest.java
@@ -24,31 +24,35 @@ public class ResourceAnnotationTest {
 
         k1.setKey("key1");
         k2.setKey("key2");
+
         a1.setResourceAnnotationKey(k1);
         a1.setValue(sameValue);
+
         a2.setResourceAnnotationKey(k2);
         a2.setValue(sameValue);
 
-        Assert.assertNotSame("these keys should have different hashcodes", k1.hashCode(), k2.hashCode());
+        //FIXME: technically hashcodes don't *need* to be different if they not equal.
+//        Assert.assertNotSame("these keys should have different hashcodes", k1.hashCode(), k2.hashCode());
         // set the id to be the same, they should *still* have different hashcodes
         Long id = -1L;
         k1.setId(id);
         k2.setId(id);
-        Assert.assertNotSame("these keys should have different hashcodes", k1.hashCode(), k2.hashCode());
-        Assert.assertFalse(k1.equals(k2));
-        Assert.assertFalse(a1.equals(a2));
-        Assert.assertNotSame("these annotations should have different hashcodes", a1.hashCode(), a2.hashCode());
+//        Assert.assertNotSame("these keys should have different hashcodes", k1.hashCode(), k2.hashCode());
+        Assert.assertNotEquals("equality based on field,  values should be different", k1, k2);
+        Assert.assertEquals("equality based on id, values should be equal", a1, a2);
+        
+//        Assert.assertNotSame("these annotations should have different hashcodes", a1.hashCode(), a2.hashCode());
 
         a1.setId(id);
         a2.setId(id);
-        Assert.assertNotSame("these annotations should have different hashcodes", a1.hashCode(), a2.hashCode());
-        Assert.assertFalse(a1.equals(a2));
+//        Assert.assertNotSame("these annotations should have different hashcodes", a1.hashCode(), a2.hashCode());
+        Assert.assertEquals("equality based on id,  should be equal", a1, a2);
 
-        // okay, put these in a set and make sure the set has two items
+        // okay, put these in a set and make sure the set has one item
         Set<ResourceAnnotation> set = new HashSet<ResourceAnnotation>();
         set.add(a1);
         set.add(a2);
-        Assert.assertEquals("set should have two items in it", 2, set.size());
+        Assert.assertEquals("set should have one item in it", 1, set.size());
     }
 
 }
