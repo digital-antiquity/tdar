420b8ee9ca28e1825ce337f95e734be837c5f0cc
abrin <abrin@localhost>
2013-05-31 06:57:47 -0700

    NOW with screenshot(s)!!!

--- src/test/java/org/tdar/web/functional/SeleniumWebITCase.java
+++ src/test/java/org/tdar/web/functional/SeleniumWebITCase.java
@@ -2,18 +2,23 @@ package org.tdar.web.functional;
 
 import static org.tdar.TestConstants.DEFAULT_BASE_URL;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.UnhandledAlertException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -30,10 +35,10 @@ public abstract class SeleniumWebITCase {
 
     public static String REGEX_DOCUMENT_VIEW = ".+\\/document\\/\\d+$";
     public static Pattern PATTERN_DOCUMENT_VIEW = Pattern.compile(REGEX_DOCUMENT_VIEW);
-    
+
     WebDriver driver;
     protected Logger logger = LoggerFactory.getLogger(getClass());
-        
+
     @Before
     public void before() throws MalformedURLException {
         /*
@@ -44,42 +49,52 @@ public abstract class SeleniumWebITCase {
         if (StringUtils.isNotBlank(xvfbPropsFile)) {
             fb.setEnvironmentProperty("DISPLAY", xvfbPropsFile);
         }
-//        Assert.fail("testing...");
+        // Assert.fail("testing...");
         driver = new FirefoxDriver(fb, newFirefoxProfile());
     }
-    
+
     public FirefoxProfile newFirefoxProfile() {
         return new FirefoxProfile();
     }
-    
+
     /**
-     * @return firefox profile that has CSS rendering disabled.  
+     * @return firefox profile that has CSS rendering disabled.
      */
     public final FirefoxProfile firefoxProfileNoCss() {
-        //http://stackoverflow.com/questions/3526361/firefoxdriver-how-to-disable-javascript-css-and-make-sendkeys-type-instantly
+        // http://stackoverflow.com/questions/3526361/firefoxdriver-how-to-disable-javascript-css-and-make-sendkeys-type-instantly
         FirefoxProfile profile = new FirefoxProfile();
         profile.setPreference("permissions.default.stylesheet", 2);
-        //profile.setPreference("permissions.default.image", 2);
+        // profile.setPreference("permissions.default.image", 2);
         return profile;
     }
-    
+
     /*
      * Shutdown Selenium
      */
     @After
     public final void shutdownSelenium() {
         logger.debug("after");
+        takeScreenshot();
         try {
             driver.close();
         } catch (UnhandledAlertException uae) {
-            logger.error("alert modal present when trying to close driver: {}", uae.getAlertText()); 
+            logger.error("alert modal present when trying to close driver: {}", uae.getAlertText());
             driver.switchTo().alert().dismiss();
             driver.close();
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             logger.error("Could not close selenium driver: {}", ex);
         }
         driver = null;
+    }
+
+    private void takeScreenshot() {
+        try {
+            File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            // Now you can do whatever you need to do with it, for example copy somewhere
+            FileUtils.copyFile(scrFile, new File("target/" + System.currentTimeMillis() +".png"));
+        } catch (Exception e) {
+            logger.error("could not take screenshot" , e);
+        }
     }
 
     /*
@@ -88,7 +103,7 @@ public abstract class SeleniumWebITCase {
     public String absoluteUrl(String path) {
         String currentUrl = driver.getCurrentUrl();
         logger.debug("current url: {}", currentUrl);
-        if(currentUrl.length() == 0 || StringUtils.equalsIgnoreCase("about:blank", currentUrl)) {
+        if (currentUrl.length() == 0 || StringUtils.equalsIgnoreCase("about:blank", currentUrl)) {
             currentUrl = DEFAULT_BASE_URL;
         }
         URL url = null;
@@ -104,13 +119,15 @@ public abstract class SeleniumWebITCase {
     public void gotoPage(String path) {
         String url = absoluteUrl(path);
         logger.debug("going to {}", url);
+        takeScreenshot();
         driver.get(absoluteUrl(path));
+        takeScreenshot();
     }
 
     public WebElement waitFor(String selector) {
         return waitFor(selector, 10);
     }
-    
+
     public WebElement waitFor(String cssSelector, int timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
         List<WebElement> elements = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(cssSelector)));
@@ -130,14 +147,16 @@ public abstract class SeleniumWebITCase {
         logger.debug("criteria:{}\t  size:{}", by, selection.size());
         return selection;
     }
-    
-    //FIXME: select() seems more appropriate, given the ways you can select stuff.  Or, since we're aping jquery.. just $()?
+
+    // FIXME: select() seems more appropriate, given the ways you can select stuff. Or, since we're aping jquery.. just $()?
     /**
      * Create a selection out of one or more.
-     * @param elems WebElement objects
+     * 
+     * @param elems
+     *            WebElement objects
      * @return
      */
-    public WebElementSelection find(WebElement ... elems) {
+    public WebElementSelection find(WebElement... elems) {
         return new WebElementSelection(Arrays.asList(elems));
     }
 
@@ -175,8 +194,9 @@ public abstract class SeleniumWebITCase {
     public String getText() {
         return find("body").getText();
     }
-        
-    @SuppressWarnings("unchecked") //this is a convenience so that callers don't have to cast. It's probably a bad idea.
+
+    @SuppressWarnings("unchecked")
+    // this is a convenience so that callers don't have to cast. It's probably a bad idea.
     /**
      * execute a snippet of javascript in an anonymous function.  if your snippet returns a value, Selenium will attempt to cast the most "appropriate"
      * java type (String, Double, Integer, etc)  or a WebElement if you return a DOM node.  
@@ -185,42 +205,44 @@ public abstract class SeleniumWebITCase {
      * contextual <code>arguments</code> object.
      * @return selenium's best approximation of the value returned by your snippet, if it exists.
      */
-    public <T> T executeJavascript(String functionBody, Object...arguments) {
-        JavascriptExecutor executor = (JavascriptExecutor)getDriver();
+    public <T> T executeJavascript(String functionBody, Object... arguments) {
+        JavascriptExecutor executor = (JavascriptExecutor) getDriver();
         Object result = executor.executeScript(functionBody, arguments);
-        return (T)result;
+        return (T) result;
     }
-    
-    //FIXME: implement someday.  the tricky part is supporting nested properties e.g. "elem.style.position", especially when property doesn't exist yet
+
+    // FIXME: implement someday. the tricky part is supporting nested properties e.g. "elem.style.position", especially when property doesn't exist yet
     public void setAttribute(WebElement elem, String property, Object value) {
         throw new RuntimeException("no");
     }
-    
-    public void setStyle(WebElement elem, String property, Object value){
+
+    public void setStyle(WebElement elem, String property, Object value) {
         executeJavascript("arguments[0].style[arguments[1]]=arguments[2]", elem, property, value);
     }
-    
+
     public void setStyle(WebElementSelection selection, String property, Object value) {
-        for(WebElement element : selection) {
+        for (WebElement element : selection) {
             setStyle(element, property, value);
         }
     }
 
     /**
-     * This is a hack that enables selenium to work with the Blueimp jQuery File Upload widget.  Typically in selenium you "upload" a file using 
-     * the sendKeys()  method,  but this will not work when using the fileupload widget because it uses CSS styles to hide the text-entry box, and selenium
+     * This is a hack that enables selenium to work with the Blueimp jQuery File Upload widget. Typically in selenium you "upload" a file using
+     * the sendKeys() method, but this will not work when using the fileupload widget because it uses CSS styles to hide the text-entry box, and selenium
      * will not execute sendkeys() on elements that selenium determines to be invisible to the user.
      */
     public void clearFileInputStyles() {
         WebElement input = find("#fileAsyncUpload").first();
         showAsyncFileInput(input);
     }
-    
+
     /**
-     * This is a hack that enables selenium to work with the Blueimp jQuery File Upload widget.  Typically in selenium you "upload" a file using 
-     * the sendKeys()  method,  but this will not work when using the fileupload widget because it uses CSS styles to hide the text-entry box, and selenium
+     * This is a hack that enables selenium to work with the Blueimp jQuery File Upload widget. Typically in selenium you "upload" a file using
+     * the sendKeys() method, but this will not work when using the fileupload widget because it uses CSS styles to hide the text-entry box, and selenium
      * will not execute sendkeys() on elements that selenium determines to be invisible to the user.
-     * @param input the actual file input element (not the div that renders the jquery file upload widget)
+     * 
+     * @param input
+     *            the actual file input element (not the div that renders the jquery file upload widget)
      */
     public void showAsyncFileInput(WebElement input) {
         setStyle(input, "position", "static");
@@ -232,19 +254,20 @@ public abstract class SeleniumWebITCase {
         setStyle(input, "direction", "ltr");
         setStyle(input, "cursor", "auto");
     }
-    
+
     /**
      * @param fieldName
-     * @return true if fieldname follows struts indexed name convention (e.g.  person[3].id)
+     * @return true if fieldname follows struts indexed name convention (e.g. person[3].id)
      */
     public boolean isIndexedField(String fieldName) {
         String indexedNamePattern = "(.+)\\[(\\d+)\\](\\..+)?";
         return fieldName.matches(indexedNamePattern);
     }
-    
+
     /**
-     * @param fieldName name attribute value of field. which is expected to follow the struts naming pattern for collections
-     * @return 
+     * @param fieldName
+     *            name attribute value of field. which is expected to follow the struts naming pattern for collections
+     * @return
      */
     private WebElement getZerothElement(String fieldName) {
         String indexedNamePattern = "(.+)\\[(\\d+)\\](\\..+)?";
@@ -252,58 +275,63 @@ public abstract class SeleniumWebITCase {
         logger.debug("old name:\t{}", fieldName);
         logger.debug("new name:\t{}", zerothFieldName);
         WebElementSelection selection = find(By.name(zerothFieldName));
-        if(selection.isEmpty()) {
+        if (selection.isEmpty()) {
             return null;
         } else {
             return selection.first();
         }
     }
-    
+
     /**
-     * Return a element with the specified name, which is assumed to follow tDAR's 'repeatable' element convention.  
+     * Return a element with the specified name, which is assumed to follow tDAR's 'repeatable' element convention.
      * If the element doesn't exist, this method tries to locate the 'add another row' button and clicks it until an element with the specified
      * field name exists.
-     * @param fieldName name attribute of the element to find.
+     * 
+     * @param fieldName
+     *            name attribute of the element to find.
      * @return The element with the specified name attribute, or null if this method could neither find the element or implicitly create it.
      */
     public WebElement findOrCreateIndexedField(String fieldName) {
         WebElement result = null;
         WebElementSelection elem = find(By.name(fieldName));
-        if(elem.isEmpty()) {
+        if (elem.isEmpty()) {
             WebElement zeroElem = getZerothElement(fieldName);
-            String repeatLastRowId  = find(zeroElem).parentsWithClass("repeatLastRow").getAttribute("id");
+            String repeatLastRowId = find(zeroElem).parentsWithClass("repeatLastRow").getAttribute("id");
             String buttonSelector = "#" + repeatLastRowId + " + .add-another-control button";
-            //selector for button after container (e.g. "#resourceNotesSection + .add-another-control button")
+            // selector for button after container (e.g. "#resourceNotesSection + .add-another-control button")
             WebElement button = find(buttonSelector).first();
-            //FIXME: create private method that takes indexed fieldname and returns index as int (or -1 if not a valid fieldname)
+            // FIXME: create private method that takes indexed fieldname and returns index as int (or -1 if not a valid fieldname)
             int attempts = clickElementUntil(button, By.name(fieldName), 100);
             result = find(By.name(fieldName)).first();
-        }else {
+        } else {
             result = elem.first();
         }
         return result;
     }
-    
+
     /**
-     * Click an element zero or more times until another element is present on the page as determined by the specified By criteria (for example, clicking a 
+     * Click an element zero or more times until another element is present on the page as determined by the specified By criteria (for example, clicking a
      * tdar "add another item" button until the page generates 10 blank person records)
-     * @param findBy the criteria to use to find a matching element on the page
-     * @param max the maximum number of times this method should click every element if a match has not been found.
-     * @return the number of clicks  performed (per selected item),  or -1 if a matching element was never found.
+     * 
+     * @param findBy
+     *            the criteria to use to find a matching element on the page
+     * @param max
+     *            the maximum number of times this method should click every element if a match has not been found.
+     * @return the number of clicks performed (per selected item), or -1 if a matching element was never found.
      */
     public int clickElementUntil(WebElement element, By findBy, int max) {
         int i = 0;
-        while(find(findBy).size() == 0 && i < max) {
+        while (find(findBy).size() == 0 && i < max) {
             element.click();
             i++;
         }
         return i;
     }
-    
+
     public void loginAdmin() {
         login(TestConstants.ADMIN_USERNAME, TestConstants.ADMIN_PASSWORD);
     }
-    
+
     public void reindex() {
         loginAdmin();
         gotoPage("/admin/searchindex/build");
@@ -311,9 +339,9 @@ public abstract class SeleniumWebITCase {
         waitFor("#spanDone", 120);
         logout();
     }
-    
+
     public void submitForm() {
         find("#submitButton").click();
     }
-    
+
 }
