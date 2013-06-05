8625d1789886de8386c1539922e469c402b6baa9
abrin <abrin@localhost>
2013-06-05 08:41:41 -0700

    more refactoring fixes

--- src/main/java/org/tdar/core/bean/resource/Resource.java
+++ src/main/java/org/tdar/core/bean/resource/Resource.java
@@ -135,7 +135,7 @@ import org.tdar.utils.jaxb.converters.JaxbPersistableConverter;
 @Inheritance(strategy = InheritanceType.JOINED)
 @XmlRootElement
 @XmlSeeAlso({ Document.class, InformationResource.class, Project.class,
-        CodingSheet.class, Dataset.class, Ontology.class, Image.class, SensoryData.class, Video.class })
+        CodingSheet.class, Dataset.class, Ontology.class, Image.class, SensoryData.class, Video.class, Geospatial.class })
 @XmlAccessorType(XmlAccessType.PROPERTY)
 @XmlType(name = "resource")
 @FetchProfiles(value = {
