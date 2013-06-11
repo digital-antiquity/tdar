ee8be7d637f3e0ead5ff5ea9d26a9ce5bcaaa256
Martin Paulo <Martin Paulo@localhost>
2013-06-11 11:24:26 +1000

    Added some comments describing the methods on the interface.

--- src/main/java/org/tdar/db/conversion/analyzers/ColumnAnalyzer.java
+++ src/main/java/org/tdar/db/conversion/analyzers/ColumnAnalyzer.java
@@ -3,7 +3,20 @@ package org.tdar.db.conversion.analyzers;
 import org.tdar.core.bean.resource.datatable.DataTableColumnType;
 
 public interface ColumnAnalyzer {
+
+    /**
+     * @return true if the value argument could be written to a database as the analysers DataTableColumn type,
+     *         false otherwise.
+     */
     public boolean analyze(String value);
+
+    /**
+     * @return The DataTableColumn/SQL type the analyser is trying to detect
+     */
     public DataTableColumnType getType();
+
+    /**
+     * @return The maximum length found, if the SQL type has a variable length, 0 otherwise
+     */
     public int getLength();
 }
