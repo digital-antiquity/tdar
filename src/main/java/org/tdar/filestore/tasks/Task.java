44aae7e95cdb4633ed548ecae7cda3dbbb1361a9
Martin Paulo <Martin Paulo@localhost>
2013-09-02 11:53:53 +1000

    Updated the documentation.

--- src/main/java/org/tdar/filestore/tasks/Task.java
+++ src/main/java/org/tdar/filestore/tasks/Task.java
@@ -30,8 +30,11 @@ public interface Task extends Serializable {
     public WorkflowContext getWorkflowContext();
 
     /**
-     * Run method called by the task workflow process. The implicit contract is that if the task exits abnormally, it will throw an exception during
-     * the execution of this method.
+     * Run method called by the task work flow process. The implicit contract is that if the task exits abnormally, it will throw an exception during
+     * the execution of this method. 
+     * Also, the exceptions are not necessarily logged: so if you want a record of the problem, log error before throwing the
+     * exception.
+     * @see WorkflowContext#setErrorFatal(boolean)
      * @see Workflow#run(WorkflowContext)
      */
     public void run() throws Exception;
