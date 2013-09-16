0f12469221459adf408a0c5b4ba12fa8450850dc
Martin Paulo <Martin Paulo@localhost>
2013-09-16 13:38:28 +1000

    TDAR-3018: Limited the number of files being accepted, and documented reasons.

--- src/main/java/org/tdar/core/service/workflow/workflows/AudioWorkflow.java
+++ src/main/java/org/tdar/core/service/workflow/workflows/AudioWorkflow.java
@@ -3,36 +3,51 @@ package org.tdar.core.service.workflow.workflows;
 import java.util.Collection;
 
 import org.springframework.stereotype.Component;
-import org.tdar.core.bean.resource.ResourceType;
 import org.tdar.core.bean.resource.InformationResourceFile.FileType;
+import org.tdar.core.bean.resource.ResourceType;
 import org.tdar.core.service.workflow.workflows.Workflow.BaseWorkflow;
+import org.tdar.filestore.tasks.ExtractAudioInfoTask;
 
+/**
+ * <p>
+ * The workflow that audio files will follow.
+ * <p>
+ * If we are going to support play back then we need to make sure that there is a mp3 copy. But perhaps we only need to do the conversion when they request the
+ * play back to occur? Another approach would be to simply sample the first 10 seconds, say, and then save that sample for play back.
+ * 
+ * @author Martin Paulo
+ */
 @Component
 public class AudioWorkflow extends BaseWorkflow {
-    
+
     /**
+     * <p>
      * From: http://guides.archaeologydataservice.ac.uk/g2gp/Audio_3
-     * We are probably going to need to add FLAC decoders to the Audio SPI
-     * AudioSystem.getAudioFileTypes() will list the types the system supports. Currently I am expecting wav and aif to be the only two on this list...
+     * <p>
+     * We should support wav, bwf, aif and flac. However, the AudioSystem in java only supports wav, au and aif. So we are probably going to need to add
+     * decoders to the Audio SPI. However, at the moment the request for this work makes no mention of the file types to be supported, so for the time being, we
+     * will only support the ones the AudioSystem supports out of the box. And solicit user feedback.
+     * <p>
+     * <code>AudioSystem.getAudioFileTypes()</code> lists the types of audio files the system supports.
+     * <p>
      * jFLAC is at: http://jflac.sourceforge.net/
-     * If we are going to support play back then we need to make sure that there is a mp3 copy. But perhaps we only need to do the conversion when they
-     * request the play back to occur? Another approach would be to simply sample the first 10 seconds, say, and then save that sample for play back.
      */
-    public static final Collection<String> AUDIO_EXTENSIONS_SUPPORTED = java.util.Arrays.asList(new String[]{"wav", "bwf", "aif", "aiff", "flac"});
-    
+    public static final Collection<String> AUDIO_EXTENSIONS_SUPPORTED = java.util.Arrays.asList(new String[] { "wav", "aif"}); 
+
     public AudioWorkflow() {
-        for (String extension: AUDIO_EXTENSIONS_SUPPORTED) {
+        for (String extension : AUDIO_EXTENSIONS_SUPPORTED) {
             registerFileExtension(extension, ResourceType.AUDIO);
         }
         // what tasks do we want to do with audio?
         // at the very least we should extract some of the meta data and put it into the parent resource.
+        addTask(ExtractAudioInfoTask.class, WorkflowPhase.PRE_PROCESS);
     }
-    
+
     @Override
     public FileType getInformationResourceFileType() {
         return FileType.AUDIO;
     }
-    
+
     // following doesn't seem to be used?
     @Override
     public boolean isEnabled() {
