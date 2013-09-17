0260772964cd8796960d6339c7957157b8d32a66
abrin <abrin@localhost>
2013-09-16 17:38:04 -0700

    smarter search reports

--- src/main/release/support_scripts/activity-report.sh
+++ src/main/release/support_scripts/activity-report.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
-# gives some understanding of what happened the day before by grepping and counting the number of views, searches, etc. from the tdar log files
 GREPDATE=`date -d yesterday +%Y-%m-%d`
 FILE="/var/log/tomcat7/tdar.$GREPDATE.log"
-CREATING=`grep -c "CREATING" ${FILE}`
-RSS=`grep -c  "begin: GET:/search/rss" ${FILE}`
-OAI=`grep -c "begin: GET:/oai-pmh/oai" ${FILE}`
-SAVING=`grep -c "SAVING" ${FILE}`
-SEARCH_DOWNLOAD=`grep -c "begin: GET:/search/download" ${FILE}`
-DELETING=`grep -c "DELETING" ${FILE}`
-SEARCH=`grep -c "SEARCH:" ${FILE}`
-CREATOR=`grep -c "browseCreators:" ${FILE}`
-COLLECTION=`grep -c "browseCollections:" ${FILE}`
-PROJECT=`grep -c "ProjectBrowse:" ${FILE}`
-VIEW=`grep -c "/view?" ${FILE}`
+CREATING=`zgrep -c "CREATING" ${FILE}`
+RSS=`zgrep -c  "begin: GET:/search/rss" ${FILE}`
+OAI=`zgrep -c "begin: GET:/oai-pmh/oai" ${FILE}`
+SAVING=`zgrep -c "SAVING" ${FILE}`
+SEARCH_DOWNLOAD=`zgrep -c "begin: GET:/search/download" ${FILE}`
+DELETING=`zgrep -c "DELETING" ${FILE}`
+SEARCH=`zgrep -c "SEARCH:" ${FILE}`
+SIMPLESEARCH=`zgrep -c "searchType=simple" ${FILE}`/2
+SIMPLESEARCH=`expr ${SIMPLESEARCH} / 2`
+
+ADVANCEDSEARCH=`zgrep -c "searchType=advanced" ${FILE}`/2
+ADVANCEDSEARCH=`expr ${ADVANCEDSEARCH} / 2`
+
+CREATOR=`zgrep -c "browseCreators:" ${FILE}`
+COLLECTION=`zgrep -c "browseCollections:" ${FILE}`
+PROJECT=`zgrep -c "ProjectBrowse:" ${FILE}`
+VIEW=`zgrep -c "/view?" ${FILE}`
 echo "DAY : ${FILE}"
 echo "create: ${CREATING}"
 echo "save: ${SAVING}"
 echo "delete: ${DELETING}"
-echo "searches: ${SEARCH}"
+echo "searches: ${SEARCH} (simple: ${SIMPLESEARCH} ; advanced: ${ADVANCEDSEARCH} )"
 echo "downloaded searches: ${SEARCH_DOWNLOAD}"
 echo "rss: ${RSS}"
 echo "OAI: ${OAI}"
@@ -25,3 +30,5 @@ echo "creator browse: ${CREATOR}"
 echo "collection browse: ${COLLECTION}"
 echo "project browse: ${PROJECT}"
 echo "view: ${VIEW}"
+
+
