2e126141266e56f682ef458e2140a62d3ba721ab
abrin <abrin@localhost>
2013-09-18 17:44:00 -0700

    more work on activity report (downloads + API Controller)

--- src/main/release/support_scripts/activity-report.sh
+++ src/main/release/support_scripts/activity-report.sh
@@ -10,19 +10,31 @@ DELETING=`zgrep -c "DELETING" ${FILE}`
 SEARCH=`zgrep -c "SEARCH:" ${FILE}`
 SIMPLESEARCH=`zgrep -c "searchType=simple" ${FILE}`
 SIMPLESEARCH=`expr ${SIMPLESEARCH} / 2`
+API=`zgrep ":/api/" ${FILE} | grep -c "begin"`
+API_VIEW=`zgrep -c "API VIEWING" ${FILE}`
+API_UPDATE=`zgrep -c "API UPDATED" ${FILE}`
+API_SAVE=`zgrep -c "API CREATED" ${FILE}`
 
 ADVANCEDSEARCH=`zgrep -c "searchType=advanced" ${FILE}`
 ADVANCEDSEARCH=`expr ${ADVANCEDSEARCH} / 2`
+
+DOWNLOADS=`zgrep "downloading" ${FILE} | grep -v "deriv" | wc -l`
 
 CREATOR=`zgrep -c "browseCreators:" ${FILE}`
 COLLECTION=`zgrep -c "browseCollections:" ${FILE}`
 PROJECT=`zgrep -c "ProjectBrowse:" ${FILE}`
 VIEW=`zgrep -c "/view?" ${FILE}`
+
+UNIQUE_USERS=`zgrep "logged in" ${FILE} | awk '/ \- /{print $11}' | sort -u | wc -l`
+USERS=`zgrep "logged in" ${FILE} | awk '/ \- /{print $11}' | wc -l`
+
 echo "DAY : ${FILE}"
+echo "USERS: ${USERS} (unique: ${UNIQUE_USERS})"
+echo "DOWNLOADS: ${DOWNLOADS}"
 echo "create: ${CREATING}"
 echo "save: ${SAVING}"
 echo "delete: ${DELETING}"
-echo "searches: ${SEARCH} (simple: ${SIMPLESEARCH} ; advanced: ${ADVANCEDSEARCH} )"
+echo "searches: ${SEARCH} (simple: ${SIMPLESEARCH} ; advanced: ${ADVANCEDSEARCH})"
 echo "downloaded searches: ${SEARCH_DOWNLOAD}"
 echo "rss: ${RSS}"
 echo "OAI: ${OAI}"
@@ -31,4 +43,4 @@ echo "collection browse: ${COLLECTION}"
 echo "project browse: ${PROJECT}"
 echo "view: ${VIEW}"
 
-
+echo "API: ${API} (view: ${API_VIEW}, create: ${API_SAVE}, update: ${API_UPDATE})"
