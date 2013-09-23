1084ce20c92f855ff9800cc8bd2cdd1d0201b384
Martin Paulo <Martin Paulo@localhost>
2013-09-23 12:39:30 +1000

    TDAR-1978: Made the comments more visible, and added a question...

--- src/main/java/org/tdar/core/bean/Obfuscatable.java
+++ src/main/java/org/tdar/core/bean/Obfuscatable.java
@@ -4,18 +4,21 @@ import java.util.List;
 
 public interface Obfuscatable {
 
-    /*
+    /**
      * check whether the object is obfuscated or not
      */
     public boolean isObfuscated();
 
-    /*
+    /**
      * obfuscates the current object and returns a list of potential objects for further obfuscation
      */
     public List<Obfuscatable> obfuscate();
 
-    /*
-     * mark the object as obfuscated, ideally should be transient
+    /**
+     * mark the object as obfuscated, ideally should be transient.
+     * This does not actually obfuscate the object as implemented by implementations of this interface: it is generally called by 
+     * the implementations of the obfuscate method. Given obfuscate() is a one way street, should this method be removed?
      */
+    @Deprecated
     public void setObfuscated(boolean obfuscated);
 }
