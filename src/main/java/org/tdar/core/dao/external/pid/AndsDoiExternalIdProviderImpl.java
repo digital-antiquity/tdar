5163fd8af6734f145fcb861b1cf1e080e5ffbb0c
Martin Paulo <Martin Paulo@localhost>
2013-09-27 16:49:07 +1000

    Is this the complete ANDS DOI provider? We will find out once I write the integration test to run against their test server.

--- src/main/java/org/tdar/core/dao/external/pid/AndsDoiExternalIdProviderImpl.java
+++ src/main/java/org/tdar/core/dao/external/pid/AndsDoiExternalIdProviderImpl.java
@@ -1,40 +1,49 @@
 package org.tdar.core.dao.external.pid;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.http.auth.AUTH;
 import org.apache.http.client.ClientProtocolException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.tdar.core.bean.entity.ResourceCreator;
+import org.tdar.core.bean.resource.Document;
 import org.tdar.core.bean.resource.Resource;
 import org.tdar.core.configuration.ConfigurationAssistant;
+import org.tdar.core.exception.TdarRecoverableRuntimeException;
+import org.tdar.core.service.processes.DoiProcess;
 
 import au.csiro.doiclient.AndsDoiClient;
 import au.csiro.doiclient.AndsDoiResponse;
 import au.csiro.doiclient.business.AndsDoiIdentity;
 import au.csiro.doiclient.business.DoiDTO;
-import au.csiro.pidclient.business.AndsPidIdentity;
 
 /**
- * DOCUMENTATION: http://andspidclient.sourceforge.net/ and http://services.ands.org.au/documentation/pids/pids_text.pdf
+ * Uses the ANDS "Cite my data" DOI service to mint doi's
+ * Overview: http://ands.org.au/services/cite-my-data.html
+ * FAQ's: http://ands.org.au/cite-data/doi_q_and_a.html
+ * Technical documentation: http://ands.org.au/resource/r9-cite-my-data-v1.1-tech-doco.pdf
+ * Client source code: http://andspidclient.sourceforge.net/
+ * 
  * @author Martin Paulo
  */
 @Service
 public class AndsDoiExternalIdProviderImpl implements ExternalIDProvider {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
-    
+
     private ConfigurationAssistant assistant = new ConfigurationAssistant();
     private boolean isEnabled = true; // the happy case
     private String configIssue;
     private AndsDoiClient doiClient = new AndsDoiClient();
-    private AndsDoiIdentity ourId;  
+    private AndsDoiIdentity ourId;
     private AndsDoiIdentity nullId = new AndsDoiIdentity();
 
     private boolean debug;
-    
+
     public AndsDoiExternalIdProviderImpl() {
         try {
             assistant.loadProperties("andsdoi.properties");
@@ -49,7 +58,6 @@ public class AndsDoiExternalIdProviderImpl implements ExternalIDProvider {
         }
     }
 
-    
     @Override
     public boolean isConfigured() {
         if (isEnabled) {
@@ -80,27 +88,59 @@ public class AndsDoiExternalIdProviderImpl implements ExternalIDProvider {
 
     @Override
     public Map<String, String> create(Resource r, String resourceUrl) throws ClientProtocolException, IOException {
-        DoiDTO doiDTO = new DoiDTO();
-        AndsDoiResponse response = doiClient.mintDOI(resourceUrl, doiDTO , debug);
-        return null;
+        Map<String, String> result = new HashMap<>();
+        AndsDoiResponse response = doiClient.mintDOI(resourceUrl, populateDTO(r), debug);
+        validateResponse("create", response);
+        result.put(DoiProcess.DOI_KEY, response.getDoi());
+        return result;
     }
 
     @Override
     public Map<String, String> getMetadata(String identifier) throws ClientProtocolException, IOException {
-        AndsDoiResponse response = doiClient.requestMetaDataOfDOI(identifier);
-        return null;
+        throw new TdarRecoverableRuntimeException("This method has yet to be writted.");
     }
 
     @Override
     public Map<String, String> modify(Resource r, String resourceUrl, String identifier) throws ClientProtocolException, IOException {
-        DoiDTO doiDTO = new DoiDTO();
-        AndsDoiResponse response = doiClient.updateDOI(identifier, resourceUrl, doiDTO, debug);
-        return null;
+        AndsDoiResponse response = doiClient.updateDOI(identifier, resourceUrl, populateDTO(r), debug);
+        validateResponse("modify", response);
+        return new HashMap<>();
     }
 
     @Override
     public Map<String, String> delete(Resource r, String resourceUrl, String identifier) throws ClientProtocolException, IOException {
-        return null;
+        AndsDoiResponse response = doiClient.deActivateDOI(identifier, debug);
+        validateResponse("deactive", response);
+        return new HashMap<>();
     }
+
+    /**
+     * Simply throws an exception if the operation did not succeed.
+     * @param operation A string describing the operation attempted.
+     * @param response The response received from the server
+     * @throws TdarRecoverableRuntimeException if the operation did not succeed.
+     */
+    @SuppressWarnings("static-method")
+    private void validateResponse(String operation, AndsDoiResponse response) {
+        if (!response.isSuccess()) {
+            throw new TdarRecoverableRuntimeException("Could not " + operation + " doi: " + response.getMessage());
+        }
+    }
+
+    @SuppressWarnings("static-method")
+    private DoiDTO populateDTO(Resource r) {
+        DoiDTO doiDTO = new DoiDTO();
+        java.util.List<String> creatorNames = new ArrayList<>();
+        for (ResourceCreator creator : r.getPrimaryCreators()) {
+            creatorNames.add(creator.getCreator().getName());
+        }
+        doiDTO.setCreators(creatorNames);
+        if (r instanceof Document) {
+            doiDTO.setPublisher(((Document) r).getPublisherName());
+        }
+        doiDTO.setTitle(r.getTitle());
+        return doiDTO;
+    }
+
 
 }
