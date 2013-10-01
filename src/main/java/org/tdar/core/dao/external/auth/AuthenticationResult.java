297adea113837f8752ce7baeb39878f68f8d8766
Martin Paulo <Martin Paulo@localhost>
2013-10-01 15:56:08 +1000

    TDAR-3315: Added missing @Override annotations, removed unneeded serializable declaration and reformatted code.

--- src/main/java/org/tdar/core/dao/external/auth/AuthenticationResult.java
+++ src/main/java/org/tdar/core/dao/external/auth/AuthenticationResult.java
@@ -1,15 +1,16 @@
 package org.tdar.core.dao.external.auth;
 
-import java.io.Serializable;
 
-public enum AuthenticationResult implements Serializable {
+public enum AuthenticationResult  {
+    
     VALID(""),
     INVALID_PASSWORD("Authentication failed.  Please check that your username and password were entered correctly."),
     INACTIVE_ACCOUNT("This account is inactive."),
     ACCOUNT_DOES_NOT_EXIST("This account does not exist"),
     REMOTE_EXCEPTION("The authentication server is currently down.  Please try authenticating again in a few minutes.");
+    
     private final String message;
-    private transient ThreadLocal<Throwable> threadLocalThrowable = new ThreadLocal<Throwable>();
+    private transient ThreadLocal<Throwable> threadLocalThrowable = new ThreadLocal<>();
 
     AuthenticationResult(String message) {
         this.message = message;
@@ -24,6 +25,7 @@ public enum AuthenticationResult implements Serializable {
         return message;
     }
 
+    @Override
     public String toString() {
         Throwable throwable = threadLocalThrowable.get();
         if (throwable == null) {
