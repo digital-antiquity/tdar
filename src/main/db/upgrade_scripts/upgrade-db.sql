282d41ced1927b99eea216c9f12342dce016e7aa
Adam Brin <adam@brin.org>
2013-10-14 15:56:55 -0700

    Merge of stable -->crowd-rest

--- src/main/db/upgrade_scripts/upgrade-db.sql
+++ src/main/db/upgrade_scripts/upgrade-db.sql
@@ -18,18 +18,17 @@ alter table person rename column creator_agreement_level  to creator_agreement_v
 alter table person rename column creator_agreement_version  to contributor_agreement_version;
 
 -- jdevos 10/13/2013: adding some indexes per TDAR-3403.
-create index Table_column_idx on authorized_user(resource_collection_id);
-create index authorized_user_resource_collection_id_idx on authorized_user(user_id);
-create index authorized_user_user_id_idx on bookmarked_resource(person_id);
-create index bookmarked_resource_person_id_idx on bookmarked_resource(resource_id);
-create index category_variable_synonyms_categoryvariable_id_idx on coding_rule(coding_sheet_id);
-create index coding_rule_coding_sheet_id_idx on coding_rule(ontology_node_id);
-create index coding_sheet_category_variable_id_idx on coding_sheet(default_ontology_id);
-create index coding_sheet_default_ontology_id_idx on collection(owner_id);
-create index collection_owner_id_idx on collection(parent_id);
-create index data_table_column_category_variable_id_idx on data_table_column(data_table_id);
-create index data_table_column_data_table_id_idx on data_table_column(default_coding_sheet_id);
-create index data_table_column_default_coding_sheet_id_idx on data_table_column(default_ontology_id);
-create index pos_item_invoice_id_idx on related_comparative_collection(resource_id);
-create index site_type_keyword_parent_id_idx on source_collection(resource_id);
-
+create index authorized_user_resource_collection_id_idx on authorized_user(resource_collection_id);
+create index authorized_user_user_id_idx on authorized_user(user_id);
+create index bookmarked_resource_person_id_idx on bookmarked_resource(person_id);
+create index bookmarked_resource_resource_id_idx on bookmarked_resource(resource_id);
+create index coding_rule_coding_sheet_id_idx on coding_rule(coding_sheet_id);
+create index coding_rule_ontology_node_id_idx on coding_rule(ontology_node_id);
+create index coding_sheet_default_ontology_id_idx on coding_sheet(default_ontology_id);
+create index collection_owner_id_idx on collection(owner_id);
+create index collection_parent_id_idx on collection(parent_id);
+create index data_table_column_data_table_id_idx on data_table_column(data_table_id);
+create index data_table_column_default_coding_sheet_id_idx on data_table_column(default_coding_sheet_id);
+create index data_table_column_default_ontology_id_idx on data_table_column(default_ontology_id);
+create index related_comparative_collection_resource_id_idx on related_comparative_collection(resource_id);
+create index source_collection_resource_id_idx on source_collection(resource_id);
