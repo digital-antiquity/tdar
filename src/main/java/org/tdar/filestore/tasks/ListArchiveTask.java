35b6acbd203a333f4ce88873cb3ef98cc48d7b3a
adam brin <adam.brin@asu.edu>
2013-10-30 09:25:19 -0700

    - adding ignore for import.log (just in case) - using common-compress instead of truezip in places where possible

--- src/main/java/org/tdar/filestore/tasks/ListArchiveTask.java
+++ src/main/java/org/tdar/filestore/tasks/ListArchiveTask.java
@@ -6,14 +6,22 @@
  */
 package org.tdar.filestore.tasks;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveException;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.tdar.core.bean.resource.InformationResourceFileVersion;
 import org.tdar.core.bean.resource.VersionType;
 import org.tdar.core.exception.TdarRecoverableRuntimeException;
 import org.tdar.filestore.tasks.Task.AbstractTask;
+
 
 import de.schlichtherle.truezip.file.TArchiveDetector;
 import de.schlichtherle.truezip.file.TFile;
@@ -56,23 +64,47 @@ public class ListArchiveTask extends AbstractTask {
             // take the file
             getLogger().debug("listing contents of: " + f_.getName());
             File f = new File(getWorkflowContext().getWorkingDirectory(), f_.getName() + ".contents.txt");
+            File f2 = new File(getWorkflowContext().getWorkingDirectory(), f_.getName() + ".index.txt");
             StringBuilder archiveContents = new StringBuilder();
 
-            // list all of the contents
-            // http://blog.msbbc.co.uk/2011/09/java-getting-started-with-truezip-api.html
-            TFile archiveFile = new TFile(f_, getArchiveDetector());
-            if (!archiveFile.isDirectory()) {
-                // logging an error means that this error is most likely never seen
-                throw new TdarRecoverableRuntimeException("Could find files within the archive?" + archiveFile.getName());
+            ArchiveInputStream ais = null;
+            boolean successful = false;
+            try {
+                ArchiveStreamFactory factory = new ArchiveStreamFactory();
+                ais = factory.createArchiveInputStream(new BufferedInputStream(new FileInputStream(f_)));
+                ArchiveEntry entry = ais.getNextEntry();
+                while (entry != null) {
+                    writeToFile(archiveContents, entry.getName());
+                    entry = ais.getNextEntry();
+                }
+                successful = true;
+            } catch (ArchiveException e) {
+                getLogger().warn("could not read archive file",e);
+            } finally {
+                if (ais != null) {
+                    IOUtils.closeQuietly(ais);
+                }
             }
-
-            listFiles(archiveContents, archiveFile, archiveFile);
-
+            
+            if (!successful) {
+                // list all of the contents
+                // http://blog.msbbc.co.uk/2011/09/java-getting-started-with-truezip-api.html
+                TFile archiveFile = new TFile(f_, getArchiveDetector());
+                if (!archiveFile.isDirectory()) {
+                    // logging an error means that this error is most likely never seen
+                    throw new TdarRecoverableRuntimeException("Could find files within the archive:" + archiveFile.getName());
+                }
+    
+                listFiles(archiveContents, archiveFile, archiveFile);
+            }
             // write that to a file with a known format (one file per line)
             FileUtils.writeStringToFile(f, archiveContents.toString());
             InformationResourceFileVersion version_ = generateInformationResourceFileVersionFromOriginal(version, f, VersionType.TRANSLATED);
+            FileUtils.writeStringToFile(f2, archiveContents.toString());
+            InformationResourceFileVersion version2_ = generateInformationResourceFileVersionFromOriginal(version, f2, VersionType.INDEXABLE_TEXT);
             version.setUncompressedSizeOnDisk(getEffectiveSize());
             getWorkflowContext().addVersion(version_);
+            getWorkflowContext().addVersion(version2_);
         }
     }
 
@@ -81,11 +113,15 @@ public class ListArchiveTask extends AbstractTask {
             getLogger().trace(file.getPath());
             setEffectiveSize(getEffectiveSize() + file.length());
             String uri = originalFile.toURI().relativize(file.toURI()).toString();
-            archiveContents.append(uri).append(System.getProperty("line.separator"));
+            writeToFile(archiveContents, uri);
             if (file.isDirectory()) {
                 listFiles(archiveContents, file, originalFile);
             }
         }
+    }
+
+    private void writeToFile(StringBuilder archiveContents, String uri) {
+        archiveContents.append(uri).append(System.getProperty("line.separator"));
     }
 
     /*
