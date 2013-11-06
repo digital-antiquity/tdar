32e02281ef722ff345ebbeaf16c6a0fc3b497e6a
adam brin <adam.brin@asu.edu>
2013-11-06 14:54:09 -0700

    sending only files, not directories

--- src/main/release/support_scripts/backupTdar.sh
+++ src/main/release/support_scripts/backupTdar.sh
@@ -4,7 +4,7 @@ export DAYS=7
 export TMPFILE=/tmp/backup${DATE}.txt
 export BKPLOG=/home/tdar/filestore/logs/backup/${DATE}.log
 export BKPERR=/home/tdar/filestore/logs/backup/${DATE}.err
-find /home/tdar/filestore -mtime -${DAYS} -print > ${TMPFILE}
+find /home/tdar/filestore -mtime -${DAYS} -type f -print > ${TMPFILE}
 tar -cvzf /home/tdar/backups/backup${DATE}.tgz -T ${TMPFILE} 1>${BKPLOG} 2>${BKPERR}
 
 echo "backup for ${DAYS} days before ${DATE}"
