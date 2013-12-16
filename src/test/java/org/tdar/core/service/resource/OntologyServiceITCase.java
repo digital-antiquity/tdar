d1d5627f90a90455c11bc5ecd05f6b2acbe82253
adam brin <adam.brin@asu.edu>
2013-12-16 09:40:49 -0700

    adding test for OntologyNode Parsing and import order

--- src/test/java/org/tdar/core/service/resource/OntologyServiceITCase.java
+++ src/test/java/org/tdar/core/service/resource/OntologyServiceITCase.java
@@ -6,33 +6,29 @@
  */
 package org.tdar.core.service.resource;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
+import java.util.Comparator;
+import java.util.List;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.codehaus.plexus.util.StringInputStream;
+import org.apache.commons.lang.ObjectUtils;
 import org.junit.Test;
-import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.tdar.TestConstants;
+import org.tdar.core.bean.resource.Ontology;
+import org.tdar.core.bean.resource.OntologyNode;
 import org.tdar.core.exception.TdarRecoverableRuntimeException;
-import org.tdar.core.parser.OwlApiHierarchyParser;
 import org.tdar.struts.action.AbstractControllerITCase;
 import org.tdar.struts.action.TdarActionSupport;
 import org.tdar.struts.action.resource.AbstractInformationResourceController;
 import org.tdar.struts.action.resource.OntologyController;
-import org.tdar.utils.TestConfiguration;
 
-import com.hp.hpl.jena.ontology.OntModel;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
+import edu.emory.mathcs.backport.java.util.Collections;
 
 /**
  * @author Adam Brin
@@ -70,16 +66,38 @@ public class OntologyServiceITCase extends AbstractControllerITCase {
     @Test
     @Rollback
     public void testProperParsing() throws Exception {
-        String inputString = FileUtils.readFileToString(new File(TestConstants.TEST_CODING_SHEET_DIR,"fauna-element-ontology.txt"));
-        String owlXml = ontologyService.toOwlXml(1234l, inputString);
-//        ontologyService.
-        logger.info(owlXml);
-        OWLOntologyManager owlOntologyManager = OWLManager.createOWLOntologyManager();
-        OWLOntology ontology = owlOntologyManager.loadOntologyFromOntologyDocument(new StringInputStream(owlXml));  
-        logger.info("{}", ontology);
-        OwlApiHierarchyParser parser = new OwlApiHierarchyParser(ontology);
-        parser.generate();
-        fail("I need a test");
+        OntologyController controller = generateNewInitializedController(OntologyController.class);
+        controller.prepare();
+        controller.getOntology().setTitle("test");
+        controller.getOntology().setDescription("test");
+        controller.setFileInputMethod(AbstractInformationResourceController.FILE_INPUT_METHOD);
+        String ontologyText = IOUtils.toString(new FileInputStream(new File(TestConstants.TEST_CODING_SHEET_DIR, "fauna-element-ontology.txt")));
+        controller.setFileTextInput(ontologyText);
+        controller.setServletRequest(getServletPostRequest());
+        controller.save();
+        Ontology ontology = controller.getOntology();
+        List<OntologyNode> nodes = ontology.getOntologyNodes();
+        Collections.sort(nodes,new Comparator<OntologyNode>() {
+            @Override
+            public int compare(OntologyNode o1, OntologyNode o2) {
+                return ObjectUtils.compare(o1.getImportOrder(), o2.getImportOrder());
+
+            }
+        });
+        for (OntologyNode node : ontology.getOntologyNodes()) {
+            logger.info("{} : {} ({}); {} [{} - {}]", node.getImportOrder(), node.getDisplayName(), node.getId(), node.getIndex(), node.getIntervalStart(), node.getIntervalEnd());
+        }
+        OntologyNode node0 = nodes.get(0);
+        assertEquals("Articulated Skeleton", node0.getDisplayName());
+        OntologyNode node1 = nodes.get(1);
+        assertEquals("Articulated Skeleton Complete", node1.getDisplayName());
+        assertEquals(2, node1.getNumberOfParents());
+        assertEquals(node0.getIndex() + "."+ node1.getIntervalStart(), node1.getIndex());
+        assertEquals("Articulated Skeleton Nearly Complete", nodes.get(2).getDisplayName());
+        assertTrue(nodes.get(2).getSynonyms().contains("ASNC"));
+        assertEquals("Articulated Skeleton Partial", nodes.get(3).getDisplayName());
+        assertEquals("Articulated Skeleton Anterior Portion", nodes.get(4).getDisplayName());
+        assertEquals("Not Recorded", nodes.get(nodes.size() - 1).getDisplayName());
     }
 
     /*
