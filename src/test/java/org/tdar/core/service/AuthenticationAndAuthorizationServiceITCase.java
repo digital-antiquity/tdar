06a147fab2264b8884a7c9ad007c0ece2eccd52c
abrin <abrin@localhost>
2014-01-28 08:27:41 -0700

    adding test for TDAR-3537

--- src/test/java/org/tdar/core/service/AuthenticationAndAuthorizationServiceITCase.java
+++ src/test/java/org/tdar/core/service/AuthenticationAndAuthorizationServiceITCase.java
@@ -6,11 +6,13 @@ import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,21 +21,22 @@ import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallback;
 import org.tdar.core.bean.AbstractIntegrationTestCase;
 import org.tdar.core.bean.AuthNotice;
+import org.tdar.core.bean.Persistable;
 import org.tdar.core.bean.entity.Person;
 import org.tdar.core.bean.resource.Image;
 import org.tdar.core.configuration.TdarConfiguration;
+import org.tdar.core.dao.external.auth.AuthenticationProvider;
+import org.tdar.core.dao.external.auth.CrowdRestDao;
 import org.tdar.core.service.external.AuthenticationAndAuthorizationService;
 import org.tdar.junit.MultipleTdarConfigurationRunner;
 import org.tdar.junit.RunWithTdarConfiguration;
+import org.tdar.struts.action.TdarActionSupport;
+import org.tdar.struts.action.UserAccountController;
 import org.tdar.struts.action.UserAgreementController;
 
-// jtd 9/5:  this doesn't need to be an integration test atm, but I figure we'll eventually want to add tests that
-// need a non-mocked service.
 @RunWith(MultipleTdarConfigurationRunner.class)
 @RunWithTdarConfiguration(runWith = { RunWithTdarConfiguration.TOS_CHANGE })
 public class AuthenticationAndAuthorizationServiceITCase extends AbstractIntegrationTestCase {
-    @Autowired
-    AuthenticationAndAuthorizationService authService;
 
     @Autowired
     GenericService genericService;
@@ -53,14 +56,14 @@ public class AuthenticationAndAuthorizationServiceITCase extends AbstractIntegra
     @Rollback
     public void testUserHasPendingRequirements() throws Exception {
         Person legacyUser = user(false, 0, 0);
-        assertThat(authService.userHasPendingRequirements(legacyUser), is(true));
+        assertThat(authenticationAndAuthorizationService.userHasPendingRequirements(legacyUser), is(true));
 
         Person legacyContributor = user(true, 0, 0);
-        assertThat(authService.userHasPendingRequirements(legacyContributor), is(true));
+        assertThat(authenticationAndAuthorizationService.userHasPendingRequirements(legacyContributor), is(true));
 
         //if user registered after latest version of TOS/CA, they have not pending requirements
         Person newUser = user(false, tosLatestVersion, contributorAgreementLatestVersion);
-        assertThat(authService.userHasPendingRequirements(newUser), is(false));
+        assertThat(authenticationAndAuthorizationService.userHasPendingRequirements(newUser), is(false));
 
     }
 
@@ -68,16 +71,16 @@ public class AuthenticationAndAuthorizationServiceITCase extends AbstractIntegra
     public void testGetUserRequirements() throws Exception {
         //should not meet either requirement
         Person legacyContributor = user(true, 0, 0);
-        List<AuthNotice> requirements = authService.getUserRequirements(legacyContributor);
+        List<AuthNotice> requirements = authenticationAndAuthorizationService.getUserRequirements(legacyContributor);
         assertThat(requirements, hasItems(AuthNotice.TOS_AGREEMENT, AuthNotice.CONTRIBUTOR_AGREEMENT));
 
         //should satisfy all requirements
         Person newUser = user(false, tosLatestVersion, contributorAgreementLatestVersion);
-        assertThat(authService.getUserRequirements(newUser), empty());
+        assertThat(authenticationAndAuthorizationService.getUserRequirements(newUser), empty());
 
         //should satisfy all requirements
         Person newContributor = user(true, tosLatestVersion, contributorAgreementLatestVersion);
-        assertThat(authService.getUserRequirements(newContributor), empty());
+        assertThat(authenticationAndAuthorizationService.getUserRequirements(newContributor), empty());
     }
 
     @Test
@@ -85,11 +88,11 @@ public class AuthenticationAndAuthorizationServiceITCase extends AbstractIntegra
     public void testSatisfyPrerequisite() throws Exception {
         //a contributor that hasn't signed on since updated TOS and creator agreement
         Person contributor = user(true, 0, 0);
-        authService.satisfyPrerequisite(contributor, AuthNotice.TOS_AGREEMENT);
-        assertThat(authService.getUserRequirements(contributor), not(hasItem(AuthNotice.TOS_AGREEMENT)));
+        authenticationAndAuthorizationService.satisfyPrerequisite(contributor, AuthNotice.TOS_AGREEMENT);
+        assertThat(authenticationAndAuthorizationService.getUserRequirements(contributor), not(hasItem(AuthNotice.TOS_AGREEMENT)));
 
-        authService.satisfyPrerequisite(contributor, AuthNotice.CONTRIBUTOR_AGREEMENT);
-        assertThat(authService.getUserRequirements(contributor), not(hasItems(AuthNotice.TOS_AGREEMENT,
+        authenticationAndAuthorizationService.satisfyPrerequisite(contributor, AuthNotice.CONTRIBUTOR_AGREEMENT);
+        assertThat(authenticationAndAuthorizationService.getUserRequirements(contributor), not(hasItems(AuthNotice.TOS_AGREEMENT,
                 AuthNotice.CONTRIBUTOR_AGREEMENT)));
     }
     
@@ -101,20 +104,20 @@ public class AuthenticationAndAuthorizationServiceITCase extends AbstractIntegra
         Person user = getBasicUser();
         user.setContributorAgreementVersion(0);
         init(controller, user);
-        assertThat(authService.getUserRequirements(user), hasItem(AuthNotice.CONTRIBUTOR_AGREEMENT));
+        assertThat(authenticationAndAuthorizationService.getUserRequirements(user), hasItem(AuthNotice.CONTRIBUTOR_AGREEMENT));
         List<AuthNotice> list = new ArrayList<>();
         list.add(AuthNotice.CONTRIBUTOR_AGREEMENT);
         list.add(AuthNotice.TOS_AGREEMENT);
         logger.info("{}", controller.getSessionData());
         logger.info("{}", controller.getSessionData().getPerson());
-        authService.satisfyUserPrerequisites(controller.getSessionData(), list);
-        assertThat(authService.getUserRequirements(user), not(hasItem(AuthNotice.CONTRIBUTOR_AGREEMENT)));
+        authenticationAndAuthorizationService.satisfyUserPrerequisites(controller.getSessionData(), list);
+        assertThat(authenticationAndAuthorizationService.getUserRequirements(user), not(hasItem(AuthNotice.CONTRIBUTOR_AGREEMENT)));
         genericService.synchronize();
         setVerifyTransactionCallback(new TransactionCallback<Image>() {
             @Override
             public Image doInTransaction(TransactionStatus status) {
                 Person user = getBasicUser();
-                assertThat(authService.getUserRequirements(user), not(hasItem(AuthNotice.CONTRIBUTOR_AGREEMENT)));
+                assertThat(authenticationAndAuthorizationService.getUserRequirements(user), not(hasItem(AuthNotice.CONTRIBUTOR_AGREEMENT)));
                 user.setContributorAgreementVersion(0);
                 genericService.saveOrUpdate(user);
                 return null;
@@ -123,4 +126,55 @@ public class AuthenticationAndAuthorizationServiceITCase extends AbstractIntegra
         });
 
     }
+    
+    @Test
+    @Rollback
+    public void testCrowdDisconnected() {
+        // Create a user ... replace crowd witha  "broken crowd" and then 
+        Person person = new Person("Thomas", "Angell", "tangell@pvd.state.ri.us");
+        person.setUsername(person.getEmail());
+        person.setContributor(true);
+        List<AuthenticationProvider> allServices = new ArrayList<AuthenticationProvider>(authenticationAndAuthorizationService.getAllServices());
+        authenticationAndAuthorizationService.getAuthenticationProvider().deleteUser(person);
+        authenticationAndAuthorizationService.getAllServices().clear();
+        Properties crowdProperties = new Properties();
+        crowdProperties.put("application.name","tdar.test");
+        crowdProperties.put("application.password","tdar.test");
+        crowdProperties.put("application.login.url","http://localhost/crowd");
+        crowdProperties.put("crowd.server.url","http://localhost/crowd");
+
+        authenticationAndAuthorizationService.getAllServices().add(new CrowdRestDao(crowdProperties));
+
+
+        String password = "super.secret";
+        UserAccountController controller = generateNewInitializedController(UserAccountController.class);
+
+
+        // create account, making sure the controller knows we're legit.
+        controller.setTimeCheck(System.currentTimeMillis() - 10000);
+        controller.setRequestingContributorAccess(true);
+        controller.setPassword(password);
+        controller.setConfirmPassword(password);
+        controller.setConfirmEmail(person.getEmail());
+        controller.setPerson(person);
+        controller.setServletRequest(getServletPostRequest());
+        controller.setServletResponse(getServletResponse());
+        controller.validate();
+        String execute = null;
+        // technically this is more appropriate -- only call create if validate passes
+        if (CollectionUtils.isEmpty(controller.getActionErrors())) {
+            execute = controller.create();
+        } else {
+            logger.error("errors: {} ", controller.getActionErrors());
+        }
+
+        logger.info("errors: {}", controller.getActionErrors());
+        assertEquals("result is not input :" + execute, execute , TdarActionSupport.INPUT);
+        logger.info("person:{}", person);
+        assertTrue("person should not have an id", Persistable.Base.isTransient(person));
+        authenticationAndAuthorizationService.getAllServices().clear();
+        authenticationAndAuthorizationService.getAllServices().addAll(allServices);
+        setIgnoreActionErrors(true);
+    }
+    
 }
