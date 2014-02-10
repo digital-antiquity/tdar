a10777fe405423b2f55cb6def712208fb7e8124e
adam brin <adam.brin@asu.edu>
2014-02-10 07:26:23 -0700

    pom updates

--- src/main/java/org/tdar/core/dao/StatisticDao.java
+++ src/main/java/org/tdar/core/dao/StatisticDao.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.codehaus.plexus.util.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.hibernate.Query;
 import org.springframework.stereotype.Component;
 import org.tdar.core.bean.resource.ResourceType;
