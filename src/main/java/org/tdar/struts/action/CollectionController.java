022d627d9e9c3a786fdc7cd6d961ccc26f2ff74a
abrin <abrin@localhost>
2014-02-20 15:57:37 -0700

    cleanup

--- src/main/java/org/tdar/struts/action/CollectionController.java
+++ src/main/java/org/tdar/struts/action/CollectionController.java
@@ -3,6 +3,7 @@ package org.tdar.struts.action;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
