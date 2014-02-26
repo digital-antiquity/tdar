38e334e1bbc9cf18121b4fead5de3fa12f71b400
abrin <abrin@localhost>
2014-02-26 13:55:26 -0700

    working on TDAR ID Search

--- src/main/java/org/tdar/search/query/part/GeneralSearchQueryPart.java
+++ src/main/java/org/tdar/search/query/part/GeneralSearchQueryPart.java
@@ -68,6 +68,7 @@ public class GeneralSearchQueryPart extends FieldQueryPart<String> {
             titlePart.setProximity(3);
             descriptionPart.setProximity(4);
         }
+
         primary.append(titlePart.setBoost(TITLE_BOOST));
         primary.append(descriptionPart.setBoost(DESCRIPTION_BOOST));
         primary.append(allFields);
