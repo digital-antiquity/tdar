f2a0746b9ddc33628acda504e23965d2446b3c14
abrin <abrin@localhost>
2014-03-20 15:15:58 -0700

    better logging (testing on alpha)

--- src/main/java/org/tdar/core/dao/entity/AuthorizedUserDao.java
+++ src/main/java/org/tdar/core/dao/entity/AuthorizedUserDao.java
@@ -140,7 +140,7 @@ public class AuthorizedUserDao extends Dao.HibernateBase<AuthorizedUser> {
                 result = CacheResult.FALSE;
             }
         }
-        getLogger().debug("  [{}] checkUserPermissionCache: {} {} [sesion: {}] {}", result, person, permission, currentSession, collectionIds);
+        getLogger().debug("  [{}] checkUserPermissionCache: {} {} [sesion: {}] {}", result, person, permission, currentSession.hashCode(), collectionIds);
         return result;
     }
 
