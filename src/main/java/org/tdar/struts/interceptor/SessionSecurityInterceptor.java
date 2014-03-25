adf2fe49e555d89a1505feb076464d017cc69d4f
abrin <abrin@localhost>
2014-03-24 17:17:22 -0700

    fixing tests + logging

--- src/main/java/org/tdar/struts/interceptor/SessionSecurityInterceptor.java
+++ src/main/java/org/tdar/struts/interceptor/SessionSecurityInterceptor.java
@@ -109,9 +109,9 @@ public class SessionSecurityInterceptor implements SessionDataAware, Interceptor
             return invoke;
         } catch (TdarActionException exception) {
             if (StatusCode.shouldShowException(exception.getStatusCode())) {
-                logger.warn("caught TdarActionException", exception.getMessage());
-            } else {
                 logger.warn("caught TdarActionException ({})", exception.getStatusCode(), exception);
+            } else {
+                logger.warn("caught TdarActionException", exception.getMessage());
             }
             response.setStatus(exception.getStatusCode());
             logger.debug("clearing session due to {} -- returning to {}", exception.getResponseStatusCode(), exception.getResultName());
--- src/test/java/org/tdar/web/functional/BasicSeachSeleniumITCase.java
+++ src/test/java/org/tdar/web/functional/BasicSeachSeleniumITCase.java
@@ -1,20 +1,19 @@
 package org.tdar.web.functional;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.tdar.core.bean.DisplayOrientation;
+import org.openqa.selenium.support.ui.Select;
+import org.tdar.utils.TestConfiguration;
 
 /**
  * Created by jimdevos on 3/12/14.
  */
 public class BasicSeachSeleniumITCase extends AbstractSeleniumWebITCase {
-
-
 
     @Before
     public void setup() {
@@ -23,23 +22,42 @@ public class BasicSeachSeleniumITCase extends AbstractSeleniumWebITCase {
 
     @Test
     public void testBrowse() {
-        gotoPage("/browse");
-        List<String> urls = new ArrayList<>();
-        for (WebElement el : find(By.tagName("a"))) {
-            urls.add(el.getAttribute("href"));
-        }
+        gotoPage("/browse/explore");
+        Set<String> urls = findAndAddUrls();
         for (String url : urls) {
-            gotoPage("/browse");
+            gotoPage("/browse/explore");
             gotoPage(url);
         }
+    }
+
+    private Set<String> findAndAddUrls() {
+        Set<String> urls = new HashSet<>();
+        for (WebElement el : find(By.tagName("a"))) {
+            String url = el.getAttribute("href");
+            if (url.contains(TestConfiguration.getInstance().getHostName())) {
+                urls.add(url);
+            }
+        }
+        return urls;
     }
 
     @Test
     public void testResults() {
         gotoPage("/search/results");
-        List<String> urls = new ArrayList<>();
-        for (WebElement el : find(By.tagName("a"))) {
-            urls.add(el.getAttribute("href"));
+        Set<String> urls = findAndAddUrls();
+        Select sel = new Select(driver.findElement(By.id("recordsPerPage")));
+        int size = sel.getOptions().size();
+        for (int i=0; i < size; i++) {
+            sel = new Select(driver.findElement(By.id("recordsPerPage")));
+            sel.selectByIndex(i);
+            waitForPageload();
+        }
+        sel = new Select(driver.findElement(By.id("sortField")));
+        size = sel.getOptions().size();
+        for (int i=0; i < size; i++) {
+            sel = new Select(driver.findElement(By.id("sortField")));
+            sel.selectByIndex(i);
+            waitForPageload();
         }
         for (String url : urls) {
             gotoPage("/search/results");
