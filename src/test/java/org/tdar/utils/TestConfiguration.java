615dc3e76b417925f87225fe0a4fda9e7a13f7a4
Adam Brin <adam@brin.org>
2014-05-18 17:06:55 -0700

    fixing reindexing bug. Working on Chrome WebDriver support. still issues with:  - consistent autocomplete support  - tree view sometimes erroring out

--- src/test/java/org/tdar/utils/TestConfiguration.java
+++ src/test/java/org/tdar/utils/TestConfiguration.java
@@ -168,7 +168,14 @@ public class TestConfiguration {
     }
 
     public String getChromeDriverPath() {
-        return assistant.getStringProperty("tdar.chromedriver.path", "/Applications/chromedriver");
+        String deflt = "chromedriver";
+        if (isUnix()) {
+            deflt = "/usr/local/bin/chromedriver";
+        }
+        if (isMac()) {
+            deflt = "/Applications/chromedriver";
+        }
+        return assistant.getStringProperty("tdar.chromedriver.path", deflt);
     }
 
     public String getIEDriverPath() {
@@ -183,4 +190,20 @@ public class TestConfiguration {
         return 1;
     }
 
-}
+    private static String OS = System.getProperty("os.name").toLowerCase();
+
+    public static boolean isWindows() {
+        return (OS.indexOf("win") >= 0);
+    }
+ 
+    public static boolean isMac() {
+        return (OS.indexOf("mac") >= 0);
+    }
+ 
+    public static boolean isUnix() {
+        return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
+    }
+ 
+    public static boolean isSolaris() {
+        return (OS.indexOf("sunos") >= 0);
+    }}
