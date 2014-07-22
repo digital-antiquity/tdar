f419a783c23f3c2d83589f9ff1b06774da068b47
Adam Brin <abrin@digitalantiquity.org>
2014-07-22 08:22:31 -0700

    Merge

--- src/main/java/org/tdar/core/service/FreemarkerService.java
+++ src/main/java/org/tdar/core/service/FreemarkerService.java
@@ -1,16 +1,12 @@
 package org.tdar.core.service;
 
-import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.io.FileUtils;
-import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
-import org.tdar.core.configuration.TdarConfiguration;
 import org.tdar.core.dao.FileSystemResourceDao;
 import org.tdar.core.exception.TdarRecoverableRuntimeException;
 
@@ -48,38 +44,6 @@ public class FreemarkerService {
             logger.error("Unable to process template " + templateName, e);
             throw new TdarRecoverableRuntimeException(e);
         }
-    }
-
-    /**
-     * Given a template name and an object model, render the FTL to the string.
-     * 
-     * @param templateName
-     * @param dataModel
-     * @return
-     * @throws IOException
-     */
-    public File renderWithCache(String fileName, String templateName, Object dataModel) throws IOException {
-        File cacheDir = new File(TdarConfiguration.getInstance().getFileCacheDirectory());
-        if (!cacheDir.exists()) {
-            cacheDir.mkdirs();
-        }
-        File cacheFile = new File(cacheDir, fileName);
-        DateTime dateTime = new DateTime(cacheFile.lastModified());
-        if (cacheFile.exists()) {
-            if (dateTime.plusDays(1).isBeforeNow()) {
-                cacheFile.delete();
-            } else {
-                return cacheFile;
-            }
-        }
-        try {
-            FileUtils.writeStringToFile(cacheFile,
-                    FreeMarkerTemplateUtils.processTemplateIntoString(freemarkerConfiguration.getTemplate(templateName), dataModel));
-        } catch (Exception e) {
-            logger.error("Unable to process template " + templateName, e);
-            throw new TdarRecoverableRuntimeException(e);
-        }
-        return cacheFile;
     }
 
 }
