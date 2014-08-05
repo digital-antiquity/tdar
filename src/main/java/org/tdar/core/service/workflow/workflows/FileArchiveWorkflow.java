ff66b4d65593d6e1bb03e2e15b48f8a21dd33ab2
Adam Brin <abrin@digitalantiquity.org>
2014-08-05 07:48:38 -0700

    adding preconditions on kettle extraction script -- only run when archive files are enabled, and only run on archive files... but don't die if we're not

--- src/main/java/org/tdar/core/service/workflow/workflows/FileArchiveWorkflow.java
+++ src/main/java/org/tdar/core/service/workflow/workflows/FileArchiveWorkflow.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.stereotype.Component;
 import org.tdar.core.bean.resource.InformationResourceFile.FileType;
 import org.tdar.core.bean.resource.ResourceType;
+import org.tdar.core.configuration.TdarConfiguration;
 import org.tdar.core.service.workflow.workflows.Workflow.BaseWorkflow;
 import org.tdar.filestore.tasks.IndexableTextExtractionTask;
 import org.tdar.filestore.tasks.ListArchiveTask;
@@ -27,7 +28,9 @@ public class FileArchiveWorkflow extends BaseWorkflow {
         }
 
         addTask(ListArchiveTask.class, WorkflowPhase.PRE_PROCESS);
-        addTask(PrepareArchiveForKettleTask.class, WorkflowPhase.POST_PROCESS);
+        if (TdarConfiguration.getInstance().isArchiveFileEnabled()) {
+            addTask(PrepareArchiveForKettleTask.class, WorkflowPhase.POST_PROCESS);
+        }
         addTask(IndexableTextExtractionTask.class, WorkflowPhase.CREATE_DERIVATIVE);
     }
 
