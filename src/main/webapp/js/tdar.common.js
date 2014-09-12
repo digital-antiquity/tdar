357e647bd7de77bec168da9b7255cb4f8295c8a9
Jim deVos <jim.devos@gmail.com>
2014-09-11 22:16:45 -0700

    forgot we already had a friendly filesize function. Well, what's one more?

--- src/main/webapp/js/tdar.common.js
+++ src/main/webapp/js/tdar.common.js
@@ -1167,6 +1167,21 @@ TDAR.common = function () {
         } while (bytes >= thresh);
         return bytes.toFixed(1) + ' ' + units[u];
     };
+
+    function logarithm(base, n) {
+      return Math.log(n) / Math.log(base);
+    }
+
+    function friendlySize(bytes, si) {
+        var thresh = si ? 1000 : 1024;
+        var units = si ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+        var logbytes = logarithm(thresh, bytes);
+        var idx = Math.floor(logbytes);
+        var base = Math.pow(thresh, idx );
+        var baseUnits = bytes / base;
+        return {amt: baseUnits, unit: units[idx], msg: baseUnits.toFixed(0) + " " + units[idx]};
+    }
+
     
     function _initImageGalleryForView() {
         //init bootstrap image gallery (if found)
