d2ac856c525461c16563ff6980bbdeb8caec2bd0
Jim deVos <james.t.devos@asu.edu>
2014-09-17 14:04:51 -0700

    isShowCollectionResults not needed at abstract level

--- src/main/java/org/tdar/struts/action/search/AdvancedSearchController.java
+++ src/main/java/org/tdar/struts/action/search/AdvancedSearchController.java
@@ -614,8 +614,8 @@ public class AdvancedSearchController extends AbstractLookupController<Resource>
 
         // if refining a search, make sure we inflate any deflated terms
         for (SearchParameters sp : groups) {
-            getLogger().debug("inflating parameters for group {}", sp);
             searchService.inflateSearchParameters(sp);
+            getLogger().debug("inflating parameters for group {}", sp);
         }
 
         return SUCCESS;
@@ -1190,7 +1190,6 @@ public class AdvancedSearchController extends AbstractLookupController<Resource>
      * Hint to view layer: true if it should display collection search results along with resource search results.
      * @return true,  if rendering search results, the list of results is not empty, the collection search box is visible
      */
-    @Override
     public boolean isShowCollectionResults() {
        return getLookupSource() == LookupSource.RESOURCE
                && collectionSearchBoxVisible
