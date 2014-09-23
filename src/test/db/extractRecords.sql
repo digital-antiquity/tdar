27a9612fbd4f79543e1c9c7dc01a3efca0e24414
Adam Brin <abrin@digitalantiquity.org>
2014-09-23 07:17:53 -0700

    fixing init-test

--- src/test/db/extractRecords.sql
+++ src/test/db/extractRecords.sql
@@ -313,3 +313,4 @@ drop table test;
 --DONT-PROCESS-- --update user_info set tos_version = 99, contributor_agreement_version = 99;
 --DONT-PROCESS-- insert into user_notification(date_created, message_key,message_type,display_type) VALUES(now(), 'lithic.announce','SYSTEM_BROADCAST','NORMAL');
 --DONT-PROCESS-- SELECT setval('user_notification_id_seq', (SELECT MAX(id) FROM user_notification)+1);
+--DONT-PROCESS-- update information_resource_file set restriction='EMBARGOED_FIVE_YEARS' where restriction='EMBARGOED';
