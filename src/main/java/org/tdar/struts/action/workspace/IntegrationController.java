0ccdff5483306996d5710ae4264c2eb1f8151911
Adam Brin <abrin@digitalantiquity.org>
2014-11-19 14:46:08 -0700

    removing project list and Collection list, these are way to big and complex to be pulled in natively, they should be ajax queries to the lookup controller

--- src/main/java/org/tdar/struts/action/workspace/IntegrationController.java
+++ src/main/java/org/tdar/struts/action/workspace/IntegrationController.java
@@ -17,7 +17,6 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.tdar.core.bean.collection.ResourceCollection;
 import org.tdar.core.bean.resource.CategoryVariable;
-import org.tdar.core.bean.resource.Project;
 import org.tdar.core.bean.resource.Resource;
 import org.tdar.core.service.GenericService;
 import org.tdar.core.service.ResourceCollectionService;
@@ -45,9 +44,7 @@ import com.opensymphony.xwork2.Preparable;
 public class IntegrationController extends AuthenticationAware.Base implements Preparable {
 
     private static final long serialVersionUID = -2356381511354062946L;
-    private List<Project> allSubmittedProjects = new ArrayList<>();
     private List<Resource> fullUserProjects = new ArrayList<>();
-    private List<ResourceCollection> allCollections = new ArrayList<>();
     private List<ResourceCollection> allResourceCollections = new ArrayList<>();
 
     private Object categoryListJsonObject;
@@ -63,62 +60,62 @@ public class IntegrationController extends AuthenticationAware.Base implements P
     @Autowired
     private GenericService genericService;
 
-//    @Actions({
-//
-//            //list all saved executions for the current user
-//            @Action("list"),
-//
-//            //generate new, transient integration object for editing
-//            @Action(value="add", results={
-//                    @Result(name="success", location="edit.ftl")
-//            }),
-//
-//            //load existing integration object for editing
-//            @Action("edit/{id}"),
-//
-//            //delete integration
-//            //TODO:  this action accepts both GET and POST
-//            @Action(value = "delete/{id}", results = {
-//                    @Result(name = "postonly", location = "delete-confirm.ftl"),
-//                    @Result(name = "success", type = "redirect", location="list")
-//            }),
-//
-//            //find ontologies matching specified filter terms
-//            @Action(value = "ajax/find-ontologies", results = {
-//                    @Result(name="success", type="jsonresult")
-//            }),
-//
-//            //find datasets matching specified filter terms
-//            @Action(value = "ajax/find-datasets", results = {
-//                    @Result(name="success", type="jsonresult")
-//            }),
-//
-//            //return full json for specified data table id's
-//            @Action(value = "ajax/data-table", results = {
-//                    @Result(name="success", type="jsonresult")
-//            }),
-//
-//            //return full json for specified ontology id's
-//            @Action(value = "ajax/ontology", results = {
-//                    @Result(name="success", type="jsonresult")
-//            }),
-//
-//            //return ontology-node participation information for the specified list of dataTable id's
-//            //(similar to data returned to the view in the old '/workspace/filter' action)
-//            @Action(value = "ajax/node-value-stats", results = {
-//                    @Result(name="success", type="jsonresult")
-//            })
-//
-//            //Actions from old WorkspaceController that we intend to keep:
-//            //    @Action(value = "display-filtered-results")
-//            //    @Action(value = "download")
-//
-//
-//
-//
-//
-//
-//    })
+    // @Actions({
+    //
+    // //list all saved executions for the current user
+    // @Action("list"),
+    //
+    // //generate new, transient integration object for editing
+    // @Action(value="add", results={
+    // @Result(name="success", location="edit.ftl")
+    // }),
+    //
+    // //load existing integration object for editing
+    // @Action("edit/{id}"),
+    //
+    // //delete integration
+    // //TODO: this action accepts both GET and POST
+    // @Action(value = "delete/{id}", results = {
+    // @Result(name = "postonly", location = "delete-confirm.ftl"),
+    // @Result(name = "success", type = "redirect", location="list")
+    // }),
+    //
+    // //find ontologies matching specified filter terms
+    // @Action(value = "ajax/find-ontologies", results = {
+    // @Result(name="success", type="jsonresult")
+    // }),
+    //
+    // //find datasets matching specified filter terms
+    // @Action(value = "ajax/find-datasets", results = {
+    // @Result(name="success", type="jsonresult")
+    // }),
+    //
+    // //return full json for specified data table id's
+    // @Action(value = "ajax/data-table", results = {
+    // @Result(name="success", type="jsonresult")
+    // }),
+    //
+    // //return full json for specified ontology id's
+    // @Action(value = "ajax/ontology", results = {
+    // @Result(name="success", type="jsonresult")
+    // }),
+    //
+    // //return ontology-node participation information for the specified list of dataTable id's
+    // //(similar to data returned to the view in the old '/workspace/filter' action)
+    // @Action(value = "ajax/node-value-stats", results = {
+    // @Result(name="success", type="jsonresult")
+    // })
+    //
+    // //Actions from old WorkspaceController that we intend to keep:
+    // // @Action(value = "display-filtered-results")
+    // // @Action(value = "download")
+    //
+    //
+    //
+    //
+    //
+    //
+    // })
 
     @Override
     public void prepare() {
@@ -127,13 +124,12 @@ public class IntegrationController extends AuthenticationAware.Base implements P
         prepareCategories();
     }
 
-
     @Actions({
-            @Action(value="add", results={
-                    @Result(name="success", location="edit.ftl")
+            @Action(value = "add", results = {
+                    @Result(name = "success", location = "edit.ftl")
             }),
-            @Action(value="add-angular", results={
-                    @Result(name="success", location="edit-angular.ftl")
+            @Action(value = "add-angular", results = {
+                    @Result(name = "success", location = "edit-angular.ftl")
             })
     })
     public String execute() {
@@ -145,34 +141,33 @@ public class IntegrationController extends AuthenticationAware.Base implements P
     }
 
     private void prepareProjectStuff() {
-        //FIXME: isAdmin should not be an argument here; ProjectService can derive isAdmin by tapping authservice.
+        // FIXME: isAdmin should not be an argument here; ProjectService can derive isAdmin by tapping authservice.
         fullUserProjects = new ArrayList<>(projectService.findSparseTitleIdProjectListByPerson(getAuthenticatedUser(), false));
         Collections.sort(fullUserProjects);
     }
 
     private void prepareCollections() {
-        //For now,  you just get flattened list of collections. Because.
+        // For now, you just get flattened list of collections. Because.
         allResourceCollections.addAll(resourceCollectionService.findParentOwnerCollections(getAuthenticatedUser()));
     }
 
-
     private void prepareCategories() {
-        //We really just need a flattened list of categories (client will build the hierarchy).
-        //FIXME: make more efficient by ensuring we never lazy-init the parent associations (n+1 select)
-        List<CategoryVariable>  cats = genericService.findAll(CategoryVariable.class);
+        // We really just need a flattened list of categories (client will build the hierarchy).
+        // FIXME: make more efficient by ensuring we never lazy-init the parent associations (n+1 select)
+        List<CategoryVariable> cats = genericService.findAll(CategoryVariable.class);
         Map<Long, String> parentNames = new HashMap<>();
         List<Map<String, Object>> subcats = new ArrayList<>();
 
-        //pass 1: build the parentNames (we assume depth of 2 with no childless parents)
-        for(CategoryVariable cat : cats) {
-            if(cat.getParent() == null) {
+        // pass 1: build the parentNames (we assume depth of 2 with no childless parents)
+        for (CategoryVariable cat : cats) {
+            if (cat.getParent() == null) {
                 parentNames.put(cat.getId(), cat.getName());
             }
         }
 
-        //pass 2: list of subcat maps
-        for(CategoryVariable cat : cats) {
-            if(cat.getParent() != null) {
+        // pass 2: list of subcat maps
+        for (CategoryVariable cat : cats) {
+            if (cat.getParent() != null) {
                 Map<String, Object> map = new HashMap<>();
                 map.put("id", cat.getId());
                 map.put("name", cat.getName());
@@ -211,31 +206,4 @@ public class IntegrationController extends AuthenticationAware.Base implements P
         return json;
     }
 
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
