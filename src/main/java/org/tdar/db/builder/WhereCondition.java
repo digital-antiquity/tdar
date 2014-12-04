92935b0af91c9394ce5d743f89c93b2b1c282062
abrin <abrin@localhost>
2014-12-03 18:32:25 -0700

    Merge with 72543bc79269b87a3b72dc7eeefd9e5b026ecda3

---
+++ src/main/java/org/tdar/db/builder/WhereCondition.java
@@ -0,0 +1,124 @@
+package org.tdar.db.builder;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.collections4.CollectionUtils;
+
+public class WhereCondition extends AbstractSqlTools implements Serializable {
+
+    private static final long serialVersionUID = -5257989550031533775L;
+
+    public enum ValueCondition {
+        EQUALS, IN, IS_NOT, NOT_EQUALS;
+    }
+
+    public enum Condition {
+        AND, OR;
+    }
+
+    private String column;
+    private ValueCondition valueCondition = ValueCondition.EQUALS;
+    private Object value;
+    private Condition condition = Condition.AND;
+    private List<Object> inValues = new ArrayList<>();
+    private boolean includeNullsWithInQuery;
+
+    public WhereCondition(String name) {
+        this.column = name;
+    }
+
+    public String getColumn() {
+        return column;
+    }
+
+    public void setColumn(String column) {
+        this.column = column;
+    }
+
+    public ValueCondition getValueCondition() {
+        return valueCondition;
+    }
+
+    public void setValueCondition(ValueCondition condition) {
+        this.valueCondition = condition;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public List<Object> getInValues() {
+        return inValues;
+    }
+
+    public void setInValues(List<Object> inValues) {
+        this.inValues = inValues;
+    }
+
+    public Condition getCondition() {
+        return condition;
+    }
+
+    public void setCondition(Condition condition) {
+        this.condition = condition;
+    }
+
+    public void setIncludeNulls(boolean b) {
+        this.includeNullsWithInQuery = b;
+    }
+
+    public String toSql() {
+        StringBuilder sb = new StringBuilder(" ");
+        if (includeNullsWithInQuery) {
+            sb.append(" (");
+        }
+        sb.append(quote(column));
+        if (CollectionUtils.isEmpty(inValues)) {
+            buildSimpleCondition(sb);
+        } else {
+            createInPart(sb, getInValues());
+            if (includeNullsWithInQuery) {
+                sb.append(" OR ").append(quote(column)).append(" IS NULL");
+            }
+        }
+        if (includeNullsWithInQuery) {
+            sb.append(") ");
+        }
+
+        return sb.toString();
+    }
+
+    private void buildSimpleCondition(StringBuilder sb) {
+        sb.append(" ");
+        if (getValue() == null) {
+            if (valueCondition == ValueCondition.EQUALS) {
+                sb.append("IS NULL");
+            } else {
+                sb.append("IS NOT NULL");
+            }
+        } else {
+            switch (valueCondition) {
+                case EQUALS:
+                    sb.append("=");
+                    break;
+                case IN:
+                    sb.append(ValueCondition.IN.name());
+                    break;
+                case NOT_EQUALS:
+                    sb.append("!=");
+                    break;
+                default:
+                    break;
+
+            }
+            appendValue(sb, getValue());
+        }
+    }
+
+}
---
+++ src/test/java/org/tdar/struts/action/KeywordActionITCase.java
@@ -0,0 +1,113 @@
+package org.tdar.struts.action;
+
+import static org.junit.Assert.assertEquals;
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import org.tdar.core.bean.keyword.InvestigationType;
+import org.tdar.core.bean.keyword.KeywordType;
+import org.tdar.core.bean.resource.Status;
+import org.tdar.struts.action.search.BrowseKeywordController;
+
+public class KeywordActionITCase extends AbstractDataIntegrationTestCase {
+
+    @Test
+    public void testBasicKeywordAction() {
+        BrowseKeywordController bkc = setupController(3L, KeywordType.CULTURE_KEYWORD, null, false);
+        bkc.view();
+    }
+
+    private BrowseKeywordController setupController(long l, KeywordType cultureKeyword, String slug,boolean expectNotFound) {
+        BrowseKeywordController bkc = generateNewController(BrowseKeywordController.class);
+        init(bkc, null);
+        bkc.setId(l);
+        bkc.setKeywordPath(cultureKeyword.getUrlSuffix());
+        bkc.setKeywordType(cultureKeyword);
+        bkc.setSlug(slug);
+        Exception e = null;
+        try {
+            bkc.prepare();
+        } catch (Exception ex) {
+            e = ex;
+            logger.error("ex",e);
+        }
+        if (expectNotFound) {
+            Assert.assertTrue(e != null);
+            Assert.assertTrue(e instanceof TdarActionException);
+            Assert.assertTrue(e.getMessage().equals("not found"));
+        } else {
+            assertEquals(null, e);
+        }
+        return bkc;
+    }
+
+    @Test
+    public void testKeywordActionInvalidId() {
+        BrowseKeywordController bkc = setupController(1000L, KeywordType.CULTURE_KEYWORD, null, true);
+//        String result = bkc.view();
+//        assertEquals(TdarActionSupport.NOT_FOUND, result);
+    }
+
+    @Test
+    @Rollback(true)
+    public void testKeywordActionStatus() {
+        InvestigationType it = setupTestInvestigationType();
+        String slug = "test-type";
+        BrowseKeywordController bkc = setupController(it.getId(), KeywordType.INVESTIGATION_TYPE, slug, true);
+//        String result = bkc.view();
+//        assertEquals(TdarActionSupport.NOT_FOUND, result);
+    }
+
+    @Test
+    @Rollback(true)
+    public void testKeywordActionStatusDraft() {
+        InvestigationType it = setupTestInvestigationType();
+        String slug = "test-type";
+
+        // change to draft
+        it.setStatus(Status.DRAFT);
+        genericService.saveOrUpdate(it);
+        BrowseKeywordController bkc = setupController(it.getId(), KeywordType.INVESTIGATION_TYPE, slug, true);
+//        String result = bkc.view();
+//        assertEquals(TdarActionSupport.NOT_FOUND, result);
+    }
+
+    @Test
+    @Rollback(true)
+    public void testKeywordActionStatusActive() {
+        InvestigationType it = setupTestInvestigationType();
+        String slug = "test-type";
+
+        // change to draft
+        it.setStatus(Status.ACTIVE);
+        genericService.saveOrUpdate(it);
+        BrowseKeywordController bkc = setupController(it.getId(), KeywordType.INVESTIGATION_TYPE, slug,false);
+        String result = bkc.view();
+        assertEquals(TdarActionSupport.SUCCESS, result);
+    }
+
+    @Test
+    @Rollback(true)
+    public void testKeywordActionBadSlug() {
+        InvestigationType it = setupTestInvestigationType();
+        String slug = "test-type";
+
+        it.setStatus(Status.ACTIVE);
+        genericService.saveOrUpdate(it);
+        BrowseKeywordController bkc = setupController(it.getId(), KeywordType.INVESTIGATION_TYPE, slug, false);
+        bkc = setupController(it.getId(), KeywordType.INVESTIGATION_TYPE, "1234", false);
+        String result = bkc.view();
+        assertEquals(BrowseKeywordController.BAD_SLUG, result);
+    }
+
+    private InvestigationType setupTestInvestigationType() {
+        InvestigationType it = new InvestigationType();
+        it.setDefinition("this is a test");
+        it.setStatus(Status.DELETED);
+        it.setLabel("test type");
+        genericService.save(it);
+        return it;
+    }
+
+}
