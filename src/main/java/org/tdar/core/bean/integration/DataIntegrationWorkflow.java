e8745a0f72cb94d33728f2333377aed81b00b2e3
Jim deVos <james.t.devos@asu.edu>
2014-12-09 09:12:37 -0700

    adding bean fields and stub for test
    PLEASE NOTE:  storing stringified jsondata is kludge. Please do not confuse it for a good idea.  It's meant as  a crutch to get by until  we have service-layer methods  for translating  serialized integration workflow data to json & vice-versa.

--- src/main/java/org/tdar/core/bean/integration/DataIntegrationWorkflow.java
+++ src/main/java/org/tdar/core/bean/integration/DataIntegrationWorkflow.java
@@ -2,6 +2,7 @@ package org.tdar.core.bean.integration;
 
 import org.tdar.core.bean.Persistable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -12,5 +13,38 @@ import javax.persistence.Table;
 @Table(name = "data_integration_workflow")
 public class DataIntegrationWorkflow extends Persistable.Base{
 
+    private static final long serialVersionUID = 0xBADFACE;
 
+    @Column(nullable=false)
+    private String title;
+
+    @Column(nullable=false)
+    private String description;
+
+    @Column(name="json_data")
+    private String jsonData;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getJsonData() {
+        return jsonData;
+    }
+
+    public void setJsonData(String jsonData) {
+        this.jsonData = jsonData;
+    }
 }
