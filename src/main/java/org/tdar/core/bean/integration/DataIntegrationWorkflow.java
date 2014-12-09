9803c4e967e99b9cf507758e3334ddc6d715a2cc
Jim deVos <james.t.devos@asu.edu>
2014-12-08 21:27:20 -0700

    adding ddlxml for data_integration_workflow

---
+++ src/main/java/org/tdar/core/bean/integration/DataIntegrationWorkflow.java
@@ -0,0 +1,16 @@
+package org.tdar.core.bean.integration;
+
+import org.tdar.core.bean.Persistable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by jim on 12/8/14.
+ */
+@Entity
+@Table(name = "data_integration_workflow")
+public class DataIntegrationWorkflow extends Persistable.Base{
+
+
+}
