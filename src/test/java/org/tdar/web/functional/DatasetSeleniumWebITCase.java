dcfff372e7b04da9f868bdb51b1e02ea87116b7d
Adam Brin <abrin@digitalantiquity.org>
2014-12-11 08:59:12 -0700

    using struts "BOM" Bill of Materials like Spring's to simplify POM

--- src/test/java/org/tdar/web/functional/DatasetSeleniumWebITCase.java
+++ src/test/java/org/tdar/web/functional/DatasetSeleniumWebITCase.java
@@ -42,7 +42,7 @@ public class DatasetSeleniumWebITCase extends AbstractBasicSeleniumWebITCase {
     public static Pattern PATTERN_DOCUMENT_VIEW = Pattern.compile(REGEX_DATASET_VIEW);
     public static String REGEX_DATASET_EDIT = ".+\\/dataset\\/\\d+$";
     public static String REGEX_RESOURCE_SAVE = ".+save$";
-    public static String REGEX_DATASET_COLUMNS = ".+\\/dataset\\/\\d+/columns$";
+    public static String REGEX_DATASET_COLUMNS = ".+\\/dataset\\/columns/\\d+$";
     public static Pattern PATTERN_DOCUMENT_EDIT = Pattern.compile(REGEX_DATASET_EDIT);
 
     public DatasetSeleniumWebITCase() {
