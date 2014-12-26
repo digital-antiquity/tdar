d0ad1cd9be7dc5925ff1b1c7b99ff1a919475013
abrin <abrin@localhost>
2014-12-26 11:21:04 -0700

    putting back static classes

--- src/main/java/org/tdar/core/service/integration/dto/v1/IntegrationWorkflowData.java
+++ src/main/java/org/tdar/core/service/integration/dto/v1/IntegrationWorkflowData.java
@@ -2,10 +2,12 @@ package org.tdar.core.service.integration.dto.v1;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.tdar.core.bean.Persistable;
 import org.tdar.core.bean.resource.Dataset;
 import org.tdar.core.bean.resource.Ontology;
 import org.tdar.core.bean.resource.OntologyNode;
@@ -154,4 +156,181 @@ public class IntegrationWorkflowData implements Serializable {
         return errors;
     }
 
+    private static class DatasetDTO implements Serializable, Persistable {
+
+        private static final long serialVersionUID = -7582567713165436710L;
+        private Long id;
+        private String title;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        @Override
+        public List<?> getEqualityFields() {
+            return Arrays.asList(id);
+        }
+    }
+
+    private static class DataTableColumnDTO implements Serializable, Persistable {
+        private static final long serialVersionUID = 1717839026465656147L;
+
+        private Long id;
+        private String name;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String displayName) {
+            this.name = displayName;
+        }
+
+        @Override
+        public List<?> getEqualityFields() {
+            return Arrays.asList(id);
+        }
+    }
+
+    private static class DataTableDTO implements Serializable, Persistable {
+
+        private static final long serialVersionUID = -3269819489102125775L;
+        private Long id;
+        private String displayName;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getDisplayName() {
+            return displayName;
+        }
+
+        public void setDisplayName(String displayName) {
+            this.displayName = displayName;
+        }
+
+        @Override
+        public List<?> getEqualityFields() {
+            return Arrays.asList(id);
+        }
+    }
+
+    private static class IntegrationColumnDTO implements Serializable {
+
+        private static final long serialVersionUID = 9061205188321045416L;
+        private ColumnType type;
+        private List<DataTableColumnDTO> dataTableColumns;
+        private OntologyDTO ontology;
+        private List<OntologyNodeDTO> nodeSelection = new ArrayList<>();
+
+        public List<DataTableColumnDTO> getDataTableColumns() {
+            return dataTableColumns;
+        }
+
+        public void setDataTableColumns(List<DataTableColumnDTO> dataTableColumns) {
+            this.dataTableColumns = dataTableColumns;
+        }
+
+        public OntologyDTO getOntology() {
+            return ontology;
+        }
+
+        public void setOntology(OntologyDTO ontology) {
+            this.ontology = ontology;
+        }
+
+        public List<OntologyNodeDTO> getNodeSelection() {
+            return nodeSelection;
+        }
+
+        public void setNodeSelection(List<OntologyNodeDTO> nodeSelection) {
+            this.nodeSelection = nodeSelection;
+        }
+
+        public ColumnType getType() {
+            return type;
+        }
+
+        public void setType(ColumnType type) {
+            this.type = type;
+        }
+    }
+    
+    private static class OntologyDTO implements Serializable, Persistable {
+
+        private static final long serialVersionUID = -7234646396247780253L;
+        private Long id;
+        private String title;
+        public Long getId() {
+            return id;
+        }
+        public void setId(Long id) {
+            this.id = id;
+        }
+        public String getTitle() {
+            return title;
+        }
+        public void setTitle(String displayName) {
+            this.title = displayName;
+        }
+        
+        @Override
+        public List<?> getEqualityFields() {
+            return Arrays.asList(id);
+        }
+    }
+
+    private static class OntologyNodeDTO implements Serializable, Persistable {
+        private static final long serialVersionUID = 6020897284883456005L;
+
+        private Long id;
+        private String iri;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getIri() {
+            return iri;
+        }
+
+        public void setIri(String iri) {
+            this.iri = iri;
+        }
+        
+        @Override
+        public List<?> getEqualityFields() {
+            return Arrays.asList(id);
+        }
+
+    }
 }
