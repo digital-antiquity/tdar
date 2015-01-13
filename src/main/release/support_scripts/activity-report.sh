44c1b4befafe949bfc5dba9dd668219be92759a2
Adam Brin <abrin@digitalantiquity.org>
2015-01-13 07:49:52 -0700

    updating reporting for munsell

--- src/main/release/support_scripts/activity-report.sh
+++ src/main/release/support_scripts/activity-report.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 GREPDATE=`date -d yesterday +%Y-%m-%d`
 FILE="/var/log/tomcat7/tdar.$GREPDATE.log"
+OFILE="/home/abrin/reports/$GREPDATE.log"
 CREATING=`zgrep -c "CREATING" ${FILE}`
 RSS=`zgrep -c  "begin: GET:/search/rss" ${FILE}`
 OAI=`zgrep -c "begin: GET:/oai-pmh/oai" ${FILE}`
-INTG_SELECT=`zgrep -c "begin: GET:/workspace/select-tables" ${FILE}`
+INTG_SELECT=`zegrep -c "begin: GET:/workspace/(select-tables|integrate)" ${FILE}`
 INTG_COL=`zgrep -c "begin: POST:/workspace/select-columns" ${FILE}`
 INTG_FILTER=`zgrep -c "begin: POST:/workspace/filter" ${FILE}`
 INTG_RESULTS=`zgrep -c "begin: POST:/workspace/display-filtered" ${FILE}`
@@ -30,10 +31,9 @@ DOWNLOADS=`zgrep -i "downloading" ${FILE} | egrep -v "\_(sm|md|lg)\.jpg" | wc -l
 
 CREATOR=`zgrep -c "browseCreators:" ${FILE}`
 COLLECTION=`zgrep -c "browseCollections:" ${FILE}`
-COLLECTION_VIEW=`zgrep -c "/collection/view" ${FILE}`
+COLLECTION_VIEW=`zegrep  "begin: GET:/collection/[0-9]+/.+" ${FILE} | grep -v "/edit" -c `
 PROJECT=`zgrep -c "ProjectBrowse:" ${FILE}`
-VIEW_=`zgrep -c "/view?" ${FILE}`
-#TEST3=$(($TEST - $TEST2))
+VIEW_=`zegrep "begin: GET:/(document|image|dataset|codingSheet|ontology|geospatial|project|sensoryData|audio|archive)/[0-9]+/.+" ${FILE} | grep -v "/edit" -c `
 VIEW=$(($VIEW_ - $COLLECTION_VIEW))
 UNIQUE_USERS=`zgrep "logged in" ${FILE} | awk '/ \- /{print $11}' | sort -u | wc -l`
 USERS=`zgrep "logged in" ${FILE} | awk '/ \- /{print $11}' | wc -l`
