b33711ed7def14ae9c3a89401200c16d8df05704
Adam Brin <abrin@digitalantiquity.org>
2015-02-06 07:27:10 -0700

    removing leading zero in tokenizing of site-codes

--- src/main/java/org/tdar/search/index/analyzer/SiteCodeTokenizingAnalyzer.java
+++ src/main/java/org/tdar/search/index/analyzer/SiteCodeTokenizingAnalyzer.java
@@ -22,7 +22,7 @@ public final class SiteCodeTokenizingAnalyzer extends Analyzer {
      * Treats entire field value as a single Keyword Token
      */
 
-    public static final String SEP = "([\\s\\,\\:\\-])";
+    public static final String SEP = "([\\s\\,\\:\\-]0*)";
     public static final String SEP_OPT = SEP + "?";
     public static final String PATTERN_SMITHSONIAN_ALPHA = "([A-Z][A-Z]\\s)?(([A-Z][A-Z])(" + SEP_OPT + "([A-Z]{1,3}))?" + SEP_OPT + "(\\d+))";
     public static final String PATTERN_SMITHSONIAN_ARIZONA = "([A-Z]{2})" + SEP_OPT + "([A-Z]{1,2})?(" + SEP_OPT + "(\\d+))+" + SEP_OPT + "(\\([A-Z]{2,5}\\))?";
