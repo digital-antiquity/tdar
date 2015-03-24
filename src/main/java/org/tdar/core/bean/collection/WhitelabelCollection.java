4f5cd6e9f232d4a27c6f4bd80ca6442d8b969e38
Jim deVos <james.t.devos@asu.edu>
2015-03-23 17:17:47 -0700

    more work fleshing out WhiteLabelCollection
    I think I figured out the hibernate configuration issues (for  table-per-subclass,  all classes in hierarchy must declare @Inheritance annotation).

--- src/main/java/org/tdar/core/bean/collection/WhitelabelCollection.java
+++ src/main/java/org/tdar/core/bean/collection/WhitelabelCollection.java
@@ -1,9 +1,6 @@
 package org.tdar.core.bean.collection;
 
-import javax.persistence.Entity;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * Created by jimdevos on 3/17/15.
@@ -11,7 +8,65 @@ import javax.persistence.Table;
 @Entity
 @Table(name="whitelabel_collection")
 @Inheritance(strategy = InheritanceType.JOINED)
-public class WhitelabelCollection extends ResourceCollection{
-    
+public class WhiteLabelCollection extends ResourceCollection{
 
+    @Column
+    private boolean customHeaderEnabled;
+
+    @Column
+    private boolean featuredResourcesEnabled;
+
+    @Column
+    private boolean searchEnabled;
+
+    @Column
+    private boolean subCollectionsEnabled;
+
+    @Lob
+    @Column
+    private String css;
+
+    public WhiteLabelCollection() {
+        super(CollectionType.SHARED);
+    }
+
+    public boolean isCustomHeaderEnabled() {
+        return customHeaderEnabled;
+    }
+
+    public void setCustomHeaderEnabled(boolean customHeaderEnabled) {
+        this.customHeaderEnabled = customHeaderEnabled;
+    }
+
+    public boolean isFeaturedResourcesEnabled() {
+        return featuredResourcesEnabled;
+    }
+
+    public void setFeaturedResourcesEnabled(boolean featuredResourcesEnabled) {
+        this.featuredResourcesEnabled = featuredResourcesEnabled;
+    }
+
+    public boolean isSearchEnabled() {
+        return searchEnabled;
+    }
+
+    public void setSearchEnabled(boolean searchEnabled) {
+        this.searchEnabled = searchEnabled;
+    }
+
+    public boolean isSubCollectionsEnabled() {
+        return subCollectionsEnabled;
+    }
+
+    public void setSubCollectionsEnabled(boolean subCollectionsEnabled) {
+        this.subCollectionsEnabled = subCollectionsEnabled;
+    }
+
+    public String getCss() {
+        return css;
+    }
+
+    public void setCss(String css) {
+        this.css = css;
+    }
 }
