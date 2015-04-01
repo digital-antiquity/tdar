b1634c92241283e69efb2c9d909fdb1cd45b1738
Adam Brin <adam@brin.org>
2015-04-01 08:41:19 -0700

    sorting on Key and not value, so we actually are sorting on the labels. fixing for Projects too

---
+++ src/main/java/org/tdar/struts/action/collection/ResourceFacetedAction.java
@@ -0,0 +1,12 @@
+package org.tdar.struts.action.collection;
+
+import java.util.ArrayList;
+
+import org.tdar.search.query.FacetValue;
+
+public interface ResourceFacetedAction {
+
+    ArrayList<FacetValue> getResourceTypeFacets();
+
+    
+}
