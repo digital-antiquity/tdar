31f0cb287972d74c8c62baec5d372e00b54c0258
Adam Brin <abrin@digitalantiquity.org>
2015-04-13 10:03:31 -0700

    trying to catch broken pipes better

--- src/main/java/org/tdar/struts/interceptor/SessionSecurityInterceptor.java
+++ src/main/java/org/tdar/struts/interceptor/SessionSecurityInterceptor.java
@@ -97,10 +97,14 @@ public class SessionSecurityInterceptor implements SessionDataAware, Interceptor
             }
             return resultName;
         } catch (Exception e) {
-            if (e.getClass().getName().equals("org.apache.catalina.connector.ClientAbortException")) {
+            if (e.getClass().getName().equals("org.apache.catalina.connector.ClientAbortException") && e.getMessage().contains("Broken pipe")) {
                 logger.warn("ClientAbortException:{}", e, e);
+                genericService.clearCurrentSession();
+                setSessionClosed(true);
+                return TdarActionSupport.INPUT;
+            } else {
+                throw e;
             }
-            throw e;
         }
     }
 
