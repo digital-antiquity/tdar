f40bd853c2f1df9001814e5c03a9b3c8415d6885
Adam Brin <adam@brin.org>
2015-05-13 15:13:00 -0700

    fixing path

--- src/main/java/org/tdar/dataone/server/ChecksumResponse.java
+++ src/main/java/org/tdar/dataone/server/ChecksumResponse.java
@@ -19,7 +19,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.tdar.dataone.service.DataOneService;
 
-@Path("/checksum")
+@Path("/v1/checksum")
 @Component
 @Scope("prototype")
 public class ChecksumResponse extends AbstractDataOneResponse {
