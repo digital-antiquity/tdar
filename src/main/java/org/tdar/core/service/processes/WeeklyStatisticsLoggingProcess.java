da187c401c6f5a21ee2c1301a77115e02995478d
Adam Brin <adam@brin.org>
2015-06-12 11:29:52 +0000

    Merge - stable

--- src/main/java/org/tdar/core/service/processes/WeeklyStatisticsLoggingProcess.java
+++ src/main/java/org/tdar/core/service/processes/WeeklyStatisticsLoggingProcess.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.tdar.core.bean.collection.WhiteLabelCollection;
 import org.tdar.core.bean.keyword.KeywordType;
 import org.tdar.core.bean.resource.ResourceType;
 import org.tdar.core.bean.statistics.AggregateStatistic;
@@ -79,6 +80,7 @@ public class WeeklyStatisticsLoggingProcess extends AbstractScheduledProcess {
         stats.add(generateStatistics(StatisticType.NUM_USERS, entityService.findAllRegisteredUsers().size(), ""));
         stats.add(generateStatistics(StatisticType.NUM_ACTUAL_CONTRIBUTORS, entityService.findNumberOfActualContributors(), ""));
         stats.add(generateStatistics(StatisticType.NUM_COLLECTIONS, resourceCollectionService.findAllResourceCollections().size(), ""));
+        stats.add(generateStatistics(StatisticType.NUM_COLLECTIONS_WHITE_LABEL, genericService.count(WhiteLabelCollection.class), ""));
         stats.add(generateStatistics(StatisticType.NUM_EMAILS, statisticService.countWeeklyEmails(), ""));
 
         stats.add(generateStatistics(StatisticType.NUM_CULTURE, genericKeywordService.countActiveKeyword(KeywordType.CULTURE_KEYWORD, true), ""));
