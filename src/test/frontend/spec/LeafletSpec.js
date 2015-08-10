5e99bffb3cc9fb392ebd98c972cc4a70d6df8f73
Adam Brin <abrin@digitalantiquity.org>
2015-08-10 11:12:28 -0700

    moving leaflet (again)

--- src/test/frontend/spec/LeafletSpec.js
+++ src/test/frontend/spec/LeafletSpec.js
@@ -21,12 +21,6 @@ describe("LeafletSpec", function() {
         expect(TDAR.leaflet.initialized()).toBeGreaterThan(-1); //fixme: implement this test
         expect($(".leaflet-container")).toHaveLength(1);
         
-        // assert that the map was properly initialized and has the rectangle
-        // assert that the map was not proeprly initialized because it was missing a parameter
-        // assert that an "edit" was initialzied
-        // assert that a change to the map, updated hidden inputs and visa-versa (may not be possible)
-        // assert that the degree translation on the hidden vs. the visible inputs updated properly
-        // assert that a result map has items
      });
 
     it("initLeafletMaps:invalidSetup", function() {
