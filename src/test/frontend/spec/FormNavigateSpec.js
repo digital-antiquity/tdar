67ad8f246faf2dec610568a39843ca04780b4d4b
Jim deVos <james.t.devos@asu.edu>
2015-08-11 21:24:07 -0700

    adding formnavigate tests

---
+++ src/test/frontend/spec/FormNavigateSpec.js
@@ -0,0 +1,41 @@
+describe("form navigate tests", function() {
+
+    function status() {
+        var status = $('form').FormNavigate("status");
+        return status;
+    }
+
+    beforeEach(function(){
+        setFixtures('<form id="frm"><input type="text"><input type="submit" id="btn"></form>');
+        spyOn(window, 'onbeforeunload');
+        document.forms.frm.onsubmit = function(){return false;}
+    });
+
+    it('knows formnavigate status', function() {
+        expect(status()).toBe('not initialized');
+
+        $('form').FormNavigate();
+        expect(status()).toBe('clean')
+
+        $('form input').val('hi').change();
+        expect(status()).toBe('dirty');
+
+        $('form').FormNavigate('clean');
+        expect(status()).toBe('clean');
+
+        $('form input').val('hi').change();
+        var evt = $.Event('beforeunload');
+        $(window).trigger(evt);
+        expect(window.onbeforeunload).toHaveBeenCalled();
+
+        //submit button forces clean to allow the submit
+        $('form input').val('hi').change();
+        $('#btn').click();
+        expect(status()).toBe('clean');
+
+
+
+    });
+
+
+})
