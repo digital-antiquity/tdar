c1a1807df14effb378b2c85a8469c1ec5266718b
Adam Brin <adam@brin.org>
2015-08-17 18:49:59 -0700

    fixing path

--- karma.conf.js
+++ karma.conf.js
@@ -39,7 +39,7 @@ module.exports = function(config) {
                 {pattern: "src/main/webapp/includes/modernizr-custom-2.6.2.min.js", watched: false},
                 {pattern: "src/main/webapp/includes/modernizr-custom-2.6.2.min.js", watched: false},
                 {pattern: "src/main/webapp/includes/jquery.validate-1.13.1/jquery.validate.js", watched: false},
-                {pattern: "/Users/jim/develop/tdar.src/src/main/webapp/includes/bootstrap-2.32/js/bootstrap.js", watched: false}
+                {pattern: "src/main/webapp/includes/bootstrap-2.32/js/bootstrap.js", watched: false}
 
 
             ],
