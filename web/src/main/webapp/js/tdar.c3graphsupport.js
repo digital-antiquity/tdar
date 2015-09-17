84a88f8eea8c0cc40cddbec692deb2ef0b0fd015
Adam Brin <adam@brin.org>
2015-09-17 16:50:06 -0700

    fixing graph links

--- web/src/main/webapp/js/tdar.c3graphsupport.js
+++ web/src/main/webapp/js/tdar.c3graphsupport.js
@@ -4,19 +4,19 @@ TDAR.c3graphsupport = (function(console, $, ctx) {
     var _resourceBarGraphClick = function(d, element) {
         var $parent = $("#resourceBarGraph");
         var source = JSON.parse($($parent.data("source")).html());
-        window.location.href = TDAR.uri() + "/search/results?resourceTypes=" + source[d.x].key;
+        window.location.href = TDAR.uri( "/search/results?resourceTypes=" + source[d.x].key);
     }
 
     var _dashboardResourcePieChartClick = function(d, element) {
         var $parent = $("#resourceTypeChart");
         var source = JSON.parse($($parent.data("columns")).html());
-        window.location.href = TDAR.uri() + "/search/results?useSubmitterContext=true&resourceTypes=" + source[d.index][2];
+        window.location.href = TDAR.uri("/search/results?useSubmitterContext=true&resourceTypes=" + source[d.index][2]);
     }
 
     var _dashboardStatusPieChartClick = function(d, element) {
         var $parent = $("#statusChart");
         var source = JSON.parse($($parent.data("columns")).html());
-        window.location.href = TDAR.uri() + "/search/results?useSubmitterContext=true&status=" + source[d.index][2];
+        window.location.href = TDAR.uri( "/search/results?useSubmitterContext=true&status=" + source[d.index][2]);
     }
     
     return {
