e0f97fa696ffed8fe78fb56fa1424056488ad672
Adam Brin <adam@brin.org>
2015-10-19 19:30:32 -0700

    adding geojson file for geotiffs

--- core/src/main/java/org/tdar/filestore/tasks/GisFileReaderTask.java
+++ core/src/main/java/org/tdar/filestore/tasks/GisFileReaderTask.java
@@ -3,6 +3,7 @@ package org.tdar.filestore.tasks;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Collection;
@@ -14,19 +15,39 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.ArrayUtils;
+import org.geotools.data.DataUtilities;
+import org.geotools.factory.BasicFactories;
+import org.geotools.factory.Hints;
+import org.geotools.feature.DefaultFeatureCollection;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.gce.geotiff.GeoTiffFormat;
 import org.geotools.geojson.feature.FeatureJSON;
+import org.geotools.geometry.jts.JTS;
+import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.kml.KMLConfiguration;
+import org.geotools.referencing.CRS;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.geotools.xml.Configuration;
 import org.geotools.xml.Parser;
 import org.opengis.coverage.grid.Format;
 import org.opengis.coverage.grid.GridCoverage;
 import org.opengis.coverage.grid.GridCoverageReader;
 import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.geometry.Envelope;
+import org.opengis.geometry.Geometry;
+import org.opengis.geometry.primitive.Point;
+import org.opengis.geometry.primitive.PrimitiveFactory;
+import org.opengis.referencing.operation.MathTransform;
 import org.tdar.core.bean.resource.file.InformationResourceFileVersion;
+import org.tdar.core.bean.resource.file.VersionType;
 import org.tdar.filestore.tasks.Task.AbstractTask;
 import org.xml.sax.SAXException;
+
+import com.vividsolutions.jts.geom.Polygon;
+
 
 public class GisFileReaderTask extends AbstractTask {
 
@@ -46,7 +67,7 @@ public class GisFileReaderTask extends AbstractTask {
             FileUtils.copyFileToDirectory(version.getTransientFile(), workingDir);
             version.setTransientFile(new File(workingDir, version.getFilename()));
         }
-        
+
         String extension = FilenameUtils.getExtension(file.getName());
         switch (extension) {
             case "jpg":
@@ -76,9 +97,33 @@ public class GisFileReaderTask extends AbstractTask {
                 getLogger().info("overviews {} ", tiffCov.getNumOverviews());
                 List<GridCoverage> sources = tiffCov.getSources();
 
+                File geoJson = new File(System.getProperty("java.io.tmpdir"), FilenameUtils.getBaseName(file.getName()) + ".json");
+                Hints hints = new Hints( Hints.CRS, DefaultGeographicCRS.WGS84 );
+
                 FeatureJSON fjson = new FeatureJSON();
                 StringWriter writer = new StringWriter();
                 fjson.writeCRS(tiffCov.getCoordinateReferenceSystem(), writer);
+                Envelope env = tiffCov.getEnvelope();
+                BasicFactories factories = new BasicFactories(hints);
+//                PrimitiveFactory primitiveFactory =factories.getPrimitiveFactory(DefaultGeographicCRS.WGS84);
+//                env.getLowerCorner().getCoordinateReferenceSystem();
+                // Transform using 10 sample points around the envelope
+                ReferencedEnvelope re = new ReferencedEnvelope(env);
+                ReferencedEnvelope wgs84Env = re.transform(DefaultGeographicCRS.WGS84, false);
+                Polygon geometry = JTS.toGeometry(re);
+                getLogger().debug("{}", geometry);
+                DefaultFeatureCollection collection = new DefaultFeatureCollection(null, null);
+                SimpleFeatureType type = DataUtilities.createType("location", "geom:Polygon,name:String");
+                final SimpleFeature feature1 = SimpleFeatureBuilder.build(type, new Object[] { geometry, file.getName() }, null);
+                collection.add(feature1);
+                try {
+                    FileWriter fwriter = new FileWriter(geoJson);
+                    fjson.writeFeatureCollection(collection, fwriter);
+                    IOUtils.closeQuietly(fwriter);
+                    addDerivativeFile(getWorkflowContext().getOriginalFiles().get(0), geoJson, VersionType.GEOJSON);
+                } catch (IOException e) {
+                    getLogger().error("could not convert dataset to GeoJSON", e);
+                }
 
                 getLogger().debug(writer.toString());
 
