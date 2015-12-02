624163415567000433f0ade3edb7e357d2bb3fa6
Adam Brin <adam@brin.org>
2015-12-01 20:08:09 -0700

    Merge - default

--- web/src/main/java/org/tdar/struts/interceptor/AuthenticationInterceptor.java
+++ web/src/main/java/org/tdar/struts/interceptor/AuthenticationInterceptor.java
@@ -5,22 +5,16 @@ import java.util.WeakHashMap;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.struts2.ServletActionContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.tdar.core.bean.TdarGroup;
 import org.tdar.core.bean.entity.TdarUser;
-import org.tdar.core.configuration.TdarConfiguration;
 import org.tdar.core.service.GenericService;
 import org.tdar.core.service.ReflectionService;
-import org.tdar.core.service.external.AuthenticationService;
 import org.tdar.core.service.external.AuthorizationService;
 import org.tdar.core.service.external.session.SessionData;
-import org.tdar.core.service.external.session.SessionDataAware;
 import org.tdar.struts.action.TdarActionSupport;
 import org.tdar.struts.interceptor.annotation.HttpForbiddenErrorResponseOnly;
 import org.tdar.struts.interceptor.annotation.HttpNotFoundErrorOnly;
@@ -30,7 +24,6 @@ import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.ActionProxy;
-import com.opensymphony.xwork2.interceptor.Interceptor;
 
 /**
  * $Id$
@@ -44,13 +37,11 @@ import com.opensymphony.xwork2.interceptor.Interceptor;
  * @author <a href='mailto:allen.lee@asu.edu'>Allen Lee</a>
  * @version $Rev$
  */
-public class AuthenticationInterceptor implements SessionDataAware, Interceptor {
+public class AuthenticationInterceptor extends AbstractAuthenticationInterceptor {
 
     private static final long serialVersionUID = -3147151913316273258L;
 
     public static final String SKIP_REDIRECT = "(.*)/(lookup|page-not-found|unauthorized|datatable\\/browse)/(.*)";
-
-    protected final transient Logger logger = LoggerFactory.getLogger(getClass());
 
     private final WeakHashMap<Class<?>, RequiresTdarUserGroup> requiredGroupClassCache = new WeakHashMap<>();
     private final WeakHashMap<Method, RequiresTdarUserGroup> requiredGroupMethodCache = new WeakHashMap<>();
@@ -58,25 +49,14 @@ public class AuthenticationInterceptor implements SessionDataAware, Interceptor 
     @Autowired
     private transient AuthorizationService authorizationService;
     @Autowired
-    private transient AuthenticationService authenticationService;
-    // A Spring AOP session scoped proxy is injected here that retrieves the appropriate SessionData bound to the HTTP Session.
-    private SessionData sessionData;
+    private transient GenericService genericService;
 
-    public AuthorizationService getAuthorizationService() {
-        return authorizationService;
-    }
 
-    public void setAuthorizationService(AuthorizationService authorizationService) {
-        this.authorizationService = authorizationService;
-    }
-
-    @Autowired
-    transient GenericService genericService;
 
     @Override
     public void destroy() {
+        super.destroy();
         authorizationService = null;
-        sessionData = null;
     }
 
     @Override
@@ -119,31 +99,9 @@ public class AuthenticationInterceptor implements SessionDataAware, Interceptor 
             methodName = "execute";
         }
         Method method = action.getClass().getMethod(methodName);
-        Object[] token = (Object[]) ActionContext.getContext().getParameters().get(TdarConfiguration.getInstance().getRequestTokenName());
-        if (sessionData.isAuthenticated() || isValidToken(token)) {
-            // FIXME: consider caching these in a local Map
-            // check for group authorization
-            RequiresTdarUserGroup classLevelRequiresGroupAnnotation = getRequiresTdarUserGroupAnnotation(action.getClass());
-            RequiresTdarUserGroup methodLevelRequiresGroupAnnotation = getRequiresTdarUserGroupAnnotation(method);
-
-            TdarGroup group = TdarGroup.TDAR_USERS;
-            if (methodLevelRequiresGroupAnnotation != null) {
-                group = methodLevelRequiresGroupAnnotation.value();
-            }
-            else if (classLevelRequiresGroupAnnotation != null) {
-                group = classLevelRequiresGroupAnnotation.value();
-            }
-            TdarUser user = genericService.find(TdarUser.class, sessionData.getTdarUserId());
-            if (getAuthorizationService().isMember(user, group)) {
-                // user is authenticated and authorized to perform requested action
-                return invocation.invoke();
-            }
-            logger.debug("unauthorized access to {}/{} from {} with required group {}", action.getClass().getSimpleName(), methodName, user, group);
-            // NOTE, for whatever reason, Struts is not allowing us to swap out the body of the message when we change the http status code
-            // thus we need to use the redirect here to get a tDAR error message. This seems to be an issue specific to the FreemarkerHttpResult and this
-            // interceptor
-            // probably because the action has not been invoked, so we redirect
-            return TdarActionSupport.UNAUTHORIZED_REDIRECT;
+        Object[] token = (Object[]) ActionContext.getContext().getParameters().get(CONFIG.getRequestTokenName());
+        if (sessionData.isAuthenticated() || validateSsoTokenAndAttachUser(token)) {
+            return evaluateRightsOnAction(invocation, sessionData, action, methodName, method);
         }
 
         if (ReflectionService.methodOrActionContainsAnnotation(invocation, HttpForbiddenErrorResponseOnly.class)) {
@@ -157,19 +115,32 @@ public class AuthenticationInterceptor implements SessionDataAware, Interceptor 
         return Action.LOGIN;
     }
 
-    private boolean isValidToken(Object[] token_) {
-        if (ArrayUtils.isEmpty(token_)) {
-            return false;
+    private String evaluateRightsOnAction(ActionInvocation invocation, SessionData sessionData, Object action, String methodName, Method method) throws Exception {
+        // FIXME: consider caching these in a local Map
+        // check for group authorization
+        RequiresTdarUserGroup classLevelRequiresGroupAnnotation = getRequiresTdarUserGroupAnnotation(action.getClass());
+        RequiresTdarUserGroup methodLevelRequiresGroupAnnotation = getRequiresTdarUserGroupAnnotation(method);
+
+        TdarGroup group = TdarGroup.TDAR_USERS;
+        if (methodLevelRequiresGroupAnnotation != null) {
+            group = methodLevelRequiresGroupAnnotation.value();
         }
-        String token = (String) token_[0];
-        if (StringUtils.isNotBlank(token)) {
-            logger.debug("checking valid token: {}", token);
-            boolean result = authenticationService.checkToken((String) token, getSessionData(), ServletActionContext.getRequest()).getType().isValid();
-            logger.debug("token authentication result: {}", result);
-            return result;
+        else if (classLevelRequiresGroupAnnotation != null) {
+            group = classLevelRequiresGroupAnnotation.value();
         }
-        return false;
+        TdarUser user = genericService.find(TdarUser.class, sessionData.getTdarUserId());
+        if (authorizationService.isMember(user, group)) {
+            // user is authenticated and authorized to perform requested action
+            return invocation.invoke();
+        }
+        logger.debug("unauthorized access to {}/{} from {} with required group {}", action.getClass().getSimpleName(), methodName, user, group);
+        // NOTE, for whatever reason, Struts is not allowing us to swap out the body of the message when we change the http status code
+        // thus we need to use the redirect here to get a tDAR error message. This seems to be an issue specific to the FreemarkerHttpResult and this
+        // interceptor
+        // probably because the action has not been invoked, so we redirect
+        return TdarActionSupport.UNAUTHORIZED_REDIRECT;
     }
+
 
     protected void setReturnUrl(ActionInvocation invocation) {
         HttpServletRequest request = ServletActionContext.getRequest();
@@ -183,20 +154,10 @@ public class AuthenticationInterceptor implements SessionDataAware, Interceptor 
             logger.warn("Not setting return url for anything other than a get {}", request.getMethod());
             return;
         }
-        sessionData.setReturnUrl(returnUrl);
-        sessionData.setParameters(invocation.getInvocationContext().getParameters());
-        logger.debug("setting returnUrl to: {}", sessionData.getReturnUrl());
+        getSessionData().setReturnUrl(returnUrl);
+        getSessionData().setParameters(invocation.getInvocationContext().getParameters());
+        logger.debug("setting returnUrl to: {}", getSessionData().getReturnUrl());
 
-    }
-
-    @Override
-    public SessionData getSessionData() {
-        return sessionData;
-    }
-
-    @Override
-    public void setSessionData(SessionData sessionData) {
-        this.sessionData = sessionData;
     }
 
 }
--- web/src/main/java/org/tdar/struts/interceptor/CrowdTokenInterceptor.java
+++ web/src/main/java/org/tdar/struts/interceptor/CrowdTokenInterceptor.java
@@ -1,67 +1,37 @@
 package org.tdar.struts.interceptor;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-//import com.atlassian.crowd.integration.http.HttpAuthenticatorFactory;
-
-import org.apache.struts2.ServletActionContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.tdar.core.service.external.AuthenticationService;
-import org.tdar.core.service.external.session.SessionData;
-import org.tdar.core.service.external.session.SessionDataAware;
-
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionInvocation;
-import com.opensymphony.xwork2.interceptor.Interceptor;
 
 /**
- * Determine if a given request contains a token for a crowd SSO session.  If crowd session does exist, make sure
- * that the corresponding tDAR user is "logged in".
+ * This class is not "Crowd" specific, but, it does use Crowd's general method for handling tokens. It looks for a token and attempts to validate it, and then
+ * set the user on the session.
+ * 
+ * @author abrin
+ *
  */
-public class CrowdTokenInterceptor implements Interceptor, SessionDataAware {
+public class CrowdTokenInterceptor extends AbstractAuthenticationInterceptor {
 
-    @Autowired
-    AuthenticationService authenticationService;
-
-    SessionData sessionData = new SessionData();
-    Logger logger = LoggerFactory.getLogger(getClass());
+    private static final long serialVersionUID = -5229488146705458709L;
 
     @Override
     public void destroy() {
-
+        super.destroy();
     }
 
     @Override
     public void init() {
     }
 
-
-
     @Override
-    public String intercept(ActionInvocation actionInvocation) throws Exception {
-        logger.debug("intercept called");
+    public String intercept(ActionInvocation invocation) throws Exception {
+        // get the token name
+        Object[] token = (Object[]) ActionContext.getContext().getParameters().get(CONFIG.getRequestTokenName());
 
-        //TODO: option 1: get cookie named "crowd.token_key", lookup user and groups from CrowdClient
-        //TODO: option 2: get httprequest instance,  pass to either authenticator.getUser()
-        //TODO: option 3: use deprecated API call via CrowdRestDao.
-
-        ActionContext invocationContext = actionInvocation.getInvocationContext();
-        HttpServletRequest request = (HttpServletRequest) invocationContext.get(ServletActionContext.HTTP_REQUEST);
-        HttpServletResponse response = (HttpServletResponse) invocationContext.get(ServletActionContext.HTTP_RESPONSE);
-
-        logger.debug("request:{}", request);
-        logger.debug("response:{}", response);
-
-        return actionInvocation.invoke();
-    }
-
-    @Override public SessionData getSessionData() {
-        return sessionData;
-    }
-
-    @Override public void setSessionData(SessionData sessionData) {
-        this.sessionData = sessionData;
+        // if not authenticated, check for the token, then validate that
+        if (!getSessionData().isAuthenticated()) {
+            validateSsoTokenAndAttachUser(token);
+        }
+        return invocation.invoke();
     }
 }
