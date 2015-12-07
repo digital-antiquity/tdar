adc6a574679a50aef55e1333f2e44deafa8e6bac
Adam Brin <abrin@digitalantiquity.org>
2015-12-07 10:48:37 -0700

    adding test that asserts the reverse, and cleanup of shared methods

--- core/src/test/java/org/tdar/core/service/resource/OntologyServiceITCase.java
+++ core/src/test/java/org/tdar/core/service/resource/OntologyServiceITCase.java
@@ -55,20 +55,47 @@ public class OntologyServiceITCase extends AbstractIntegrationTestCase {
     }
 
     @Test
-    @Rollback(false)
-    public void testReplaceOntology() throws IOException {
-        Ontology ont = new Ontology();
-        ont.setTitle("test");
-        ont.setDescription("test");
-        ont.setDate(1234);
-        ont.markUpdated(getBasicUser());
-        genericService.saveOrUpdate(ont);
-        String ontologyTextInput = FileUtils.readFileToString(new File(TestConstants.TEST_ONTOLOGY_DIR, "synonym_initial.txt"));
+    @Rollback(true)
+    public void testSplitOntologyNodeSynonymIntoNode() throws IOException {
+        Ontology ont = createOntology();
+        String ontologyTextInput;
+        String owlXml;
+        File file = createOwlFile(ont,"synonym_initial.txt");
+        ont = addFileToResource(ont, file);
+        
+        mapTrivialCodingSheet(ont);
+        
+        OntologyNode dent = ont.getNodeByIri("Dentary");
+        OntologyNode mand = ont.getNodeByIri("Mandible");
+        Long mandId = mand.getId();
+        logger.debug("Dentary {}", dent);
+        logger.debug("Mandible {}", mand);
+        assertNull(dent);
+        genericService.synchronize();
+        logger.debug("nodes:{}", ont.getOntologyNodes());
+        File newFile = createOwlFile(ont,"synonym_as_parent.txt");
+        ont = replaceFileOnResource(ont, newFile, ont.getFirstInformationResourceFile());
+        logger.debug("nodes:{}", ont.getOntologyNodes());
+        ont.clearTransientMaps();
+        dent = ont.getNodeByIri("Dentary");
+        mand = ont.getNodeByIri("Mandible");
+        logger.debug("Dentary: {}", dent);
+        logger.debug("Mandible: {}", mand);
+        assertNotNull(dent);
+        assertNotEquals(dent.getId(), mand.getId());
+        assertEquals(mandId, mand.getId());
+
+    }
+
+    private File createOwlFile(Ontology ont, String name) throws IOException {
+        String ontologyTextInput = FileUtils.readFileToString(new File(TestConstants.TEST_ONTOLOGY_DIR, name));
         String owlXml = ontologyService.toOwlXml(ont.getId(), ontologyTextInput);
         File file = File.createTempFile("test-owl", ".owl");
         FileUtils.write(file , owlXml);
-        ont = addFileToResource(ont, file);
-        
+        return file;
+    }
+
+    private void mapTrivialCodingSheet(Ontology ont) {
         CodingSheet sheet = new CodingSheet();
         sheet.setTitle("test element coding sheet");
         sheet.setDescription(sheet.getTitle());
@@ -82,19 +109,29 @@ public class OntologyServiceITCase extends AbstractIntegrationTestCase {
         rule.setCode("E");
         rule.setOntologyNode(ont.getNodeByIri("Epiotic"));
         genericService.saveOrUpdate(rule);
+    }
+
+    
+    @Test
+    @Rollback(true)
+    public void testJoinOntologyNodeSynonymIntoNode() throws IOException {
+        // assert that the merged node gets dropped in mappings of IDs
+        Ontology ont = createOntology();
+        File file = createOwlFile(ont,"synonym_as_parent.txt");
+        ont = addFileToResource(ont, file);
+        
+        mapTrivialCodingSheet(ont);
         
         OntologyNode dent = ont.getNodeByIri("Dentary");
         OntologyNode mand = ont.getNodeByIri("Mandible");
         Long mandId = mand.getId();
+        Long dentId = dent.getId();
         logger.debug("Dentary {}", dent);
         logger.debug("Mandible {}", mand);
-        assertNull(dent);
+        assertNotNull(dent);
         genericService.synchronize();
         logger.debug("nodes:{}", ont.getOntologyNodes());
-        ontologyTextInput = FileUtils.readFileToString(new File(TestConstants.TEST_ONTOLOGY_DIR, "synonym_as_parent.txt"));
-        owlXml = ontologyService.toOwlXml(ont.getId(), ontologyTextInput);
-        File newFile = File.createTempFile("test-owl-new", ".owl");
-        FileUtils.write(newFile , owlXml);
+        File newFile = createOwlFile(ont,"synonym_initial.txt");
         ont = replaceFileOnResource(ont, newFile, ont.getFirstInformationResourceFile());
         logger.debug("nodes:{}", ont.getOntologyNodes());
         ont.clearTransientMaps();
@@ -102,28 +139,19 @@ public class OntologyServiceITCase extends AbstractIntegrationTestCase {
         mand = ont.getNodeByIri("Mandible");
         logger.debug("Dentary: {}", dent);
         logger.debug("Mandible: {}", mand);
-        assertNotNull(dent);
-        assertNotEquals(dent.getId(), mand.getId());
+        assertNull(dent);
         assertEquals(mandId, mand.getId());
 
-    /**
-     *         logger.debug("nodes:{}", ont.getOntologyNodes());
-        ontologyTextInput = FileUtils.readFileToString(new File(TestConstants.TEST_ONTOLOGY_DIR, "synonym_as_parent.txt"));
-        owlXml = ontologyService.toOwlXml(ont.getId(), ontologyTextInput);
-        File newFile = File.createTempFile("test-owl-new", ".owl");
-        FileUtils.write(newFile , owlXml);
-        InformationResourceFile irf = ont.getFirstInformationResourceFile();
-        ont.getInformationResourceFiles().add(irf);
-        InformationResourceFileVersion version = new InformationResourceFileVersion(VersionType.UPLOADED_ARCHIVAL, newFile.getName(), 2, ont.getId(), irf.getId());
-        version.setDateCreated(new Date());
-        genericService.saveOrUpdate(version);
-        genericService.saveOrUpdate(irf);
-        ontologyService.shred(ont);
-//        ont = replaceFileOnResource(ont, newFile, ont.getFirstInformationResourceFile());
-        logger.debug("nodes:{}", ont.getOntologyNodes());
-        
+    }
 
-     */
+    private Ontology createOntology() {
+        Ontology ont = new Ontology();
+        ont.setTitle("test");
+        ont.setDescription("test");
+        ont.setDate(1234);
+        ont.markUpdated(getBasicUser());
+        genericService.saveOrUpdate(ont);
+        return ont;
     }
 
     
