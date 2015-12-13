28d6be9bb2e7006e7e3dff92c8cc1f61f91f1be2
Adam Brin <adam@brin.org>
2015-12-13 12:12:11 -0700

    allowing for external configurations of solr as needed via a search.properties and setting solr.server.url (url) or solr.server.path (local file path)

--- search/src/main/java/org/tdar/search/config/SolrConfig.java
+++ search/src/main/java/org/tdar/search/config/SolrConfig.java
@@ -5,28 +5,45 @@ import java.nio.file.Path;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.solr.repository.config.EnableSolrRepositories;
 
 @EnableSolrRepositories
 @Configuration
-//@PropertySource("classpath:search.properties")
+@PropertySource(value = SolrConfig.SEARCH_PROPERTIES, ignoreResourceNotFound = true)
+@PropertySource(value = "classpath:" + SolrConfig.SEARCH_PROPERTIES, ignoreResourceNotFound = true)
+@PropertySource(value = "file://${TDAR_CONFIG_PATH}/" + SolrConfig.SEARCH_PROPERTIES, ignoreResourceNotFound = true)
 public class SolrConfig {
+    
+    public static final String SEARCH_PROPERTIES = "search.properties";
     
     @Resource
     private Environment environment;
     
     @Bean
     public SolrClient solrServerFactoryBean() {
-        // alternately https://taidevcouk.wordpress.com/2013/07/01/testing-solr-4-3-x-within-spring-an-update-to-zoominfos-inprocesssolrserver/
+        // more configuration info
+        //https://cwiki.apache.org/confluence/display/solr/Using+SolrJ
+        String solrServerUrl = environment.getProperty("solr.server.url");
+        if (StringUtils.isNotBlank(solrServerUrl)) {
+            return new HttpSolrClient(solrServerUrl);
+        }
+        
+        String solrServerPath = environment.getProperty("solr.server.path");
         Path path = new File("target/classes/solr/").toPath();
-        //environment.getRequiredProperty("solr.solr.home")
-        EmbeddedSolrServer embed = new EmbeddedSolrServer(path, "resources");
-//        embed.getCoreContainer().
-        return embed;
+        if (StringUtils.isNotBlank(solrServerPath)) {
+            File dir = new File(solrServerPath);
+            if (dir.exists()) {
+                path = dir.toPath();
+            }
+        }
+        return new EmbeddedSolrServer(path, "resources");
     }
 }
