cda442937c7c39f38bf7784b23690548ad943056
Jim deVos <james.t.devos@asu.edu>
2016-01-19 12:16:06 -0700

    forgot to remove intentionally broken file upload controller (sorry!!)

--- web/src/main/java/org/tdar/struts/action/UploadController.java
+++ web/src/main/java/org/tdar/struts/action/UploadController.java
@@ -70,12 +70,6 @@ public class UploadController extends AuthenticationAware.Base {
         return SUCCESS;
     }
 
-
-    @Override
-    public void validate() {
-        addActionError("forcing upload to fail");
-    }
-
     @Action(value = "upload",
             interceptorRefs = { @InterceptorRef("editAuthenticatedStack") },
             results = { @Result(name = SUCCESS, type = JSONRESULT, params = { "stream", "jsonInputStream" }),
