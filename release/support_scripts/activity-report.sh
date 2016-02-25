d64f1e82488e55e39b1f6dbb90e11f9cce4bcfd7
Adam Brin <adam@brin.org>
2016-02-25 07:41:18 -0700

    updating activity report

--- release/support_scripts/activity-report.sh
+++ release/support_scripts/activity-report.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+
+#tdar.2015-10-30.log.gz
 GREPDATE=`date -d yesterday +%Y-%m-%d`
-FILE="/var/log/tomcat7/tdar.$GREPDATE.log"
+FILE="/var/log/tomcat7/tdar.$GREPDATE.log.gz"
+OAIFILE="/var/log/tdar-tomcat7/oai-pmh.$GREPDATE.log.gz"
+TAGFILE="/var/log/tdar-tomcat7/tag.$GREPDATE.log.gz"
+DATAONEFILE="/var/log/tdar-tomcat7/dataone.$GREPDATE.log.gz"
 OFILE="/home/abrin/reports/$GREPDATE.log"
 CREATING=`zgrep -c "CREATING" ${FILE}`
 RSS=`zgrep -c  "begin: GET:/search/rss" ${FILE}`
-OAI=`zgrep -c "begin: GET:/oai-pmh/oai" ${FILE}`
+OAI=`zgrep  "> /oai-pmh/oai" ${OAIFILE}  | grep -v -c "nagios" 
+#DATAONE=`zgrep -c "> /dataone/v1"  ${DATAONEFILE} `
 INTG_SELECT=`zegrep -c "begin: GET:/workspace/(select-tables|integrate)" ${FILE}`
 INTG_COL=`zgrep -c "begin: POST:/workspace/select-columns" ${FILE}`
 INTG_FILTER=`zgrep -c "begin: POST:/workspace/filter" ${FILE}`
@@ -14,9 +20,21 @@ SEARCH_DOWNLOAD=`zgrep -c "begin: GET:/search/download" ${FILE}`
 DELETING=`zgrep -c "DELETING" ${FILE}`
 SEARCH=`zgrep -c "SEARCH:" ${FILE}`
 KBROWSE=`zgrep -c "KeywordBrowse:" ${FILE}`
-SIMPLESEARCH=`zgrep -c "searchType=simple" ${FILE}`
-SIMPLESEARCH=`expr ${SIMPLESEARCH} / 2`
-TAG=`zgrep -c "TAGGateway: Called" ${FILE} `
+SIMPLESEARCH=`zegrep -c  "(.+)begin(.+)searchType=simple" ${FILE}`
+if [ -f "$TAGFILE" ]
+then
+    TAG=`zgrep  "TAGGateway: Called" ${TAGFILE} /var/log/tdar-tomcat7/tag.log | grep -c ${GREPDATE} `
+else
+    TAG=`zgrep  "TAGGateway: Called"  /var/log/tdar-tomcat7/tag.log | grep -c ${GREPDATE} `
+fi
+
+if [ -f "$DATAONEFILE" ]
+then
+    DATAONE=`zgrep  ">> /dataone/v1" ${DATAONEFILE} /var/log/tdar-tomcat7/dataone.log | grep -v "nagios" | grep -c ${GREPDATE} `
+else
+    DATAONE=`zgrep  ">> /dataone/v1"  /var/log/tdar-tomcat7/dataone.log | grep -v "nagios" | grep -c ${GREPDATE} `
+fi
+
 API=`zgrep ":/api/" ${FILE} | grep -c "begin"`
 API_VIEW=`zgrep -c "API VIEWING" ${FILE}`
 API_UPDATE=`zgrep -c "API UPDATED" ${FILE}`
@@ -24,8 +42,7 @@ API_SAVE=`zgrep -c "API CREATED" ${FILE}`
 MODS=`zgrep  "/mods" ${FILE} | grep -c "begin" `
 DC=`zgrep  "/dc" ${FILE} | grep -c "begin" `
 BOOKMARKS=`zgrep  "/bookmark" ${FILE} | grep -c 'begin'`
-ADVANCEDSEARCH=`zgrep -c "searchType=advanced" ${FILE}`
-ADVANCEDSEARCH=`expr ${ADVANCEDSEARCH} / 2`
+ADVANCEDSEARCH=`zegrep -c "(.+)begin(.+)searchType=advanced" ${FILE}`
 
 DOWNLOADS=`zgrep -i "downloading" ${FILE} | egrep -v "\_(sm|md|lg)\.jpg" | wc -l`
 
@@ -38,18 +55,20 @@ VIEW=$(($VIEW_ - $COLLECTION_VIEW))
 UNIQUE_USERS=`zgrep "logged in" ${FILE} | awk '/ \- /{print $11}' | sort -u | wc -l`
 USERS=`zgrep "logged in" ${FILE} | awk '/ \- /{print $11}' | wc -l`
 
-echo "DAY : ${FILE}"
-echo "USERS: ${USERS} (unique: ${UNIQUE_USERS})"
-echo "DOWNLOADS: ${DOWNLOADS}"
-echo "create: ${CREATING}"
-echo "save: ${SAVING}"
-echo "delete: ${DELETING}"
-echo "view: ${VIEW} MODS: ${MODS} DC: ${DC} Collection: ${COLLECTION_VIEW}"
-echo "searches: ${SEARCH} (simple: ${SIMPLESEARCH} ; advanced: ${ADVANCEDSEARCH})"
-echo "downloaded searches: ${SEARCH_DOWNLOAD}"
-echo "rss: ${RSS}"
-echo "OAI: ${OAI} TAG: ${TAG}"
-echo "Browse - Collection: ${COLLECTION} Keyword: ${KBROWSE} Project: ${PROJECT} Creator: ${CREATOR}"
-echo "bookmarks: ${BOOKMARKS}"
-echo "Integration Tables: ${INTG_SELECT} Cols: ${INTG_COL} Filter: ${INTG_FILTER} Results: ${INTG_RESULTS}"
-echo "API: ${API} (view: ${API_VIEW}, create: ${API_SAVE}, update: ${API_UPDATE})"
+echo "DAY : ${FILE}" >> $OFILE
+echo "USERS: ${USERS} (unique: ${UNIQUE_USERS})" >> $OFILE
+echo "DOWNLOADS: ${DOWNLOADS}" >> $OFILE
+echo "create: ${CREATING}" >> $OFILE
+echo "save: ${SAVING}" >> $OFILE
+echo "delete: ${DELETING}" >> $OFILE
+echo "view: ${VIEW} MODS: ${MODS} DC: ${DC} Collection: ${COLLECTION_VIEW}" >> $OFILE
+echo "searches: ${SEARCH} (simple: ${SIMPLESEARCH} ; advanced: ${ADVANCEDSEARCH})" >> $OFILE
+echo "downloaded searches: ${SEARCH_DOWNLOAD}" >> $OFILE
+echo "rss: ${RSS}" >> $OFILE
+echo "OAI: ${OAI} TAG: ${TAG} DATAONE: ${DATAONE}" >> $OFILE
+echo "Browse - Collection: ${COLLECTION} Keyword: ${KBROWSE} Project: ${PROJECT} Creator: ${CREATOR}" >> $OFILE
+echo "bookmarks: ${BOOKMARKS}" >> $OFILE
+echo "Integration Tables: ${INTG_SELECT} Cols: ${INTG_COL} Filter: ${INTG_FILTER} Results: ${INTG_RESULTS}" >> $OFILE
+echo "API: ${API} (view: ${API_VIEW}, create: ${API_SAVE}, update: ${API_UPDATE})" >> $OFILE
+cat $OFILE
+
