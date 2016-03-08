92536aed62edbdbe99e3ccde3d014ee2e89cd51c
Adam Brin <abrin@digitalantiquity.org>
2016-03-08 13:02:22 -0700

    little more work on exporting

--- core/src/main/java/org/tdar/core/service/resource/ResourceExportService.java
+++ core/src/main/java/org/tdar/core/service/resource/ResourceExportService.java
@@ -1,8 +1,10 @@
 package org.tdar.core.service.resource;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +17,18 @@ import org.tdar.core.bean.entity.Person;
 import org.tdar.core.bean.entity.ResourceCreator;
 import org.tdar.core.bean.keyword.HierarchicalKeyword;
 import org.tdar.core.bean.keyword.Keyword;
+import org.tdar.core.bean.resource.CodingSheet;
+import org.tdar.core.bean.resource.Dataset;
 import org.tdar.core.bean.resource.InformationResource;
+import org.tdar.core.bean.resource.Ontology;
 import org.tdar.core.bean.resource.Project;
 import org.tdar.core.bean.resource.Resource;
 import org.tdar.core.bean.resource.ResourceAnnotation;
 import org.tdar.core.bean.resource.ResourceAnnotationKey;
 import org.tdar.core.bean.resource.file.InformationResourceFile;
 import org.tdar.core.dao.GenericDao;
+import org.tdar.core.service.SerializationService;
+import org.tdar.utils.PersistableUtils;
 
 @Service
 public class ResourceExportService {
@@ -29,16 +36,51 @@ public class ResourceExportService {
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
-    GenericDao genericDao;
+    private GenericDao genericDao;
+
+    @Autowired
+    private SerializationService serializationService;
+
+    @Transactional
+    public void export(final Resource... resources) {
+//        for (Resource r : resources) {
+//            File dir = new File();
+//            FileUtils.forceMkdir(type);
+//            File dir = new File(type, id.toString());
+//            FileUtils.forceMkdir(dir);
+//            
+//            List<File> files = new ArrayList<>();
+//            if (r instanceof InformationResource && ((InformationResource) r).getProject() != Project.NULL) {
+//                Project p = setupResourceForExport(((InformationResource) r).getProject());
+//                File file = writeToFile(dir, p, "project.xml");
+//            }
+//            Resource r_ = setupResourceForExport(r);
+//
+//        }
+    }
     
+    
+
     @Transactional(readOnly=true)
-    public Resource setupResourceForExport(final Resource resource) {
+    private File writeToFile(File dir, Resource resource,String filename) throws Exception {
+      String convertToXML = serializationService.convertToXML(resource);
+      File type = new File("target/export/" + resource.getResourceType().name());
+      File file = new File(dir, filename);
+
+      FileUtils.writeStringToFile(file, convertToXML);
+        return null;
+    }
+
+
+
+    @Transactional(readOnly = true)
+    public <R extends Resource> R setupResourceForExport(final R resource) {
         genericDao.markReadOnly(resource);
-        
+
         Long id = resource.getId();
         if (id == null) {
             logger.debug("ID NULL: {}", resource);
-            
+
         }
 
         for (Keyword kwd : resource.getAllActiveKeywords()) {
@@ -61,7 +103,6 @@ public class ResourceExportService {
             rc.setResourceIds(null);
             rc.getResources().clear();
         });
-
 
         resource.getActiveRelatedComparativeCollections().forEach(cc -> clearId(cc));
         resource.getActiveSourceCollections().forEach(cc -> clearId(cc));
@@ -90,10 +131,30 @@ public class ResourceExportService {
             }
             ir.getInformationResourceFiles().clear();
             ir.setFileProxies(proxies);
-            if (ir.getProject() != Project.NULL) {
-                genericDao.markReadOnly(ir.getProject());
-                clearId(ir.getProject());
+
+            if (PersistableUtils.isNotNullOrTransient(ir.getProjectId())) {
+                ir.setProject(new Project(ir.getProjectId(), null));
+                ir.setMappedDataKeyColumn(null);
             }
+
+            if (resource instanceof Dataset) {
+                Dataset dataset = (Dataset) resource;
+                dataset.setDataTables(null);
+                dataset.setRelationships(null);
+            }
+
+            if (resource instanceof CodingSheet) {
+                CodingSheet codingSheet = (CodingSheet) resource;
+                codingSheet.setCodingRules(null);
+                codingSheet.setAssociatedDataTableColumns(null);
+                codingSheet.setDefaultOntology(null);
+            }
+
+            if (resource instanceof Ontology) {
+                ((Ontology) resource).setOntologyNodes(null);
+            }
+
+        
         }
 
         resource.setId(null);
@@ -103,7 +164,7 @@ public class ResourceExportService {
     public void clearId(Persistable p) {
         genericDao.markReadOnly(p);
         p.setId(null);
-        
+
     }
 
     private void nullifyCreator(Creator creator) {
@@ -118,5 +179,5 @@ public class ResourceExportService {
             }
         }
     }
-    
+
 }
