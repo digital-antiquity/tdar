4582b41e98e9155d0c142628f897de908a2b7cf3
Adam Brin <adam@brin.org>
2016-04-09 15:10:36 -0700

    reverting

--- core/src/main/java/org/tdar/core/configuration/IntegrationAppConfiguration.java
+++ core/src/main/java/org/tdar/core/configuration/IntegrationAppConfiguration.java
@@ -12,8 +12,8 @@ public class IntegrationAppConfiguration extends SimpleAppConfiguration {
 
 	private static final long serialVersionUID = 6361299741688277624L;
 
-//	@Bean
-//	@Qualifier("tdarDataTx")
+	@Bean
+	@Qualifier("tdarDataTx")
 	public DataSourceTransactionManager dataTransactionManager(
 			@Qualifier("tdarDataImportDataSource") DataSource dataSource) throws PropertyVetoException {
 		DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager(dataSource);
