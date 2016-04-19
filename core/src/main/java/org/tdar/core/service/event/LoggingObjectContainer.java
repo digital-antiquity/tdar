17036ae18cfbe747039e8eae365a31daddb527bd
abrin <adam@brin.org>
2016-04-19 15:57:11 -0700

    adding Logging Object Container

---
+++ core/src/main/java/org/tdar/core/service/event/LoggingObjectContainer.java
@@ -0,0 +1,39 @@
+package org.tdar.core.service.event;
+
+import java.io.File;
+
+import org.tdar.core.event.EventType;
+import org.tdar.filestore.FilestoreObjectType;
+
+public class LoggingObjectContainer extends ObjectContainer<File> {
+
+	private FilestoreObjectType filestoreObjectType;
+	private Long persistableId;
+
+	public LoggingObjectContainer(File doc, String id, EventType type, FilestoreObjectType filestoreObjectType, Long long1) {
+		super(doc, id, type);
+		this.setPersistableId(long1);
+		this.setFilestoreObjectType(filestoreObjectType);
+	}
+
+	@Override
+	public String getType() {
+		return "LOG";
+	}
+
+	public FilestoreObjectType getFilestoreObjectType() {
+		return filestoreObjectType;
+	}
+
+	public void setFilestoreObjectType(FilestoreObjectType filestoreObjectType) {
+		this.filestoreObjectType = filestoreObjectType;
+	}
+
+	public Long getPersistableId() {
+		return persistableId;
+	}
+
+	public void setPersistableId(Long persistableId) {
+		this.persistableId = persistableId;
+	}
+}
