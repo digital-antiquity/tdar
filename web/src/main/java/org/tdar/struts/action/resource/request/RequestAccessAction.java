328ab933696fef7d534aaa6ac2957e012e184287
Adam Brin <abrin@digitalantiquity.org>
2016-04-20 07:04:32 -0700

    adding some null-checks

--- web/src/main/java/org/tdar/struts/action/resource/request/RequestAccessAction.java
+++ web/src/main/java/org/tdar/struts/action/resource/request/RequestAccessAction.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
@@ -45,133 +46,140 @@ import com.opensymphony.xwork2.Preparable;
 @Component
 @Scope("prototype")
 public class RequestAccessAction extends AuthenticationAware.Base
-		implements Preparable, PersistableLoadingAction<Resource> {
+        implements Preparable, PersistableLoadingAction<Resource> {
 
-	@Autowired
-	private transient RecaptchaService recaptchaService;
-	private AntiSpamHelper h = new AntiSpamHelper();
-	private Set<EmailMessageType> emailTypes = new HashSet<>(EmailMessageType.valuesWithoutConfidentialFiles());
-	private List<ResourceCreatorProxy> proxies = new ArrayList<>();
+    @Autowired
+    private transient RecaptchaService recaptchaService;
+    private AntiSpamHelper h = new AntiSpamHelper();
+    private Set<EmailMessageType> emailTypes = new HashSet<>(EmailMessageType.valuesWithoutConfidentialFiles());
+    private List<ResourceCreatorProxy> proxies = new ArrayList<>();
 
-	@Autowired
-	private transient ObfuscationService obfuscationService;
-	private static final String SUCCESS_UNAUTH = "success-unauth";
-	private static final long serialVersionUID = -6110216327414755768L;
-	private Long id;
-	private Resource resource;
+    @Autowired
+    private transient ObfuscationService obfuscationService;
+    private static final String SUCCESS_UNAUTH = "success-unauth";
+    private static final long serialVersionUID = -6110216327414755768L;
+    private Long id;
+    private Resource resource;
     private EmailMessageType type = EmailMessageType.CONTACT;
 
-	@Override
-	public void prepare() throws Exception {
-		getLogger().debug("id: {}, {}", getId(), getPersistableClass());
-		prepareAndLoad(this, RequestType.VIEW);
+    @Override
+    public void prepare() throws Exception {
+        getLogger().debug("id: {}, {}", getId(), getPersistableClass());
+        prepareAndLoad(this, RequestType.VIEW);
 
-		// this may be duplicative... check
-		for (ResourceCreator rc : resource.getActiveResourceCreators()) {
-			if (getTdarConfiguration().obfuscationInterceptorDisabled()) {
-				if ((rc.getCreatorType() == CreatorType.PERSON) && !isAuthenticated()) {
-					obfuscationService.obfuscate(rc.getCreator(), getAuthenticatedUser());
-				}
-			}
+        if (PersistableUtils.isNullOrTransient(getResource())) {
+            return;
+        }
+        // this may be duplicative... check
+        if (CollectionUtils.isNotEmpty(resource.getActiveResourceCreators())) {
+            for (ResourceCreator rc : resource.getActiveResourceCreators()) {
+                if (getTdarConfiguration().obfuscationInterceptorDisabled()) {
+                    if ((rc.getCreatorType() == CreatorType.PERSON) && !isAuthenticated()) {
+                        obfuscationService.obfuscate(rc.getCreator(), getAuthenticatedUser());
+                    }
+                }
 
-			ResourceCreatorProxy proxy = new ResourceCreatorProxy(rc);
-			if (proxy.isValidEmailContact()) {
-				proxies.add(proxy);
-			}
-		}
+                ResourceCreatorProxy proxy = new ResourceCreatorProxy(rc);
+                if (proxy.isValidEmailContact()) {
+                    proxies.add(proxy);
+                }
+            }
+        }
 
-		if (getResource() instanceof InformationResource) {
-			InformationResource informationResource = (InformationResource) getResource();
-			if (informationResource.hasConfidentialFiles()) {
-				emailTypes = new HashSet<>(EmailMessageType.valuesWithoutSAA());
-			}
-		}
-		// only add the SAA option if ...
-		resource.getResourceCollections().forEach(c -> {
-			if (TdarConfiguration.getInstance().getSaaCollectionIds().contains(c.getId())) {
-				emailTypes.add(EmailMessageType.SAA);
-			}
-		});
+        if (getResource() instanceof InformationResource) {
+            InformationResource informationResource = (InformationResource) getResource();
+            if (informationResource.hasConfidentialFiles()) {
+                emailTypes = new HashSet<>(EmailMessageType.valuesWithoutSAA());
+            }
+        }
+        // only add the SAA option if ...
+        if (CollectionUtils.isNotEmpty(resource.getResourceCollections())) {
+            resource.getResourceCollections().forEach(c -> {
+                if (TdarConfiguration.getInstance().getSaaCollectionIds().contains(c.getId())) {
+                    emailTypes.add(EmailMessageType.SAA);
+                }
+            });
+        }
 
-	}
+    }
 
-	@Action(value = "{id}", results = { @Result(name = SUCCESS, location = "request-access.ftl"),
-			@Result(name = SUCCESS_UNAUTH, location = "request-access-unauthenticated.ftl"),
-			@Result(name = ERROR, type = TdarActionSupport.FREEMARKERHTTP, location = "/WEB-INF/content/errors/error.ftl", params = {
-					"status", "500" }),
-			@Result(name = INPUT, type = TdarActionSupport.FREEMARKERHTTP, location = "/WEB-INF/content/errors/error.ftl", params = {
-					"status", "500" }) })
-	@HttpsOnly
-	@SkipValidation
-	@Override
-	public String execute() throws TdarActionException {
-		// if we're logged out, go to request-access-unathenticated.ftl
-		if (PersistableUtils.isNullOrTransient(getAuthenticatedUser())) {
-			return SUCCESS_UNAUTH;
-		}
+    @Action(value = "{id}", results = { @Result(name = SUCCESS, location = "request-access.ftl"),
+            @Result(name = SUCCESS_UNAUTH, location = "request-access-unauthenticated.ftl"),
+            @Result(name = ERROR, type = TdarActionSupport.FREEMARKERHTTP, location = "/WEB-INF/content/errors/error.ftl", params = {
+                    "status", "500" }),
+            @Result(name = INPUT, type = TdarActionSupport.FREEMARKERHTTP, location = "/WEB-INF/content/errors/error.ftl", params = {
+                    "status", "500" }) })
+    @HttpsOnly
+    @SkipValidation
+    @Override
+    public String execute() throws TdarActionException {
+        // if we're logged out, go to request-access-unathenticated.ftl
+        if (PersistableUtils.isNullOrTransient(getAuthenticatedUser())) {
+            return SUCCESS_UNAUTH;
+        }
 
-		return SUCCESS;
-	}
+        return SUCCESS;
+    }
 
-	@Override
-	public boolean authorize() throws TdarActionException {
-		return true;
-	}
+    @Override
+    public boolean authorize() throws TdarActionException {
+        return true;
+    }
 
-	@Override
-	public Resource getPersistable() {
-		return getResource();
-	}
+    @Override
+    public Resource getPersistable() {
+        return getResource();
+    }
 
-	@Override
-	public void setPersistable(Resource persistable) {
-		getLogger().debug("set persistable: {}", persistable);
-		this.resource = persistable;
-	}
+    @Override
+    public void setPersistable(Resource persistable) {
+        getLogger().debug("set persistable: {}", persistable);
+        this.resource = persistable;
+    }
 
-	@Override
-	public Class<Resource> getPersistableClass() {
-		return Resource.class;
-	}
+    @Override
+    public Class<Resource> getPersistableClass() {
+        return Resource.class;
+    }
 
-	public Resource getResource() {
-		return resource;
-	}
+    public Resource getResource() {
+        return resource;
+    }
 
-	public void setResource(Resource resource) {
-		this.resource = resource;
-	}
+    public void setResource(Resource resource) {
+        this.resource = resource;
+    }
 
-	@Override
-	public Long getId() {
-		return id;
-	}
+    @Override
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	@Override
-	public InternalTdarRights getAdminRights() {
-		return InternalTdarRights.VIEW_ANYTHING;
-	}
+    @Override
+    public InternalTdarRights getAdminRights() {
+        return InternalTdarRights.VIEW_ANYTHING;
+    }
 
-	public AntiSpamHelper getH() {
-		return h;
-	}
+    public AntiSpamHelper getH() {
+        return h;
+    }
 
-	public void setH(AntiSpamHelper h) {
-		this.h = h;
-	}
+    public void setH(AntiSpamHelper h) {
+        this.h = h;
+    }
 
-	public Set<EmailMessageType> getEmailTypes() {
-		return emailTypes;
-	}
+    public Set<EmailMessageType> getEmailTypes() {
+        return emailTypes;
+    }
 
-	public List<ResourceCreatorProxy> getContactProxies() {
-		return proxies;
+    public List<ResourceCreatorProxy> getContactProxies() {
+        return proxies;
 
-	}
+    }
 
     public EmailMessageType getType() {
         return type;
