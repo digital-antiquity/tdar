bf6681144f29b6e2d8fdfe548c2e476ac947ab93
Adam Brin <abrin@digitalantiquity.org>
2016-04-22 12:44:36 -0700

    moving interface into better location

--- web/src/main/java/org/tdar/struts/action/collection/CollectionViewAction.java
+++ web/src/main/java/org/tdar/struts/action/collection/CollectionViewAction.java
@@ -45,6 +45,7 @@ import org.tdar.search.query.facet.FacetWrapper;
 import org.tdar.search.query.facet.FacetedResultHandler;
 import org.tdar.search.service.query.ResourceSearchService;
 import org.tdar.struts.action.AbstractPersistableViewableAction;
+import org.tdar.struts.action.ResourceFacetedAction;
 import org.tdar.struts.action.SlugViewAction;
 import org.tdar.struts.action.TdarActionException;
 import org.tdar.struts.action.TdarActionSupport;
