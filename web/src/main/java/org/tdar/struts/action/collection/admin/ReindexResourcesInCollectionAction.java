6f3dfd3e462c16ad5264186af4d296ba1db55da6
Adam Brin <adam@brin.org>
2016-05-09 17:33:30 -0700

    TDAR-5257 - adding display orientation toggle - also adding dedicated action to fully reindex a collection

---
+++ web/src/main/java/org/tdar/struts/action/collection/admin/ReindexResourcesInCollectionAction.java
@@ -0,0 +1,77 @@
+package org.tdar.struts.action.collection.admin;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.tdar.core.bean.TdarGroup;
+import org.tdar.core.bean.collection.ResourceCollection;
+import org.tdar.core.service.ResourceCollectionService;
+import org.tdar.search.service.index.SearchIndexService;
+import org.tdar.struts.action.AbstractAuthenticatableAction;
+import org.tdar.struts.interceptor.annotation.PostOnly;
+import org.tdar.struts.interceptor.annotation.RequiresTdarUserGroup;
+import org.tdar.struts.interceptor.annotation.WriteableSession;
+
+import com.opensymphony.xwork2.Preparable;
+
+@Component
+@Scope("prototype")
+@ParentPackage("secured")
+@RequiresTdarUserGroup(TdarGroup.TDAR_EDITOR)
+@Namespace("/collection/admin")
+public class ReindexResourcesInCollectionAction extends AbstractAuthenticatableAction implements Preparable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -5131633751574486076L;
+    @Autowired
+    private ResourceCollectionService resourceCollectionService;
+    @Autowired
+    private SearchIndexService searchIndexService;
+
+    private Long id;
+    private ResourceCollection collection;
+    private boolean async;
+
+    @Override
+    public void prepare() throws Exception {
+        collection = resourceCollectionService.find(id);
+    }
+
+    @Override
+    @PostOnly
+    @WriteableSession
+    @Action(value = "reindex/{id}", results={
+            @Result(name = SUCCESS, type = TDAR_REDIRECT, location = "${collection.detailUrl}"),
+    })
+    public String execute() throws Exception {
+        if (isAsync()) {
+            searchIndexService.indexAllResourcesInCollectionSubTreeAsync(collection);
+        } else {
+            searchIndexService.indexAllResourcesInCollectionSubTree(collection);
+        }
+        return SUCCESS;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isAsync() {
+        return async;
+    }
+
+    public void setAsync(boolean async) {
+        this.async = async;
+    }
+
+}
