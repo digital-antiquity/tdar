2b1368fee32887ce8c698dc1e874a4b4f849e67b
Adam Brin <adam@brin.org>
2016-05-10 20:38:35 -0700

    fixing missing import

--- core/src/main/java/org/tdar/core/service/integration/ModernIntegrationDataResult.java
+++ core/src/main/java/org/tdar/core/service/integration/ModernIntegrationDataResult.java
@@ -2,6 +2,7 @@ package org.tdar.core.service.integration;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
