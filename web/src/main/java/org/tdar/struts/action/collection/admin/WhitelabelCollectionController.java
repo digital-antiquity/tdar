8a47a586bf8c65f37b91f4e7c9a0b490cd63a23c
Adam Brin <adam@brin.org>
2016-07-15 16:01:04 -0700

    cleanup and fixing more tests

--- web/src/main/java/org/tdar/struts/action/collection/admin/WhitelabelCollectionController.java
+++ web/src/main/java/org/tdar/struts/action/collection/admin/WhitelabelCollectionController.java
@@ -56,7 +56,7 @@ public class WhitelabelCollectionController extends AbstractAuthenticatableActio
                     @Result(name = INPUT, location = "edit.ftl") })
     public String save() throws TdarActionException {
         getGenericService().saveOrUpdate(getCollection());
-        getGenericService().saveOrUpdate(getCollection().getProperties());
+//        getGenericService().saveOrUpdate(getCollection().getProperties());
         getLogger().trace("{} {} {} ", getCollection().getId(), getCollection().getProperties().getSubtitle(), getCollection().getProperties().isWhitelabel());
         return SUCCESS;
     }
