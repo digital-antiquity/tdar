045e67c8c2725f7311437b5fd6dfed85c2a82ba0
Adam Brin <abrin@digitalantiquity.org>
2016-07-28 11:31:20 -0700

    quick fix for LLB issue -- putting back indexing call

--- search/src/main/java/org/tdar/search/converter/ResourceDocumentConverter.java
+++ search/src/main/java/org/tdar/search/converter/ResourceDocumentConverter.java
@@ -306,6 +306,9 @@ public class ResourceDocumentConverter extends AbstractSolrDocumentConverter {
         List<Integer> scales = new ArrayList<>();
         List<Long> llibId = new ArrayList<>();
         for (LatitudeLongitudeBox llb : resource.getActiveLatitudeLongitudeBoxes()) {
+            if (llb.getObfuscatedNorth() == null) {
+                llb.obfuscateAll();
+            }
             Envelope env = new Envelope(llb.getObfuscatedWest(), llb.getObfuscatedEast(), llb.getObfuscatedSouth(),
                     llb.getObfuscatedNorth());
             llibId.add(llb.getId());
