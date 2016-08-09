7bb44424af8e1f2c5ae73afadc2f041da6a89d93
Adam Brin <adam@brin.org>
2016-08-08 18:07:02 -0700

    making sure we're storing everything

--- search/src/main/java/org/tdar/search/query/QueryFieldNames.java
+++ search/src/main/java/org/tdar/search/query/QueryFieldNames.java
@@ -110,9 +110,9 @@ public interface QueryFieldNames {
 	String ACRONYM = "acronym";
 	String DOCUMENT_SUB_TYPE = "subType";
 	String DEGREE = "degree";
-	String SERIES_NAME = "seriesName";
+	String SERIES_NAME = "series.name";
 	String BOOK_TITLE = "bookTitle";
-	String JOURNAL_NAME = "journalName";
+	String JOURNAL_NAME = "journal.name";
 	String ISSN = "issn";
 	String ISBN = "isbn";
 	String RESOURCE_LANGUAGE = "resourceLanguage";
@@ -140,6 +140,6 @@ public interface QueryFieldNames {
     String COPY_LOCATION = "copy.location";
     String START_PAGE = "page.start";
     String END_PAGE = "page.end";
-    String JOURNAL_NUMBER = "journal.nymber";
+    String JOURNAL_NUMBER = "journal.number";
     
 }
