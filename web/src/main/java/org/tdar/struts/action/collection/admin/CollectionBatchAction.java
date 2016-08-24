95f6f24ffacbf3a6265be2de09005cc986ef4eb4
Adam Brin <adam@brin.org>
2016-08-23 18:11:57 -0700

    adding basics for collections in batch-acton and adding some validation

--- web/src/main/java/org/tdar/struts/action/collection/admin/CollectionBatchAction.java
+++ web/src/main/java/org/tdar/struts/action/collection/admin/CollectionBatchAction.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.tdar.core.bean.TdarGroup;
 import org.tdar.core.bean.billing.BillingAccount;
+import org.tdar.core.bean.collection.ResourceCollection;
 import org.tdar.core.bean.resource.Project;
 import org.tdar.core.bean.resource.Resource;
 import org.tdar.core.bean.resource.Status;
@@ -30,23 +31,25 @@ import org.tdar.struts.interceptor.annotation.WriteableSession;
 import org.tdar.utils.PersistableUtils;
 
 import com.opensymphony.xwork2.Preparable;
+import com.opensymphony.xwork2.Validateable;
 
 @Component
 @Scope("prototype")
 @ParentPackage("secured")
 @RequiresTdarUserGroup(TdarGroup.TDAR_EDITOR)
 @Namespace("/collection/admin/batch")
-public class CollectionBatchAction extends AbstractCollectionAdminAction implements Preparable {
+public class CollectionBatchAction extends AbstractCollectionAdminAction implements Preparable, Validateable {
 
     private static final long serialVersionUID = 1L;
 
     private Long accountId;
     private BillingAccount account;
-    
+
     private Long projectId;
     private Project project;
+    private Long collectionId;
+    private ResourceCollection collectionToAdd;
 
-    
     private List<Long> ids = new ArrayList<>();
     private List<String> titles = new ArrayList<>();
     private List<String> descriptions = new ArrayList<>();
@@ -66,15 +69,15 @@ public class CollectionBatchAction extends AbstractCollectionAdminAction impleme
     private ResourceService resourceService;
 
     private List<BillingAccount> availableAccounts;
-    
+
     @Override
     public void prepare() throws Exception {
         super.prepare();
         setResources(new ArrayList<>(getCollection().getResources()));
-        Collections.sort(resources , new Comparator<Resource>() {
+        Collections.sort(resources, new Comparator<Resource>() {
             @Override
             public int compare(Resource o1, Resource o2) {
-                return PersistableUtils.compareIds(o1,o2);
+                return PersistableUtils.compareIds(o1, o2);
             }
         });
         setAllSubmittedProjects(projectService.findBySubmitter(getAuthenticatedUser()));
@@ -83,15 +86,32 @@ public class CollectionBatchAction extends AbstractCollectionAdminAction impleme
         setFullUserProjects(new ArrayList<Resource>(projectService.findSparseTitleIdProjectListByPerson(getAuthenticatedUser(), canEditAnything)));
         getFullUserProjects().removeAll(getAllSubmittedProjects());
         setAvailableAccounts(billingAccountService.listAvailableAccountsForUser(getAuthenticatedUser(), Status.ACTIVE));
-        
+
         if (PersistableUtils.isNotNullOrTransient(projectId)) {
             project = genericService.find(Project.class, projectId);
         }
         if (PersistableUtils.isNotNullOrTransient(accountId)) {
             account = billingAccountService.find(accountId);
         }
+        if (PersistableUtils.isNotNullOrTransient(collectionId)) {
+            collectionToAdd = genericService.find(ResourceCollection.class, collectionId);
+        }
     }
 
+    @Override
+    public void validate() {
+        if (PersistableUtils.isNotNullOrTransient(project) && !authorizationService.canEdit(getAuthenticatedUser(), project)) {
+            addActionError("error.permission_denied");
+        }
+        if (PersistableUtils.isNotNullOrTransient(account) && !authorizationService.canEdit(getAuthenticatedUser(), account)) {
+            addActionError("error.permission_denied");
+        }
+        if (PersistableUtils.isNotNullOrTransient(collectionToAdd) && !authorizationService.canAdministerCollection(getAuthenticatedUser(), collectionToAdd)) {
+            addActionError("error.permission_denied");
+        }
+        super.validate();
+    }
+    
     @Override
     @Action(value = "{id}", results = {
             @Result(name = SUCCESS, type = FREEMARKER, location = "../batch.ftl"),
@@ -101,19 +121,17 @@ public class CollectionBatchAction extends AbstractCollectionAdminAction impleme
         return SUCCESS;
     }
 
-    
-    @Action(value = "save", 
+    @Action(value = "save",
             interceptorRefs = { @InterceptorRef("editAuthenticatedStack") },
             results = {
                     @Result(name = SUCCESS, type = REDIRECT, location = "${collection.detailUrl}"),
-    })
+            })
     @PostOnly
     @WriteableSession
     public String save() throws Exception {
-        resourceService.updateBatch(project,account, ids, dates, titles, descriptions, getAuthenticatedUser());
+        resourceService.updateBatch(project, account, collectionToAdd, ids, dates, titles, descriptions, getAuthenticatedUser());
         return SUCCESS;
     }
-
 
     public Long getAccountId() {
         return accountId;
@@ -209,6 +227,22 @@ public class CollectionBatchAction extends AbstractCollectionAdminAction impleme
 
     public void setResources(List<Resource> resources) {
         this.resources = resources;
+    }
+
+    public Long getCollectionId() {
+        return collectionId;
+    }
+
+    public void setCollectionId(Long collectionId) {
+        this.collectionId = collectionId;
+    }
+
+    public ResourceCollection getCollectionToAdd() {
+        return collectionToAdd;
+    }
+
+    public void setCollectionToAdd(ResourceCollection collectionToAdd) {
+        this.collectionToAdd = collectionToAdd;
     }
 
 }
