b174255afc08b5bb4fbc0aeee1159d3c606eb2f4
Adam Brin <adam@brin.org>
2016-09-10 14:45:15 -0700

    - uncommenting handleUploads - preferring classpath based properties file over configured path

--- core/src/main/java/org/tdar/core/configuration/ConfigurationAssistant.java
+++ core/src/main/java/org/tdar/core/configuration/ConfigurationAssistant.java
@@ -81,6 +81,10 @@ public class ConfigurationAssistant implements Serializable {
         InputStream stream = null;
         String CONFIG_DIR = System.getenv(DEFAULT_CONFIG_PATH);
         try {
+            stream = getResourceAsStream(resource);
+            if (stream != null) {
+                return stream;
+            }
             if (CONFIG_DIR != null) {
                 File file = new File(CONFIG_DIR, resource);
                 if (file.exists()) {
@@ -91,8 +95,6 @@ public class ConfigurationAssistant implements Serializable {
             File file = new File(resource);
             if (file.isFile() && file.exists()) {
                 stream = new FileInputStream(file);
-            } else {
-                stream = getResourceAsStream(resource);
             }
         } catch (AccessControlException e) {
             stream = getResourceAsStream(resource);
