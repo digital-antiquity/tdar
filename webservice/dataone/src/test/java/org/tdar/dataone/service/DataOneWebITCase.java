ee48be4b55880531795c449c67835be24317e881
Adam Brin <adam@brin.org>
2016-10-06 09:01:07 -0700

    fixing tests

--- webservice/dataone/src/test/java/org/tdar/dataone/service/DataOneWebITCase.java
+++ webservice/dataone/src/test/java/org/tdar/dataone/service/DataOneWebITCase.java
@@ -146,8 +146,8 @@ public class DataOneWebITCase extends AbstractWebTest {
         xml = xml.replaceAll("&amp;", "&"); // not valid XML, but making sure we're matching what we put in
         logger.debug(xml);
         if (path.contains(TEST_DOI) && path.contains("/object")) {
-            assertTrue(xml.contains("dcterms:identifier rdf:datatype")); // dataOne object
-
+            assertTrue(xml.contains("ore:isAggregatedBy")); // dataOne object
+            assertTrue(xml.contains("ore:ResourceMap")); // dataOne object
         }
         if (path.contains(TEST_DOI_META) && path.contains("/object")) {
             assertTrue(xml.contains("bibliographicCitation><references"));
