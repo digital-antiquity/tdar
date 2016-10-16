a713ca18ac3ed4437cccd76d71f4e8e1d22ba577
Adam Brin <adam@brin.org>
2016-10-16 16:45:39 -0700

    removing debug

--- search/src/main/java/org/tdar/search/converter/ResourceDocumentConverter.java
+++ search/src/main/java/org/tdar/search/converter/ResourceDocumentConverter.java
@@ -55,7 +55,6 @@ public class ResourceDocumentConverter extends AbstractSolrDocumentConverter {
 
         SolrInputDocument doc = convertPersistable(resource);
         doc.setField(QueryFieldNames.NAME, resource.getName());
-        logger.debug("{}", resource);
         doc.setField(QueryFieldNames.NAME_SORT, resource.getTitleSort());
         addRequiredField(resource, doc);
         if (resource.getSubmitter() != null) {
