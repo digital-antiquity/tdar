21caa435d1bcd833c54bd27cd232fcd9413fec88
Adam Brin <adam@brin.org>
2016-10-24 18:36:25 -0700

    setting container on controller (fixing tests for default locale)

--- struts-base/src/main/java/org/tdar/struts_base/interceptor/BaseAuthenticationInterceptor.java
+++ struts-base/src/main/java/org/tdar/struts_base/interceptor/BaseAuthenticationInterceptor.java
@@ -1,12 +1,15 @@
 package org.tdar.struts_base.interceptor;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.WeakHashMap;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.dispatcher.HttpParameters;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.tdar.core.bean.TdarGroup;
 import org.tdar.core.service.ReflectionService;
@@ -138,7 +141,8 @@ public abstract class BaseAuthenticationInterceptor extends AbstractAuthenticati
             return;
         }
         getSessionData().setReturnUrl(returnUrl);
-        getSessionData().setParameters(invocation.getInvocationContext().getParameters());
+        HttpParameters parameters = invocation.getInvocationContext().getParameters();
+        getSessionData().setParameters(parameters.toMap());
         logger.debug("setting returnUrl to: {}", getSessionData().getReturnUrl());
 
     }
