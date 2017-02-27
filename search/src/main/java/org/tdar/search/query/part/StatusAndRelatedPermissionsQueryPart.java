18aeadb79ee36ff8e32aeb804a0a5452cfff2f31
Adam Brin <abrin@digitalantiquity.org>
2017-02-27 13:22:48 -0700

    Fixing Test - and cleanup of logic again (Set instead of List ? just in case). Fix was to properly ignore ACTIVE when status is not included by not adding the effictivelyPublic part

--- search/src/main/java/org/tdar/search/query/part/StatusAndRelatedPermissionsQueryPart.java
+++ search/src/main/java/org/tdar/search/query/part/StatusAndRelatedPermissionsQueryPart.java
@@ -2,8 +2,11 @@ package org.tdar.search.query.part;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -30,10 +33,10 @@ public class StatusAndRelatedPermissionsQueryPart extends FieldQueryPart<Status>
 
     @Override
     public String generateQueryString() {
-        List<Status> localStatuses = new ArrayList<Status>(getFieldValues());
+        Set<Status> localStatuses = new HashSet<Status>(getFieldValues());
         QueryPartGroup draftSubgroup = new QueryPartGroup(Operator.AND);
         QueryPartGroup fieldsSubgroup = new QueryPartGroup(Operator.OR);
-        if (PersistableUtils.isNotNullOrTransient(getPerson()) ) {
+        if (PersistableUtils.isNotNullOrTransient(getPerson())) {
             fieldsSubgroup.append(new FieldQueryPart<Status>(QueryFieldNames.STATUS, Status.DRAFT));
             fieldsSubgroup.append(new FieldQueryPart<Boolean>(QueryFieldNames.HIDDEN, Boolean.TRUE));
             QueryPartGroup permissionsSubgroup = new QueryPartGroup(Operator.OR);
@@ -44,16 +47,23 @@ public class StatusAndRelatedPermissionsQueryPart extends FieldQueryPart<Status>
             }
             draftSubgroup.append(fieldsSubgroup);
         }
-        
+
         // we remove DRAFT because it was handled above
         localStatuses.remove(Status.DRAFT);
         // we remove ACTIVE because it's handled in EffectivelyPublic
-        localStatuses.remove(Status.ACTIVE);
 
-        QueryPartGroup statusSubgroup = new QueryPartGroup(Operator.OR, 
-                new FieldQueryPart<Status>(QueryFieldNames.STATUS, Operator.OR, localStatuses),
-                new FieldQueryPart<Boolean>(QueryFieldNames.EFFECTIVELY_PUBLIC, Boolean.TRUE), 
-                draftSubgroup);
+        QueryPartGroup statusSubgroup = new QueryPartGroup(Operator.OR);
+        // if we're looking for "ACTIVE" resources, then include the EffectivelyPublic test, otherwise,
+        if (localStatuses.contains(Status.ACTIVE)) {
+            FieldQueryPart<Boolean> effectivePart = new FieldQueryPart<Boolean>(QueryFieldNames.EFFECTIVELY_PUBLIC, Boolean.TRUE);
+            localStatuses.remove(Status.ACTIVE);
+            statusSubgroup.append(effectivePart);
+        }
+
+        if (CollectionUtils.isNotEmpty(localStatuses)) {
+            statusSubgroup.append(new FieldQueryPart<Status>(QueryFieldNames.STATUS, Operator.OR, localStatuses));
+        }
+        statusSubgroup.append(draftSubgroup);
 
         return statusSubgroup.generateQueryString();
     }
