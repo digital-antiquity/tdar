b980326a7f6af7ec8fdc29b45cfdefae031e5aee
Adam Brin <abrin@digitalantiquity.org>
2017-03-24 11:18:27 -0700

    TDAR-5689 - fiixng datepicker

--- web/src/main/webapp/js/tdar.datepicker.js
+++ web/src/main/webapp/js/tdar.datepicker.js
@@ -5,15 +5,20 @@
     //scan for any inputs with .datepicker class and initialize them.
     var _init = function() {
         $('input.datepicker').each(function(idx, el) {
-            $(el).datepicker().on('changeDate', function(ev){
-                $(ev.target).datepicker('hide');
-            })
+            _bind(el);
         });
     };
+
+    var _bind = function(el) {
+        $(el).datepicker().on('changeDate', function(ev){
+            $(ev.target).datepicker('hide');
+        })
+    }
     
     //expose public elements
     TDAR.datepicker = {
         "init": _init,
+        "bind": _bind,
         main : function() {
             TDAR.datepicker.init();
         }
