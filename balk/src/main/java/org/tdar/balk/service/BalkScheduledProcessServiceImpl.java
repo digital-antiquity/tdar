1157c46fa0f06351139066b3947b27d94638b5e4
Adam Brin <adam@brin.org>
2017-07-20 06:08:06 -0700

    Finishing the interfacing process (DataONE, OAI-PMH, balk, web)

---
+++ balk/src/main/java/org/tdar/balk/service/BalkScheduledProcessServiceImpl.java
@@ -0,0 +1,70 @@
+package org.tdar.balk.service;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.tdar.balk.bean.PollType;
+import org.tdar.utils.dropbox.DropboxClient;
+import org.tdar.utils.dropbox.DropboxConfig;
+import org.tdar.utils.dropbox.DropboxConstants;
+import org.tdar.utils.dropbox.ToPersistListener;
+
+import com.dropbox.core.DbxException;
+
+@Service
+public class BalkScheduledProcessServiceImpl implements BalkScheduledProcessService {
+
+    private static final long ONE_MIN_MS = 60000;
+    private static final long FIVE_MIN_MS = ONE_MIN_MS * 5;
+    
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private CursorService cursorService;
+    
+    @Autowired
+    private ItemService itemService;
+    
+    /* (non-Javadoc)
+     * @see org.tdar.balk.service.BalkScheduledProcessService#cronUploadTdar()
+     */
+    @Override
+    @Scheduled(fixedDelay = FIVE_MIN_MS)
+    @Transactional(readOnly=false)
+    public void cronUploadTdar() {
+        DropboxClient client;
+        try {
+            client = new DropboxClient();
+            if (client.getDebug() == Boolean.FALSE) {
+                itemService.handleUploads();
+            }
+        } catch (URISyntaxException | IOException | DbxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.tdar.balk.service.BalkScheduledProcessService#cronPollingStatsQueue()
+     */
+    @Override
+    @Scheduled(fixedDelay = ONE_MIN_MS)
+    @Transactional(readOnly=false)
+    public void cronPollingStatsQueue() {
+        try {
+            ToPersistListener listener = new ToPersistListener(itemService);
+            cursorService.pollAndProcess(PollType.STATUS, DropboxConfig.getInstance().getBaseDropboxPath(), listener);
+//            itemService.store(listener);
+        } catch (URISyntaxException | IOException | DbxException e) {
+            logger.error("polling error {}", e,e);
+        }
+        logger.debug("completed poling");
+    }
+
+}
---
+++ web/src/main/java/org/tdar/web/service/DatasetMappingServiceImpl.java
@@ -0,0 +1,60 @@
+package org.tdar.web.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.tdar.core.bean.resource.Project;
+import org.tdar.core.bean.resource.datatable.DataTableColumn;
+import org.tdar.core.dao.resource.DatasetDao;
+import org.tdar.search.exception.SearchIndexException;
+import org.tdar.search.service.index.SearchIndexService;
+
+@Service
+public class DatasetMappingServiceImpl implements DatasetMappingService {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final DatasetDao datasetDao;
+    private final SearchIndexService searchIndexService;
+
+    @Autowired
+    public DatasetMappingServiceImpl(DatasetDao datasetDao, SearchIndexService searchIndexService) {
+        this.datasetDao = datasetDao;
+        this.searchIndexService = searchIndexService;
+    }
+    
+    /*
+     * convenience method, used for Asynchronous as opposed to the Synchronous version by the Controller
+     */
+    /* (non-Javadoc)
+     * @see org.tdar.web.service.DatasetMappingService#remapColumnsAsync(java.util.List, org.tdar.core.bean.resource.Project)
+     */
+    @Override
+    @Async
+    @Transactional
+    public void remapColumnsAsync(final List<DataTableColumn> columns, final Project project) {
+        remapColumns(columns, project);
+    }
+
+    /* (non-Javadoc)
+     * @see org.tdar.web.service.DatasetMappingService#remapColumns(java.util.List, org.tdar.core.bean.resource.Project)
+     */
+    @Override
+    @Transactional
+    public void remapColumns(List<DataTableColumn> columns, Project project) {
+        datasetDao.remapColumns(columns, project);
+        try {
+            searchIndexService.indexProject(project);
+        } catch (SearchIndexException | IOException e) {
+            logger.error("error in reindexing",e);
+        }
+        
+    }
+
+}
