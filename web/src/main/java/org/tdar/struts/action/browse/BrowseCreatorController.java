0f9ec1a32e566b0bcf02a392d04cd3ec66f05035
Adam Brin <abrin@digitalantiquity.org>
2017-07-27 10:21:11 -0700

    speeding up browseCreator search by limiting the # of facets to 10

--- web/src/main/java/org/tdar/struts/action/browse/BrowseCreatorController.java
+++ web/src/main/java/org/tdar/struts/action/browse/BrowseCreatorController.java
@@ -278,7 +278,8 @@ public class BrowseCreatorController extends AbstractLookupController<Resource> 
                     roles.add(role.name());
                     getFacetWrapper().facetBy(role.name(), Creator.class);
                 }
-                getFacetWrapper().setMaxFacetLimit(20);
+                // 10 per facet group should be plenty
+                getFacetWrapper().setMaxFacetLimit(10);
                 resourceSearchService.generateQueryForRelatedResources(creator, getAuthenticatedUser(), this, this);
                 List<Long> ignoreIds = new ArrayList<>();
                 ignoreIds.add(creator.getId());
