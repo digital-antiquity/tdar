81e0d7c5c65bc0d8d64e85dbab13f90ad9d154e8
Adam Brin <adam@brin.org>
2017-08-17 17:34:22 -0700

    fixing test

--- web/src/test/java/org/tdar/struts/action/BillingAccountControllerITCase.java
+++ web/src/test/java/org/tdar/struts/action/BillingAccountControllerITCase.java
@@ -188,12 +188,13 @@ public class BillingAccountControllerITCase extends AbstractControllerITCase imp
     private Long setupAccountWithUsers() throws TdarActionException {
         BillingAccountController controller = generateNewInitializedController(BillingAccountController.class);
         controller.prepare();
+        controller.add();
         controller.setName("my test account");
-        controller.validate();
         controller.getAuthorizedMembers().add(getAdminUser());
         controller.getAuthorizedMembers().add(getBillingUser());
         controller.getAuthorizedMembers().add(getEditorUser());
         controller.setServletRequest(getServletPostRequest());
+//        controller.validate();
         String save = controller.save();
         Long id = controller.getAccount().getId();
         assertEquals(Action.SUCCESS, save);
