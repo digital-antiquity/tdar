5b21cabb2e882afadf4e514e2e1d97ba5e32b402
Adam Brin <abrin@digitalantiquity.org>
2017-11-02 09:29:55 -0700

    limiting balk users

--- core/src/main/java/org/tdar/core/bean/TdarGroup.java
+++ core/src/main/java/org/tdar/core/bean/TdarGroup.java
@@ -11,11 +11,12 @@ public enum TdarGroup implements HasLabel {
     TDAR_ADMIN("tdar-admins", 1000),
     TDAR_BILLING_MANAGER("tdar-billing", 600),
     TDAR_EDITOR("tdar-editors", 500),
-    TDAR_API_USER("tdar-api-users", 2),
+    TDAR_API_USER("tdar-api-users", 3),
     TDAR_USERS("tdar-users", 1),
     JIRA_USERS("jira-users", -1),
     CONFLUENCE_USERS("confluence-users", -1),
-    UNAUTHORIZED("", -2);
+    UNAUTHORIZED("", -2), 
+    TDAR_BALK("tdar-balk",2);
 
     private final String groupName;
     private final int permissionLevel;
