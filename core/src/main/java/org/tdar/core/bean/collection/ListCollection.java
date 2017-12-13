1beb3993bdbd03f7870914913882a804342a8b09
Adam Brin <abrin@digitalantiquity.org>
2017-12-13 12:23:07 -0700

    Merge - stable

--- core/src/main/java/org/tdar/core/bean/collection/ListCollection.java
+++ core/src/main/java/org/tdar/core/bean/collection/ListCollection.java
@@ -57,7 +57,7 @@ public class ListCollection extends HierarchicalCollection<ListCollection> imple
     private Set<Resource> unmanagedResources = new LinkedHashSet<Resource>();
 
     public ListCollection() {
-        setProperties(new CollectionDisplayProperties(false,false,false,false,false,false));
+        setProperties(new CollectionDisplayProperties(false,false,false,false,false,false, false));
         setType(CollectionType.LIST);
     }
     
@@ -68,7 +68,7 @@ public class ListCollection extends HierarchicalCollection<ListCollection> imple
         setSortBy(sortBy);
         setHidden(hidden);
         this.setType(CollectionType.LIST);
-        setProperties(new CollectionDisplayProperties(false,false,false,false,false,false));
+        setProperties(new CollectionDisplayProperties(false,false,false,false,false,false, false));
     }
 
     public ListCollection(String name, String description, SortOption sort, boolean hidden, TdarUser basicUser) {
@@ -78,7 +78,7 @@ public class ListCollection extends HierarchicalCollection<ListCollection> imple
         setHidden(hidden);
         this.setType(CollectionType.LIST);
         setOwner(basicUser);
-        setProperties(new CollectionDisplayProperties(false,false,false,false,false,false));
+        setProperties(new CollectionDisplayProperties(false,false,false,false,false,false, false));
     }
 
     @XmlTransient
--- core/src/main/java/org/tdar/core/bean/collection/ResourceCollection.java
+++ core/src/main/java/org/tdar/core/bean/collection/ResourceCollection.java
@@ -218,6 +218,7 @@ public abstract class ResourceCollection extends AbstractPersistable
             @AttributeOverride(name = "search_enabled", column = @Column(table = "whitelabel_collection", columnDefinition="boolean default false")),
             @AttributeOverride(name = "sub_collections_enabled", column = @Column(table = "whitelabel_collection", columnDefinition="boolean default false")),
             @AttributeOverride(name = "subtitle", column = @Column(table = "whitelabel_collection")),
+            @AttributeOverride(name = "hide_collection_sidebar", column = @Column(table = "whitelabel_collection")),
             @AttributeOverride(name = "css", column = @Column(table = "whitelabel_collection"))
     })
     @Access(AccessType.FIELD)
