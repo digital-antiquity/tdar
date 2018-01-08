535070460011308b7bfbfb8df56eaf9f1bc6f8b9
Adam Brin <abrin@digitalantiquity.org>
2018-01-08 11:31:18 -0700

    adding karma coverage back in

--- web/karma.conf.js
+++ web/karma.conf.js
@@ -93,7 +93,7 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress',"junit"],
+        reporters: ['progress',"junit",'coverage'],
         junitReporter: {
             outputDir: 'target/karma/reports/',
             suite: 'models'
