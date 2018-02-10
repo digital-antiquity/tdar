91f8d3a75835437b19f8969d96cc9ad656d93239
Adam Brin <adam@brin.org>
2018-02-10 15:09:48 -0700

    Merge stable and fixing compilation issues

--- core/src/test/java/org/tdar/core/bean/AdminRightsAssignmentITCase.java
+++ core/src/test/java/org/tdar/core/bean/AdminRightsAssignmentITCase.java
@@ -2,7 +2,6 @@ package org.tdar.core.bean;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -11,7 +10,7 @@ import org.junit.Test;
 import org.springframework.test.annotation.Rollback;
 import org.tdar.core.bean.entity.AuthorizedUser;
 import org.tdar.core.bean.entity.TdarUser;
-import org.tdar.core.bean.entity.permissions.GeneralPermissions;
+import org.tdar.core.bean.entity.permissions.Permissions;
 import org.tdar.core.bean.resource.Dataset;
 import org.tdar.core.bean.resource.UserRightsProxy;
 
@@ -28,7 +27,7 @@ public class AdminRightsAssignmentITCase extends AbstractIntegrationTestCase{
     	Dataset resource = createAndSaveNewDataset();
     	Long resourceId = resource.getId();
     	AuthorizedUser adminPermission = resource.getAuthorizedUsers().iterator().next();
-    	adminPermission.setGeneralPermission(GeneralPermissions.ADMINISTER_SHARE);
+    	adminPermission.setGeneralPermission(Permissions.ADMINISTER_COLLECTION);
     	
     	genericService.saveOrUpdate(adminPermission);
     	genericService.saveOrUpdate(resource);
@@ -37,8 +36,8 @@ public class AdminRightsAssignmentITCase extends AbstractIntegrationTestCase{
     	
     	TdarUser newUser = createAndSaveNewUser();
     	
-        List<UserRightsProxy> proxies = Arrays.asList(new UserRightsProxy(new AuthorizedUser(getAdminUser(), getAdminUser(), GeneralPermissions.MODIFY_RECORD)),
-        		new UserRightsProxy(new AuthorizedUser(getAdminUser(), newUser, GeneralPermissions.MODIFY_RECORD)));
+        List<UserRightsProxy> proxies = Arrays.asList(new UserRightsProxy(new AuthorizedUser(getAdminUser(), getAdminUser(), Permissions.MODIFY_RECORD)),
+        		new UserRightsProxy(new AuthorizedUser(getAdminUser(), newUser, Permissions.MODIFY_RECORD)));
         resourceCollectionService.saveResourceRights(proxies, getAdminUser(), resource);    	
 
         genericService.synchronize();
@@ -52,10 +51,10 @@ public class AdminRightsAssignmentITCase extends AbstractIntegrationTestCase{
         	AuthorizedUser permission = it.next();
         	
         	if(permission.getUser().equals(getAdminUser())){
-        		assertEquals("The permission was set to MODIFY_RECORD", GeneralPermissions.MODIFY_RECORD, permission.getGeneralPermission());
+        		assertEquals("The permission was set to MODIFY_RECORD", Permissions.MODIFY_RECORD, permission.getGeneralPermission());
         	}
         	else if(permission.getUser().equals(newUser)){
-        		assertEquals("The permission was set to MODIFY_RECORD", GeneralPermissions.MODIFY_RECORD, permission.getGeneralPermission());
+        		assertEquals("The permission was set to MODIFY_RECORD", Permissions.MODIFY_RECORD, permission.getGeneralPermission());
         	}
         	
         }
