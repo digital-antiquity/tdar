b25ab318d5e16ae06075e25d49861a6a856bd6a1
Brian Castellanos <bcastel1@asu.edu>
2018-04-17 16:17:28 -0700

    Add the TDAR components to webpack. not sure if the jQuery modules will resolve correctly.

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main/webapp/js/tdar.auth.js',
+  entry: './src/main/webapp/js/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './src/main/webapp/components')
