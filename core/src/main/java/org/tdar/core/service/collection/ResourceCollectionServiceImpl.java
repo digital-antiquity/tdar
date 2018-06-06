b8943ece6972a42fa2c9647a1610989c338cd44c
Adam Brin <abrin@digitalantiquity.org>
2018-06-06 10:33:54 -0700

    Merge - stable

--- core/src/main/java/org/tdar/core/service/collection/ResourceCollectionServiceImpl.java
+++ core/src/main/java/org/tdar/core/service/collection/ResourceCollectionServiceImpl.java
@@ -21,7 +21,6 @@ import java.util.TreeSet;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.jena.atlas.test.Gen;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
