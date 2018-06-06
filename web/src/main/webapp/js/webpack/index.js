14c4a59e24d751ae1a9156704154a4ce0d7f8bfb
Adam Brin <abrin@digitalantiquity.org>
2018-06-06 08:52:27 -0700

    !!! breaks tons of stuff:
    1. replaces yarn with npm (so use `npm install` not yarn install)
    2. gets down to a single version of jquery
    3. adds in validation and other plugins
    4. resolves $
    5. running `rm -Rrf src/main/webapp/webpack/* ; npm run-script build; npm run-script webpack-server` each time we change a file to rest to confirm changes propagate properly

--- web/src/main/webapp/js/webpack/index.js
+++ web/src/main/webapp/js/webpack/index.js
@@ -4,60 +4,97 @@ import './../tdar.core';
 // import TDAR from './../tdar.core';
 // window.TDAR = TDAR;
 console.log("hi");
-import 'jquery';
-import $ from 'jquery/dist/jquery';
+//import 'jquery';
+import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
+import "jquery-validation/dist/jquery.validate";
+import "jquery-validation/dist/additional-methods";
 import 'leaflet/dist/leaflet';
 import 'leaflet-draw/dist/leaflet.draw';
 import 'leaflet-choropleth/dist/choropleth.js';
 import 'd3/d3';
+//import 'vue/dist/vue.min.js'
 import c3 from 'c3/c3';
 window.c3 = c3;
+
+//import "bootstrap-2.3.2/js/bootstrap-affix.js";
+//import "bootstrap-2.3.2/js/bootstrap-alert.js";
+//import "bootstrap-2.3.2/js/bootstrap-button.js";
+//import "bootstrap-2.3.2/js/bootstrap-carousel.js";
+//import "bootstrap-2.3.2/js/bootstrap-collapse.js";
+//import "bootstrap-2.3.2/js/bootstrap-dropdown.js";
+//import "bootstrap-2.3.2/js/bootstrap-modal.js";
+//import "bootstrap-2.3.2/js/bootstrap-popover.js";
+//import "bootstrap-2.3.2/js/bootstrap-scrollspy.js";
+//import "bootstrap-2.3.2/js/bootstrap-tab.js";
+//import "bootstrap-2.3.2/js/bootstrap-tooltip.js";
+//import "bootstrap-2.3.2/js/bootstrap-transition.js";
+//import "bootstrap-2.3.2/js/bootstrap-typeahead.js";
+//import './src/main/webapp/css/tdar-svg.css';
+
+import './../../includes/jquery-ui-1.11.4.custom/jquery-ui.js';
+
+import './../../includes/jquery.cookie.js';
+//import 'svgxuse/svgxuse.min.js';
+
+import './../../includes/jquery.textarearesizer.js';
+
+/* LICENSE: MIT */
+import './../jquery.FormNavigate.js';
+import './../../includes/jquery.watermark-3.1.3.min.js';
+
+/* LICENSE: MIT */
+import './../../includes/jquery.datatables-1.9.4/media/js/jquery.dataTables.js';
+
+/* LICENSE: BSD /GPL 2.0 */
+import './../../includes/jquery.datatables.plugins-1.9.4/integration/bootstrap/2/dataTables.bootstrap.js';
+
+/* LICENSE: BSD /GPL 2.0 */
+//import 'jquery.treeview/jquery.treeview.js';
+
+
 // import 'c3/c3.min.css';
-
-
+//
+//
 import './../tdar.common';
 import './../tdar.datepicker';
 import './../tdar.moreinfo';
 import './../tdar.upload';
 import './../tdar.bulk';
 import './../tdar.repeatrow';
-import './../tdar.autocomplete';
+//import './../tdar.autocomplete';
 import './../tdar.c3graphsupport';
 import './../tdar.c3graph';
 import './../tdar.validate';
 import './../tdar.d3tree';
-
-import './../tdar.datatable';
-import './../tdar.dataset-metadata';
-import './../tdar.ontology-mapping';
+//
+//import './../tdar.datatable';
+//import './../tdar.dataset-metadata';
+//import './../tdar.ontology-mapping';
 import './../tdar.sensorydata';
 import './../tdar.notifications';
 import './../tdar.advanced-search';
-import './../tdar.authority-management';
+//import './../tdar.authority-management';
 import './../tdar.auth';
-import './../tdar.inheritance';
+//import './../tdar.inheritance';
 import './../tdar.download';
 import './../tdar.pricing';
 import './../tdar.contexthelp';
 import './../tdar.menu';
-//
-// import './../tdar.formValidateExtensions';
-// import './../tdar.jquery-upload-validation';
-// import './../tdar.messages_en';
-import './../../includes/bindWithDelay';
-import './../tdar.bookmark';
+////
+import './../tdar.formValidateExtensions';
+import './../tdar.jquery-upload-validation';
+ import './../tdar.messages_en';
+//import './../../includes/bindWithDelay';
+//import './../tdar.bookmark';
 import './../maps/tdar.leaflet';
 import './../maps/tdar.worldmap';
-
-
-// import worldmap from './../maps/tdar.worldmap';
-// TDAR.worldmap = worldmap;
 //
-// import './../vue/vue-selectize';
+
+//
 // import './../vue/view-collection-widget';
 // import './../vue/edit-collection';
 
 
-TDAR.main();
+//TDAR.main();
