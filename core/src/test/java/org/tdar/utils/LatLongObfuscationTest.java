2fe5db2dbbc81b4a626d8b8c6e85772ee99f70bf
Adam Brin <adam@brin.org>
2018-06-10 17:40:13 -0700

    resetting jackson

--- core/src/test/java/org/tdar/utils/LatLongObfuscationTest.java
+++ core/src/test/java/org/tdar/utils/LatLongObfuscationTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertTrue;
 
 import org.eclipse.rdf4j.model.vocabulary.SP;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,7 +36,6 @@ public class LatLongObfuscationTest {
     }
 
     @Test
-    @Ignore
     public void testNegLatLongWithSaltedResult() {
         logger.debug("****** TEST testNegLatLongWithSaltedResult *******");
         SpatialObfuscationUtil.useRandom(false);
