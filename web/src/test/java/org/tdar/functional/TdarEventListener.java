f963435e86151f46e4b35d033e3207023686a790
Adam Brin <abrin@digitalantiquity.org>
2018-07-03 10:10:28 -0700

    cleaning up dependencies and dropping legacy selenium (may work)

--- web/src/test/java/org/tdar/functional/TdarEventListener.java
+++ web/src/test/java/org/tdar/functional/TdarEventListener.java
@@ -239,7 +239,16 @@ public class TdarEventListener extends WebDriverEventAdapter {
     @Override
     public <X> void beforeGetScreenshotAs(OutputType<X> arg0) {
         logger.trace("beforeGetScreenshotAs");
-        
+    }
+
+    @Override
+    public void beforeGetText(WebElement we, WebDriver wd) {
+        logger.trace("beforeGetText");
+    }
+
+    @Override
+    public void afterGetText(WebElement we, WebDriver wd, String str) {
+        logger.trace("afterGetText");
     }
 
 }
