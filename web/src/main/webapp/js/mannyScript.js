89b876acc311ec158288145ff423dfac772433b0
ASURITE\msingh45 <msingh45@asu.edu>
2018-08-01 14:11:15 -0700

    Removing script because of issues with other browsers. Will fix later

--- web/src/main/webapp/js/mannyScript.js
+++ web/src/main/webapp/js/mannyScript.js
@@ -1,8 +1,8 @@
-
-$('.tdarNavItem').click(function()
-{
-    if ($(this).find('.nav-chevron').attr('transform'))
-        $(this).find('.nav-chevron').removeAttr('transform');
-    else
-        $(this).find('.nav-chevron').attr('transform', 'rotate(180)');
-});
+//
+// $('.tdarNavItem').click(function()
+// {
+//     if ($(this).find('.nav-chevron').attr('transform'))
+//         $(this).find('.nav-chevron').removeAttr('transform');
+//     else
+//         $(this).find('.nav-chevron').attr('transform', 'rotate(180)');
+// });
