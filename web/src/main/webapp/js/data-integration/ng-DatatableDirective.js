442a9fb59bb9338bf9b8158b41712ff04ae85c41
Adam Brin <adam@brin.org>
2018-09-04 06:11:28 -0700

    trying to store data

--- web/src/main/webapp/js/data-integration/ng-DatatableDirective.js
+++ web/src/main/webapp/js/data-integration/ng-DatatableDirective.js
@@ -22,7 +22,7 @@
         };
         var self =  {
             restrict: 'A',
-            link: function(scope, element, attrs){
+            link: function(scope, element, attrs, ctrl){
                 console.debug("linking to dom element:%s", attrs.id);
                 var widget = false;
                 // If THEAD has columns, we are ready to init datatable
@@ -33,9 +33,9 @@
                 //if controller sets/modifies  $scope[attrs.aoColumns], (re)initialize the datatable
                 if(typeof attrs.aoColumns !== "undefined") {
                     scope.$watch(attrs.aoColumns, function(val){
-                        console.debug("%s:: ao-columns:'", attrs.id, val);
+                        console.debug("%s:: ao-columns:'", attrs.id, attrs);
                         if(!val){return}
-
+                        console.debug(scope);
                         //aoColumns needs to be in specific format. for now we define all columns
                         var aoColumns = val.map(function(colname){
                             return {sTitle: colname};
@@ -49,17 +49,18 @@
                         } 
                         var options = $.extend({}, _defaultOptions, {
                             aoColumns: aoColumns,
-                            aaData: []
+                            aaData: val.aoData
                         });
                         widget = element.DataTable(options);
                         console.debug(widget);
+                        element.widget = widget;
                     });
                 }
                 console.debug("%s:: ", attrs.id, element, self);
                 //look for changes to the aa-data attribute (which corresponds to the options.aaData value you would pass to $.Datatable() )
                 scope.$watch(attrs.aaData, function(val){
                     console.debug("%s:: aa-data:", attrs.id,  val, widget);
-                    self.updateRowData(widget, val);
+                    self.updateRowData(element.widget, val);
                 });
             },
 
