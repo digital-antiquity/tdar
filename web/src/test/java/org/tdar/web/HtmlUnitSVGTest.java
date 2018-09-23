56b2d6fcb16b4a77da42d2e5b3489f7febfdb113
Adam Brin <adam@brin.org>
2018-09-23 13:02:08 -0700

    - adding ignores - updating spring, tika, pdfbox - fixing compilation issues

--- web/src/test/java/org/tdar/web/HtmlUnitSVGTest.java
+++ web/src/test/java/org/tdar/web/HtmlUnitSVGTest.java
@@ -17,7 +17,7 @@ public class HtmlUnitSVGTest {
     @SuppressWarnings({ "resource", "unused" })
     @Test
     public void testSVG() throws FailingHttpStatusCodeException, MalformedURLException, IOException {
-        WebClient webClient = new WebClient(BrowserVersion.FIREFOX_45);
+        WebClient webClient = new WebClient(BrowserVersion.BEST_SUPPORTED);
         webClient.getOptions().setJavaScriptEnabled(true);
         HtmlPage page = (HtmlPage) webClient.getPage("http://c3js.org");
 
