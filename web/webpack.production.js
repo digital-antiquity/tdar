7678a25516aa670b3a32e9597b52953f02af6ff6
Adam Brin <adam@brin.org>
2018-10-01 11:08:02 -0700

    fixing compilation issue with webpack

--- web/webpack.production.js
+++ web/webpack.production.js
@@ -16,6 +16,7 @@ module.exports = merge(common, {
     module: {rules:[
         {
                test: /\.js$/,
+               exclude: /node_modules/,
                use: {
                  loader: 'babel-loader',
                  options: {
