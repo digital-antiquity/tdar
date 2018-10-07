1d963a074738da79621554b20d9088386157850e
Adam Brin <adam@brin.org>
2018-10-07 14:25:10 -0700

    fixing endpoint

--- web/src/main/webapp/js/webpack/website-entry-point.js
+++ web/src/main/webapp/js/webpack/website-entry-point.js
@@ -1,10 +1,10 @@
 window.$ = window.jQuery = require('jquery'); // or import $ from 'jquery';
-const TDAR          = require("./tdar.core");
-TDAR.common         = require("./tdar.common");
-TDAR.c3graph        = require("./tdar.c3graph");
-TDAR.c3graphsupport = require("./tdar.c3graphsupport");
-TDAR.leaflet         = require("./maps/tdar.leaflet");
-TDAR.worldmap        = require("./maps/tdar.worldmap");
+const TDAR          = require("../tdar.core");
+TDAR.common         = require("../tdar.common");
+TDAR.c3graph        = require("../tdar.c3graph");
+TDAR.c3graphsupport = require("../tdar.c3graphsupport");
+TDAR.leaflet         = require("../maps/tdar.leaflet");
+TDAR.worldmap        = require("../maps/tdar.worldmap");
 require("select2/dist/css/select2.min.css");
 require("select2/dist/js/select2.full.min.js")
 
